3df1e0ed20dcd3a5c209eb58d081a08ac3159884
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2014-07-15 17:11:26 +0200

    New Windows Store project. Authentization - Facebook, Google.

---
+++ UJCSystem/ITJakub.MobileApps.MainApp/LoginPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System.Threading.Tasks;
+using ITJakub.MobileApps.MainApp.Common;
+using ITJakub.MobileApps.MainApp.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+using Microsoft.Live;
+
+namespace ITJakub.MobileApps.MainApp
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class GroupedItemsPage : Page
+    {
+        private NavigationHelper navigationHelper;
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public GroupedItemsPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+        }
+
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            await updateUserName(pageTitle, false);
+        }
+
+        public static async Task updateUserName(TextBlock userName, Boolean signIn)
+        {
+            try
+            {
+                // Open Live Connect SDK client.
+                LiveAuthClient LCAuth = new LiveAuthClient();
+                LiveLoginResult LCLoginResult = await LCAuth.InitializeAsync();
+                try
+                {
+                    LiveLoginResult loginResult = null;
+                    if (signIn)
+                    {
+                        // Sign in to the user's Microsoft account with the required scope.
+                        //  
+                        //  This call will display the Microsoft account sign-in screen if 
+                        //   the user is not already signed in to their Microsoft account 
+                        //   through Windows 8.
+                        // 
+                        //  This call will also display the consent dialog, if the user has 
+                        //   has not already given consent to this app to access the data 
+                        //   described by the scope.
+                        // 
+                        //  Change the parameter of LoginAsync to include the scopes 
+                        //   required by your app.
+                        loginResult = await LCAuth.LoginAsync(new string[] { "wl.basic" });
+                    }
+                    else
+                    {
+                        // If we don't want the user to sign in, continue with the current 
+                        //  sign-in state.
+                        loginResult = LCLoginResult;
+                    }
+                    if (loginResult.Status == LiveConnectSessionStatus.Connected)
+                    {
+                        // Create a client session to get the profile data.
+                        LiveConnectClient connect = new LiveConnectClient(LCAuth.Session);
+
+                        // Get the profile info of the user.
+                        LiveOperationResult operationResult = await connect.GetAsync("me");
+                        dynamic result = operationResult.Result;
+                        if (result != null)
+                        {
+                            // Update the text of the object passed in to the method. 
+                            userName.Text = string.Join(" ", "Hello", result.name, "!");
+                        }
+                        else
+                        {
+                            // Handle the case where the user name was not returned. 
+                        }
+                    }
+                    else
+                    {
+                        // The user hasn't signed in so display this text 
+                        //  in place of his or her name.
+                        userName.Text = "You're not signed in.";
+                    }
+                }
+                catch (LiveAuthException exception)
+                {
+                    // Handle the exception. 
+                }
+            }
+            catch (LiveAuthException exception)
+            {
+                // Handle the exception. 
+            }
+            catch (LiveConnectException exception)
+            {
+                // Handle the exception. 
+            }
+        }
+    }
+}
---
+++ UJCSystem/ITJakub.MobileApps.MainApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Windows.Security.Authentication.Web;
+using Windows.Storage.Streams;
+using Facebook;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace ITJakub.MobileApps.MainApp.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class LoginViewModel : ViewModelBase
+    {
+        private RelayCommand m_facebookLoginCommand;
+        private RelayCommand m_googleLoginCommand;
+
+        /// <summary>
+        /// Initializes a new instance of the LoginViewModel class.
+        /// </summary>
+        public LoginViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+            
+            m_facebookLoginCommand = new RelayCommand(FacebookLogin);
+            m_googleLoginCommand = new RelayCommand(GoogleLogin);
+        }
+
+        public RelayCommand FacebookLoginCommand
+        {
+            get { return m_facebookLoginCommand; }
+        }
+
+        public string Message { get; set; }
+
+        public RelayCommand GoogleLoginCommand
+        {
+            get { return m_googleLoginCommand; }
+        }
+
+        private async void FacebookLogin()
+        {
+            var fbClient = new FacebookClient();
+
+            var loginUrl = fbClient.GetLoginUrl(new
+            {
+                client_id = ***REMOVED***,
+                redirect_uri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().AbsoluteUri,
+                scope = "email",
+                display = "popup",
+                response_type = "token"
+            });
+
+            var webAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(
+                  WebAuthenticationOptions.None,
+                  loginUrl);
+
+            switch (webAuthenticationResult.ResponseStatus)
+            {
+                case WebAuthenticationStatus.Success:
+                    var callbackUri = new Uri(webAuthenticationResult.ResponseData.ToString());
+                    var facebookOAuthResult = fbClient.ParseOAuthCallbackUrl(callbackUri);
+
+                    // Retrieve the Access Token. You can now interact with Facebook on behalf of the user
+                    // using the Access Token.
+                    var accessToken = facebookOAuthResult.AccessToken;
+
+                    fbClient = new FacebookClient(accessToken);
+                    var user = await fbClient.GetTaskAsync("/me");
+                    var result = (IDictionary<string, object>)user;
+
+                    Message = String.Format("First name: {0}, Last name: {1}, Name: {2}, E-mail: {3}, Access token: {4}", result["first_name"], result["last_name"], result["name"], result["email"], accessToken);
+                    RaisePropertyChanged(() => Message);
+                    break;
+                case WebAuthenticationStatus.ErrorHttp:
+                    // handle authentication failure
+                    break;
+                default:
+                    // The user canceled the authentication
+                    break;
+            }
+        }
+
+        private async void GoogleLogin()
+        {
+          /*   var startURL =
+                 "https://accounts.google.com/o/oauth2/auth?client_id=***REMOVED***&response_type=code&scope=openid%20email&redirect_uri=urn:ietf:wg:oauth:2.0:oob";
+            
+
+             var startURI = new Uri(startURL);
+             var endUri = new Uri("https://accounts.google.com/o/oauth2/approval?");
+            
+             string result;
+
+             try
+             {
+                 var webAuthenticationResult =
+                     await WebAuthenticationBroker.AuthenticateAsync(
+                     WebAuthenticationOptions.UseTitle,
+                     startURI,
+                     endUri
+                     );
+
+                 switch (webAuthenticationResult.ResponseStatus)
+                 {
+                     case WebAuthenticationStatus.Success:
+                         // Successful authentication. 
+                         result = webAuthenticationResult.ResponseData.ToString();
+                         break;
+                     case WebAuthenticationStatus.ErrorHttp:
+                         // HTTP error. 
+                         result = webAuthenticationResult.ResponseErrorDetail.ToString();
+                         break;
+                     default:
+                         // Other error.
+                         result = webAuthenticationResult.ResponseData.ToString();
+                         break;
+                 }
+             }
+             catch (Exception ex)
+             {
+                 // Authentication failed. Handle parameter, SSL/TLS, and Network Unavailable errors here. 
+                 result = ex.Message;
+             }
+             Message = result;
+             RaisePropertyChanged(() => Message);
+
+             */
+
+          
+            
+                 var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                         new Uri("ms-appx:///Assets/client_secrets.json"),
+                         new[] { Uri.EscapeUriString(CalendarService.Scope.Calendar) },
+                         "user",
+                         CancellationToken.None);
+            
+        }
+    }
+}
