f212ca181c50f0b68c4d9fe79627f215ceee1e8d
Honza Minarik <minarja4@gmail.com>
2014-09-09 19:22:56 +0200

    Working dropzone ( but at least in firefox is drop not working)

---
+++ UJCSystem/ITJakub.Web.Hub/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITJakub.Web.Hub.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+         [AllowAnonymous]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        //Dropzone upload method
+        public ActionResult UploadFiles()
+        {
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                if (file == null || file.ContentLength == 0) continue;
+
+                string pathString = Path.Combine("D:\\", "UploadedFiles");
+                
+                if (!Directory.Exists(pathString))
+                    Directory.CreateDirectory(pathString);
+
+                string path = string.Format("{0}\\{1}", pathString, file.FileName);
+                file.SaveAs(path);
+            }
+            return Json(new {});
+        }
+    }
+}
