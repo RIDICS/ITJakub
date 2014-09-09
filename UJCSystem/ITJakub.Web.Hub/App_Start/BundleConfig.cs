30a8e2984598fc0c7b57287a55b22710acdc4499
Honza Minarik <minarja4@gmail.com>
2014-09-09 19:06:15 +0200

    Woking dropzone ( but at least in firefox is drop issue)

--- UJCSystem/ITJakub.Web.Hub/App_Start/BundleConfig.cs
+++ UJCSystem/ITJakub.Web.Hub/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace ITJakub.Web.Hub
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
+                     "~/Scripts/dropzone/dropzone.js"));
+
             bundles.Add(new ScriptBundle("~/itjakub/javascript").Include(
                 "~/Scripts/itjakub.js"));
 
@@ -29,6 +32,10 @@ namespace ITJakub.Web.Hub
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzonescss").Include(
+                     "~/Scripts/dropzone/css/basic.css",
+                     "~/Scripts/dropzone/css/dropzone.css"));
 
             bundles.Add(new StyleBundle("~/itjakub/css").Include(
                 "~/Content/itjakub.css"));
