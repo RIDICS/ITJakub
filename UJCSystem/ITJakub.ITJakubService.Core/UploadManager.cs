72137d3246ff63d2a05367c77bb933b9e042bb4c
honza minarik <minarja4@gmail.com>
2014-09-19 16:04:41 +0200

    LocalFilesystemManager save methods and XmlProcessingManager first draft

--- UJCSystem/ITJakub.ITJakubService.Core/UploadManager.cs
+++ UJCSystem/ITJakub.ITJakubService.Core/UploadManager.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Xml;
 using ITJakub.ITJakubService.DataContracts;
 
 namespace ITJakub.ITJakubService.Core
@@ -6,16 +8,29 @@ namespace ITJakub.ITJakubService.Core
     public class UploadManager
     {
         private readonly LocalFilesystemManager m_localFilesystemManager;
+        private readonly XmlProcessingManager m_xmlProcessingManager;
 
-        public UploadManager(LocalFilesystemManager localFilesystemManager)
+        public UploadManager(LocalFilesystemManager localFilesystemManager, XmlProcessingManager xmlProcessingManager)
         {
             m_localFilesystemManager = localFilesystemManager;
+            m_xmlProcessingManager = xmlProcessingManager;
         }
 
         public ProcessedFileInfoContract ProcessUploadedFile(Stream dataStream)
         {
-            string tempName = m_localFilesystemManager.SaveToTempFolder(dataStream);
-            throw new System.NotImplementedException();
+            string tempName = m_localFilesystemManager.SaveTempFile(dataStream);
+            ProcessedFileInfoContract fileInfo = GetFileInfoFromTempFile(tempName);
+            m_localFilesystemManager.RenameTempFile(tempName, fileInfo.Guid);
+            return fileInfo;
+        }
+
+        private ProcessedFileInfoContract GetFileInfoFromTempFile(string fileName)
+        {
+            using (var fileStream = m_localFilesystemManager.OpenTempFile(fileName))
+            {
+                var fileHeader = m_xmlProcessingManager.ParseHeader(fileStream);
+                return m_xmlProcessingManager.GetInfoFromHeader(fileHeader);
+            }
         }
 
         public void SaveFrontImageForFile(Stream dataStream)
---
+++ UJCSystem/ITJakub.ITJakubService.Core/XmlProcessingManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using ITJakub.ITJakubService.DataContracts;
+
+namespace ITJakub.ITJakubService.Core
+{
+    public class XmlProcessingManager
+    {
+        private const string HeaderElement = "teiHeader";
+
+        public ProcessedFileInfoContract GetInfoFromHeader(XmlDocument header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlDocument ParseHeader(FileStream fileStream)
+        {
+            var xmlTextReader = new XmlTextReader(fileStream)
+            {
+                WhitespaceHandling = WhitespaceHandling.None
+            };
+
+            while (xmlTextReader.Read())
+            {
+                if (xmlTextReader.NodeType == XmlNodeType.Element &&
+                    xmlTextReader.LocalName == HeaderElement &&
+                    xmlTextReader.IsStartElement())
+                {
+                    var header = new XmlDocument();
+                    header.LoadXml(xmlTextReader.ReadOuterXml());
+                    return header;
+                }
+            }
+            return null;
+        }
+    }
+}
