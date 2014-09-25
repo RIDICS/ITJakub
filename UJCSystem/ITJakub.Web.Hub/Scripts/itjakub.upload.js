dae9004e31fd5d4fdaad6a29f53a7b92b0689b0f
honza minarik <minarja4@outlook.com>
2014-09-25 18:12:10 +0200

    Working filtering table content

--- UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.upload.js
+++ UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.upload.js
@@ -146,14 +146,55 @@ function getVersionIdFromPage() {
 //});
 
 
-$(document).ready(function () {
-    $('#authorsTable tr')
-    .click(function (event) {
-        var _this = this;
-        $(_this).toggleClass('selected');
-        if (event.target.type !== 'checkbox') {
-            var checkbox = $(_this).find(':checkbox');
-            $(checkbox).attr('checked', !$(checkbox).is(':checked'));
+//$(document).ready(function () {
+   
+//});
+
+$("#searchInput").keyup(function () {
+    //split the current value of searchInput
+    var data = this.value.split(" ");
+    //create a jquery object of the rows
+    var jo = $("#authorsTableBody").find("tr");
+    if (this.value == "") {
+        jo.show();
+        return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
+            }
         }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+}).focus(function () {
+    this.value = "";
+    $(this).css({
+        "color": "black"
     });
+    $(this).unbind('focus');
+}).css({
+    "color": "#C0C0C0"
 });
+
+$('#addAuthorButton')
+   .click(function (event) {
+       $('#addAuthorModal').modal('show');
+   });
+
+$('#authorsTable tr')
+   .click(function (event) {
+       var _this = this;
+       $(_this).toggleClass('selected');
+       if (event.target.type !== 'checkbox') {
+           var checkbox = $(_this).find(':checkbox');
+           $(checkbox).attr('checked', !$(checkbox).is(':checked'));
+       }
+   });
