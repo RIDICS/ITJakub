096f150660ded21d587ab77671c6cd0fa8f8cab7
honza minarik <minarja4@outlook.com>
2014-10-13 15:28:14 +0200

    Plugin menu draft

--- UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.js
+++ UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.js
@@ -1,2 +1,8 @@
 ﻿//window.onload = () => { alert("hello from typescript!"); }
+$('#main-plugins-menu').find('li').click(function (event) {
+    $('#main-plugins-menu').find('li').removeClass('active');
+    $(this).addClass('active');
+    $(this).parents('li').addClass('active');
+    event.stopPropagation();
+});
 //# sourceMappingURL=itjakub.js.map
--- UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.ts
+++ UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.ts
@@ -1,1 +1,8 @@
 ﻿//window.onload = () => { alert("hello from typescript!"); }
+
+$('#main-plugins-menu').find('li').click(function (event: Event) {
+    $('#main-plugins-menu').find('li').removeClass('active');
+    $(this).addClass('active');
+    $(this).parents('li').addClass('active');
+    event.stopPropagation();
+});
