92e15d786f3258ed5f3cb1de1d7dfc359d6e4f69
honza minarik <minarja4@outlook.com>
2014-11-04 17:04:39 +0100

    XML parsing very first draft

---
+++ UJCSystem/ITJakub.XmlProcessingConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ITJakub.XmlProcessingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream xmlFileStream = File.Open("D:\\ITJakubTestXml\\Albrecht.xml", FileMode.Open);
+            var xmlProcessingManager = new XmlProcessingManager();
+            var bookVerionn = xmlProcessingManager.GetMetadataFromXml(xmlFileStream);
+            Console.WriteLine("XML sucessfully parsed!");
+            Console.ReadKey();
+        }
+    }
+}
