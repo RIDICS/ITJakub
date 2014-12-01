a34933f363bf250950bb97b6e795b75df985d416
honza minarik <minarja4@outlook.com>
2014-12-01 13:34:08 +0100

    Reader highlighting performance test

--- UJCSystem/ITJakub.Web.Hub/Scripts/Plugins/Reader/itjakub.plugins.reader.ts
+++ UJCSystem/ITJakub.Web.Hub/Scripts/Plugins/Reader/itjakub.plugins.reader.ts
@@ -133,8 +133,39 @@ class ReaderModule {
     }
 
     private makeControls(book: IBookInfo): HTMLDivElement {
+
         var controlsDiv: HTMLDivElement = document.createElement('div');
         $(controlsDiv).addClass('reader-controls content-container');
+
+        //TEST START
+        //var testDiv: HTMLDivElement = document.createElement('div');
+        //$(testDiv).addClass('testDiv');
+        //controlsDiv.appendChild(testDiv);
+        //var testButton = document.createElement("button");
+        //$(testButton).addClass('testButton-button');
+        //var testSpan = document.createElement("span");
+        //$(testSpan).addClass('glyphicon glyphicon-search');
+        //$(testButton).append(testSpan);
+        //$(testButton).click((event: Event) => {
+        //    if (!$(testButton).hasClass('searched-a')) {
+        //        $(testButton).addClass('searched-a');
+        //    } else {
+        //        $(testButton).removeClass('searched-a');
+        //    }
+        //    for (var i = 0; i < this.pages.length; i++) {
+        //        var pageId = '#page_' + this.pages[i];
+        //       // $(pageId).find('span[data-nlp-type="tei:w"]').addClass('search-hit');
+        //        $(pageId).find('span').removeClass('search-hit');
+        //        if ($(testButton).hasClass('searched-a')) {
+        //            $(pageId).find('span[data-id*="a"]').addClass('search-hit');
+        //        } else {
+        //            $(pageId).find('span[data-id*="e"]').addClass('search-hit');
+        //        }
+        //    }
+        //});
+        //controlsDiv.appendChild(testButton);
+        //TEST END
+
 
         var slider: HTMLDivElement = document.createElement('div');
         $(slider).addClass('slider');
