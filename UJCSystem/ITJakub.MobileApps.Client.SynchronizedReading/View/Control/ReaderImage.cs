ab3cd28796452877ce3378b049de9b9687fb49fa
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2015-02-16 18:09:59 +0100

    Improved image control in synchronized reading.

--- UJCSystem/ITJakub.MobileApps.Client.SynchronizedReading/View/Control/ReaderImage.cs
+++ UJCSystem/ITJakub.MobileApps.Client.SynchronizedReading/View/Control/ReaderImage.cs
@@ -13,6 +13,7 @@ namespace ITJakub.MobileApps.Client.SynchronizedReading.View.Control
         private const double FinalPointerCorrectionX = 17.0;
         private const double PointerCorrectionX = 8.0;
         private const double PointerCorrectionY = 30.0;
+        private const double ScrollStep = 20.0;
 
         public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof (ImageSource), typeof (ReaderImage), new PropertyMetadata(null));
         public static readonly DependencyProperty PointerPositionXProperty = DependencyProperty.Register("PointerPositionX", typeof (double), typeof (ReaderImage), new PropertyMetadata(0.0, OnPointerPositionChanged));
@@ -21,11 +22,11 @@ namespace ITJakub.MobileApps.Client.SynchronizedReading.View.Control
         public static readonly DependencyProperty PointerCalibrationYProperty = DependencyProperty.Register("PointerCalibrationY", typeof(double), typeof(ReaderImage), new PropertyMetadata(0.0));
         public static readonly DependencyProperty ModeProperty = DependencyProperty.Register("Mode", typeof (Modes), typeof (ReaderImage), new PropertyMetadata(Modes.Reader));
         public static readonly DependencyProperty IsScrollingEnabledProperty = DependencyProperty.Register("IsScrollingEnabled", typeof(bool), typeof(ReaderImage), new PropertyMetadata(true, OnScrollingEnabledChanged));
-        
+
         private Image m_cursorImage;
         private Image m_sourceImage;
         private ScrollViewer m_scrollViewer;
-
+        
         public ReaderImage()
         {
             DefaultStyleKey = typeof(ReaderImage);
@@ -86,37 +87,70 @@ namespace ITJakub.MobileApps.Client.SynchronizedReading.View.Control
             if (readerImage == null)
                 return;
 
+            // Count correct pointer position
             var scrollViewer = readerImage.m_scrollViewer;
             var sourceImage = readerImage.m_sourceImage;
-            var zoom = readerImage.m_scrollViewer.ZoomFactor;
+            var zoom = scrollViewer.ZoomFactor;
             var zoomedWidth = sourceImage.ActualWidth*zoom;
             var zoomedHeight = sourceImage.ActualHeight*zoom;
+            var scrollViewerContentWidth = scrollViewer.ActualWidth - scrollViewer.Padding.Left - scrollViewer.Padding.Right;
+            var scrollViewerContentHeight = scrollViewer.ActualHeight - scrollViewer.Padding.Top - scrollViewer.Padding.Bottom;
             
-            var left = zoomedWidth * readerImage.PointerPositionX - FinalPointerCorrectionX;
-            var top = zoomedHeight * readerImage.PointerPositionY;
+            var left = zoomedWidth * readerImage.PointerPositionX - FinalPointerCorrectionX;  // scaled left image position
+            var top = zoomedHeight * readerImage.PointerPositionY;  // scaled top image position
 
-            if (scrollViewer.ActualWidth >= zoomedWidth)
-                left += (scrollViewer.ActualWidth - zoomedWidth)/2;
+            if (scrollViewerContentWidth >= zoomedWidth)
+                left += (scrollViewerContentWidth - zoomedWidth)/2;  // image is in the center of the scrollViewer
             else
-                left -= scrollViewer.HorizontalOffset;
+                left -= scrollViewer.HorizontalOffset;  // image is wider than scrollViewer, so it's only shifted by horizontal offset
 
-            if (scrollViewer.ActualHeight >= zoomedHeight)
-                top += (scrollViewer.ActualHeight - zoomedHeight)/2;
+            if (scrollViewerContentHeight >= zoomedHeight)
+                top += (scrollViewerContentHeight - zoomedHeight)/2;  // image is in the center of the scrollViewer
             else
-                top -= scrollViewer.VerticalOffset;
+                top -= scrollViewer.VerticalOffset;  // image is higher than scrollViewer, so it's only shifted by vertical offset
 
-            if (left < 0)
+            // Count scrolling values and new pointer position
+            bool isScroll = false;
+            double scrollHorizontal = scrollViewer.HorizontalOffset;
+            double scrollVertical = scrollViewer.VerticalOffset;
+            if (left < -FinalPointerCorrectionX)
             {
-                //todo scroll to position
-                left = 0;
+                isScroll = true;
+                scrollHorizontal += left - ScrollStep + FinalPointerCorrectionX;
+                left = scrollHorizontal < 0 ? ScrollStep + scrollHorizontal : ScrollStep;
+                left -= FinalPointerCorrectionX;
+            }
+            else if (left > scrollViewerContentWidth - FinalPointerCorrectionX)
+            {
+                isScroll = true;
+                scrollHorizontal += (left - scrollViewerContentWidth + FinalPointerCorrectionX) + ScrollStep;
+                left = scrollViewerContentWidth - ScrollStep - FinalPointerCorrectionX;
+                if (scrollHorizontal > scrollViewer.ScrollableWidth) left += scrollHorizontal - scrollViewer.ScrollableWidth;
             }
             if (top < 0)
             {
-                //todo scroll to position
-                top = 0;
+                isScroll = true;
+                scrollVertical += top - ScrollStep;
+                top = scrollVertical < 0 ? ScrollStep + scrollVertical : ScrollStep;
             }
+            else if (top > scrollViewerContentHeight)
+            {
+                isScroll = true;
+                scrollVertical += (top - scrollViewerContentHeight) + ScrollStep;
+                top = scrollViewerContentHeight - ScrollStep;
+                if (scrollVertical > scrollViewer.ScrollableHeight) top += scrollVertical - scrollViewer.ScrollableHeight;
+            }
+
+            if (isScroll && readerImage.IsScrollingEnabled)
+            {
+                // scroll and count new pointer position
+                scrollViewer.ChangeView(scrollHorizontal, scrollVertical, null);
+            }
+            
             readerImage.m_cursorImage.Margin = new Thickness(left, top, 0, 0);
         }
+
+        
 
         protected override void OnPointerMoved(PointerRoutedEventArgs e)
         {
@@ -124,6 +158,10 @@ namespace ITJakub.MobileApps.Client.SynchronizedReading.View.Control
             if (Mode == Modes.Reader)
                 return;
 
+            var point2 = e.GetCurrentPoint(this).Position;
+            if (point2.X > ActualWidth - 18 || point2.Y > ActualHeight - 18)
+                return;
+            
             var point = e.GetCurrentPoint(m_sourceImage).Position;
             var zoom = m_scrollViewer.ZoomFactor;
             var x = (point.X - (PointerCorrectionX + PointerCalibrationX)/zoom) / m_sourceImage.ActualWidth;
