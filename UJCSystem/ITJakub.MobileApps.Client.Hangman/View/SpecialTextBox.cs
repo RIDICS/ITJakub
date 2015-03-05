1461f80492029ce3569460a8042a31fc84edfcf5
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2015-03-05 18:26:16 +0100

    Draft: textbox with special chars keyboard.

---
+++ UJCSystem/ITJakub.MobileApps.Client.Hangman/View/SpecialTextBox.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ITJakub.MobileApps.Client.Hangman.View
+{
+    public sealed class SpecialTextBox : Control
+    {
+        public SpecialTextBox()
+        {
+            DefaultStyleKey = typeof(SpecialTextBox);
+        }
+    }
+}
