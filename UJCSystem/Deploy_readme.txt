829eb688f435edae9b5692434d6558ee9c8397bd
honza minarik <minarja4@outlook.com>
2015-03-17 19:43:25 +0100

    Deployable on local IIS

---
+++ UJCSystem/Deploy_readme.txt
@@ -0,0 +1,2 @@
+1. Move *.container.config to root folder of deployed service
+2. Xquery GetDocument in existDB folder modules change resource path to actual project name in existDB
--- UJCSystem/ITJakub.Core/SearchService/SearchServiceClient.cs
+++ UJCSystem/ITJakub.Core/SearchService/SearchServiceClient.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.ServiceModel;
 using System.Threading.Tasks;
 using ITJakub.Shared.Contracts;
+using log4net;
 
 namespace ITJakub.Core.SearchService
 {
     public class SearchServiceClient : ClientBase<ISearchService>, ISearchService
     {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public async Task<string> GetBookPageByPositionAsync(string bookId, string versionId, int pagePosition, string transformationName)
         {
             return await Channel.GetBookPageByPositionAsync(bookId, versionId, pagePosition, transformationName);
@@ -24,7 +30,29 @@ namespace ITJakub.Core.SearchService
 
         public async Task<IList<BookPage>> GetBookPageListAsync(string bookId,string versionId)
         {
-            return await Channel.GetBookPageListAsync(bookId, versionId);
+            try
+            {
+                return await Channel.GetBookPageListAsync(bookId, versionId);
+            }
+            catch (CommunicationException ex)
+            {
+                if (m_log.IsErrorEnabled)
+                    m_log.ErrorFormat("{0} failed with: {1}", GetCurrentMethod(), ex);
+                throw;
+            }
+            catch (TimeoutException ex)
+            {
+                if (m_log.IsErrorEnabled)
+                    m_log.ErrorFormat("{0} failed with: {1}", GetCurrentMethod(), ex);
+                throw;
+            }
+            catch (ObjectDisposedException ex)
+            {
+                if (m_log.IsErrorEnabled)
+                    m_log.ErrorFormat("{0} failed with: {1}", GetCurrentMethod(), ex);
+                throw;
+            }
+
         }
 
         public async Task UploadVersionFileAsync(VersionResourceUploadContract versionResourceUploadContract)
@@ -40,6 +68,13 @@ namespace ITJakub.Core.SearchService
         public async Task UploadSharedFileAsync(ResourceUploadContract contract)
         {
             await Channel.UploadSharedFileAsync(contract);
+        }
+
+
+
+        private string GetCurrentMethod([CallerMemberName] string methodName = null)
+        {
+            return methodName;
         }
     }
 }
