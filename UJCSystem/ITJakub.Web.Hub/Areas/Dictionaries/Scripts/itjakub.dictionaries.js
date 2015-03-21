16e74c878d4d4be8a55505377c87f9f56a5df975
honza minarik <minarja4@gmail.com>
2015-03-21 18:41:30 +0100

    Sliders and checkboxes

--- UJCSystem/ITJakub.Web.Hub/Areas/Dictionaries/Scripts/itjakub.dictionaries.js
+++ UJCSystem/ITJakub.Web.Hub/Areas/Dictionaries/Scripts/itjakub.dictionaries.js
@@ -1,2 +1,13 @@
-﻿//window.onload = () => { alert("hello from dictionaries!"); }
+﻿$(".dictionary-select-more").click(function () {
+    var body = $(this).parents(".dictionary-select").children(".dictionary-select-body");
+    if (body.is(":hidden")) {
+        $(this).children().removeClass("glyphicon-chevron-down");
+        $(this).children().addClass("glyphicon-chevron-up");
+        body.slideDown();
+    } else {
+        $(this).children().removeClass("glyphicon-chevron-up");
+        $(this).children().addClass("glyphicon-chevron-down");
+        body.slideUp();
+    }
+});
 //# sourceMappingURL=itjakub.dictionaries.js.map
