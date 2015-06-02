514dfa5fd716cf8f5eb03c71763c5a35eae7a9dd
honza minarik <minarja4@outlook.com>
2015-06-02 18:13:49 +0200

    ExistManager book content draft

---
+++ UJCSystem/ITJakub.Shared.Contracts/BookContentItemContract.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ITJakub.Shared.Contracts
+{
+    [DataContract]
+    public class BookContentItemContract
+    {
+        [DataMember]
+        public string Text { get; set; }
+
+        [DataMember]
+        public string ReferredPageXmlId { get; set; }
+    }
+}
