97fb5cc22e9e20475bca29a00b382b0b1e8bc036
Martin Mudra <mudra.martin@gmail.com>
2015-06-30 13:01:56 +0200

    fixed container.

--- UJCSystem/ITJakub.MobileApps.Client.Books/ViewModel/ViewModelLocator.cs
+++ UJCSystem/ITJakub.MobileApps.Client.Books/ViewModel/ViewModelLocator.cs
@@ -12,9 +12,8 @@
   See http://www.galasoft.ch/mvvm
 */
 
-using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Ioc;
-using Microsoft.Practices.ServiceLocation;
+using ITJakub.MobileApps.Client.Books.ViewModel.SelectPage;
+using Microsoft.Practices.Unity;
 
 namespace ITJakub.MobileApps.Client.Books.ViewModel
 {
@@ -24,38 +23,14 @@ namespace ITJakub.MobileApps.Client.Books.ViewModel
     /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
-        public ViewModelLocator()
+        public SelectBookViewModel SelectBookViewModel
         {
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
-            SimpleIoc.Default.Register<MainViewModel>();
+            get { return Container.Current.Resolve<SelectBookViewModel>(); }
         }
 
-        public MainViewModel Main
+        public SelectPageViewModel SelectPageViewModel
         {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
-        
-        public static void Cleanup()
-        {
-            // TODO Clear the ViewModels
+            get { return Container.Current.Resolve<SelectPageViewModel>(); }
         }
     }
 }
