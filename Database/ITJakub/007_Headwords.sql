2470a9133942cf18b2fffefa24013a4896fbeb05
daliboris <boris@daliboris.cz>
2015-06-30 12:03:22 +0200

    Using nvarchar for headwords

--- Database/ITJakub/007_Headwords.sql
+++ Database/ITJakub/007_Headwords.sql
@@ -7,9 +7,9 @@ BEGIN TRAN
     (
 	   [Id] bigint IDENTITY(1,1) NOT NULL CONSTRAINT [PK_BookHeadword(Id)] PRIMARY KEY CLUSTERED,
 	   [BookVersion] bigint NOT NULL FOREIGN KEY REFERENCES [dbo].[BookVersion] (Id),
-	   [XmlEntryId] varchar(max) NOT NULL,
-	   [DefaultHeadword] varchar(255) NOT NULL,
-	   [Headword] varchar(255) NOT NULL,
+	   [XmlEntryId] varchar(255) NOT NULL,
+	   [DefaultHeadword] nvarchar(255) NOT NULL,
+	   [Headword] nvarchar(255) NOT NULL,
 	   [Visibility] smallint NOT NULL,
     )
 
