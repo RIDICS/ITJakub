7c56796bd289f6732b4e4bc87627fe3c4960e9e7
honza minarik <minarja4@gmail.com>
2015-07-08 17:35:46 +0200

    Search in text criteria with mockups

--- UJCSystem/ITJakub.Web.Hub/Areas/Dictionaries/Controllers/DictionariesController.cs
+++ UJCSystem/ITJakub.Web.Hub/Areas/Dictionaries/Controllers/DictionariesController.cs
@@ -110,5 +110,165 @@ namespace ITJakub.Web.Hub.Areas.Dictionaries.Controllers
             m_mainServiceClient.SearchByCriteria(wordListCriteriaContracts);
             return Json(new {}, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult SearchCriteriaMocked()
+        {
+            var title1 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Title,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        EndsWith = "Romanoru_",
+                        StartsWith = "_esta"
+                    },
+                    new WordCriteriaContract
+                    {
+                        StartsWith = "Sbírka",
+                        Contains = new List<string> {"založených", "na"},
+                        EndsWith = "legendách"
+                    }
+                }
+            };
+
+
+            var title2 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Title,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"Ge%", "%oman", "Ge"}
+                    },
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"al_žený"} //založených
+                    },
+                }
+            };
+
+            var editor1 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Editor,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        StartsWith = "Voleková"
+                    },
+                    new WordCriteriaContract
+                    {
+                        StartsWith = "Hanzová"
+                    }
+                }
+            };
+
+
+            var editor2 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Editor,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"Kateřina"}
+                    },
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"Barbora"}
+                    },
+                }
+            };
+
+            var fulltext1 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Fulltext,
+                Values = new List<WordCriteriaContract>
+                {
+                    //new WordCriteriaContract
+                    //{
+                    //    Contains = new List<string>{"T%s v Ř_mě"} // Titus v Římě    ---- Will this match if it's not single word?
+                    //},
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"p%st_n%"} // pústeník
+                    },
+                }
+            };
+
+            var fulltext2 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Fulltext,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        StartsWith = "Ti",
+                        Contains = new List<string> {"tu", "tus"},
+                        EndsWith = "s"
+                    },
+                }
+            };
+
+            var sentence1 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Sentence,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        StartsWith = "Ti",
+                        Contains = new List<string> {"tu", "tus"},
+                        EndsWith = "s"
+                    },
+                    new WordCriteriaContract
+                    {
+                         Contains = new List<string> {"zavinil"}
+                    }
+                }
+            };
+
+            var sentence2 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Sentence,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"prvorození"}
+                    },
+                }
+            };
+
+            var heading1 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Heading,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"lenosti"}
+                    },
+                }
+            };
+            
+            var heading2 = new WordListCriteriaContract
+            {
+                Key = CriteriaKey.Heading,
+                Values = new List<WordCriteriaContract>
+                {
+                    new WordCriteriaContract
+                    {
+                        Contains = new List<string> {"synóv"}
+                    },
+                }
+            };
+
+            var wordListCriteriaContracts = new List<WordListCriteriaContract>{title1, title2, editor1, editor2, fulltext1, fulltext2, sentence1, sentence2, heading1, heading2};
+            m_mainServiceClient.SearchByCriteria(wordListCriteriaContracts);
+            return Json(new {}, JsonRequestBehavior.AllowGet);
+        }
     }
 }
