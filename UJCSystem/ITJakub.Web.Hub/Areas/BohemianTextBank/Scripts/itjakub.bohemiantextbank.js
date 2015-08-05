f69dbf882df10d5c498245d3178cdf31ee367cb5
Martin Mudra <mudra.martin@hotmail.cz>
2015-08-05 18:25:09 +0200

    ignoring js files

--- UJCSystem/ITJakub.Web.Hub/Areas/BohemianTextBank/Scripts/itjakub.bohemiantextbank.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var searchBox = new SearchBox("#searchbox", "BohemianTextBank/BohemianTextBank");
-    searchBox.addDataSet("Title", "Názvy");
-    searchBox.addDataSet("Author", "Autoři");
-    searchBox.create();
-});
--- UJCSystem/ITJakub.Web.Hub/Scripts/Home/itjakub.home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    var searchBox = new SearchBox("#searchbox", "Home");
-    searchBox.addDataSet("Title", "Názvy");
-    searchBox.addDataSet("Author", "Autoři");
-    searchBox.addDataSet("DictionaryHeadword", "Slovníková hesla");
-    searchBox.create();
-});
