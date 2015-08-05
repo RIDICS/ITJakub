43b34ce974623975535cdcecb2dfa52b90a08fe2
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2015-08-05 19:20:14 +0200

    stop tracking maps, js, css

--- UJCSystem/ITJakub.Web.Hub/Areas/ProfessionalLiterature/Scripts/itjakub.professionalliterature.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var searchBox = new SearchBox("#searchbox", "ProfessionalLiterature/ProfessionalLiterature");
-    searchBox.addDataSet("Title", "Názvy");
-    searchBox.addDataSet("Author", "Autoři");
-    searchBox.create();
-});
