6391d36b07c1c1c2f9323c971f237bfab44d5690
Honza Minarik <minarja4@outlook.com>
2015-08-17 17:01:35 +0200

    Login return url

---
+++ UJCSystem/ITJakub.Web.Hub/Controllers/FeedbackController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace ITJakub.Web.Hub.Controllers
+{
+    [Authorize]
+    public class FeedbackController : Controller
+    {
+        private readonly ItJakubServiceClient m_mainServiceClient = new ItJakubServiceClient();
+        private readonly ItJakubServiceEncryptedClient m_mainServiceEncryptedClient = new ItJakubServiceEncryptedClient();
+
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+
+        public ActionResult GetFeedbacksCount()
+        {
+            return Json(0, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
