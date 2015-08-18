afec14f8534f28af06b4148721f0ebaf2ff422a1
Honza Minarik <minarja4@outlook.com>
2015-08-18 13:54:40 +0200

    Feedback layout improvements, display basic data

--- UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.feedbacks.ts
+++ UJCSystem/ITJakub.Web.Hub/Scripts/itjakub.feedbacks.ts
@@ -1,5 +1,33 @@
-﻿
+﻿//enum FeedbackCategory {
+//    None = 0,
+//    Dictionaries = 1,
+//    Editions = 2,
+//    BohemianTextBank = 3,
+//    OldGrammar = 4,
+//    ProfessionalLiterature = 5,
+//    Bibliographies = 6,
+//    CardFiles = 7,
+//    AudioBooks = 8,
+//    Tools = 9,
+//}
+
+var categoryTranslation = [
+    "Žádná",
+    "Slovníky",
+    "Edice",
+    "Korpusy",
+    "Mluvnice",
+    "Odborná literatura",
+    "Bibliografie",
+    "Kartotéky",
+    "Audioknihy",
+    "Pomůcky"
+];
+
 $(document).ready(() => {
+
+    var notFilledMessage = "<Nezadáno>";
+
 
     function deleteFeedback(feedbackId: string)
     {
@@ -47,24 +75,77 @@ $(document).ready(() => {
                 $(feedbackDiv).addClass("feedback");
                 feedbackDiv.id = actualFeedback["Id"];
 
-                var feedbackTextDiv = document.createElement("div");
-                $(feedbackTextDiv).addClass("feedback-text");
-                $(feedbackTextDiv).html(actualFeedback.Text);
+                var feedbackHeaderDiv = document.createElement("div");
+                $(feedbackHeaderDiv).addClass("feedback-header");
+
+                var name = "";
+                var email = "";
+                var signed = "";
+                var category = actualFeedback["Category"];
+
+                var user = actualFeedback["User"];
+                if (typeof user !== "undefined" && user !== null) {
+                    name = user["Name"];
+                    email = user["Email"];
+                    signed = "ano";
+                } else {
+                    name = actualFeedback["FilledName"];
+                    email = actualFeedback["FilledEmail"];
+                    signed = "ne";
+                }
+
+                if (typeof name === "undefined" || name === null || name === "") {
+                    name = notFilledMessage;
+                }
+
+                if (typeof email === "undefined" || email === null || email === "") {
+                    email = notFilledMessage;
+                }
+
+                var feedbackNameSpan = document.createElement("span");
+                $(feedbackNameSpan).addClass("feedback-name");
+                feedbackNameSpan.innerHTML = "Jméno: " + name;
+
+                feedbackHeaderDiv.appendChild(feedbackNameSpan);
+
+                var feedbackEmailSpan = document.createElement("span");
+                $(feedbackEmailSpan).addClass("feedback-email");
+                feedbackEmailSpan.innerHTML = "E-mail: " + email;
+
+                feedbackHeaderDiv.appendChild(feedbackEmailSpan);
+
+                var feedbackSignedUserSpan = document.createElement("span");
+                $(feedbackSignedUserSpan).addClass("feedback-signed");
+                feedbackSignedUserSpan.innerHTML = "Přihlášený uživatel: " + signed;
+
+                feedbackHeaderDiv.appendChild(feedbackSignedUserSpan);
+
+                var feedbackCategorySpan = document.createElement("span");
+                $(feedbackCategorySpan).addClass("feedback-category");
+                feedbackCategorySpan.innerHTML = "Kategorie: " + categoryTranslation[parseInt(category)];
+
+                feedbackHeaderDiv.appendChild(feedbackCategorySpan);
 
 
                 var feedbackDeleteButton = document.createElement("button");
                 feedbackDeleteButton.type = "button";
                 feedbackDeleteButton.innerHTML = "Smazat";
-                $(feedbackDeleteButton).addClass("feedback-delete-button");
+                $(feedbackDeleteButton).addClass("feedback-delete-button btn btn-default styled-button ");
 
                 $(feedbackDeleteButton).click((event: Event) => {
                     var elementId = $(event.target).parents(".feedback")[0].id;
                     deleteFeedback(elementId);
                 });
 
+                feedbackHeaderDiv.appendChild(feedbackDeleteButton);
 
+                var feedbackTextDiv = document.createElement("div");
+                $(feedbackTextDiv).addClass("feedback-text");
+                $(feedbackTextDiv).html(actualFeedback.Text);
+
+
+                feedbackDiv.appendChild(feedbackHeaderDiv);
                 feedbackDiv.appendChild(feedbackTextDiv);
-                feedbackDiv.appendChild(feedbackDeleteButton);
 
 
                 $(feedbacksContainer).append(feedbackDiv);
