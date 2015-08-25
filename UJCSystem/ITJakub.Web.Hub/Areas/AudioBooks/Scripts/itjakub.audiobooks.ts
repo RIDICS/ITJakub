aacc21de5fc832306cbc9bc1ee1ad6ee7d0cf5f9
Honza Minarik <minarja4@outlook.com>
2015-08-25 15:26:35 +0200

    Audiobooks audio length align

--- UJCSystem/ITJakub.Web.Hub/Areas/AudioBooks/Scripts/itjakub.audiobooks.ts
+++ UJCSystem/ITJakub.Web.Hub/Areas/AudioBooks/Scripts/itjakub.audiobooks.ts
@@ -23,6 +23,14 @@ function translateAudioType(audioType: number): string {
     return aduioTypeTranslation[audioType];
 }
 
+function fillLeadingZero(seconds: number): string {
+    var secondsString = seconds.toString();
+    if (secondsString.length === 1) {
+        secondsString = `0${secondsString}`;
+    }
+    return secondsString;
+}
+
 $(document).ready(() => {
     initAudibooksList();
 });
