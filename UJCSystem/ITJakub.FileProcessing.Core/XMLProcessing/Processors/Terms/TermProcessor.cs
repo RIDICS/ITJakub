b13e8f5a9d9753a6a191d5a8f169d15b304e9d8b
Honza Minarik <minarja4@outlook.com>
2015-08-25 17:13:09 +0200

    Term processors draft

---
+++ UJCSystem/ITJakub.FileProcessing.Core/XMLProcessing/Processors/Terms/TermProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Xml;
+using Castle.MicroKernel;
+using ITJakub.DataEntities.Database.Entities;
+using ITJakub.FileProcessing.Core.XMLProcessing.XSLT;
+using log4net;
+
+namespace ITJakub.FileProcessing.Core.XMLProcessing.Processors.Terms
+{
+    public class TermProcessor : ListProcessorBase
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public TermProcessor(XsltTransformationManager xsltTransformationManager, IKernel container) : base(xsltTransformationManager, container)
+        {
+        }
+
+        protected override string NodeName
+        {
+            get { return "term"; }
+        }
+
+        protected override void ProcessAttributes(BookVersion bookVersion, XmlReader xmlReader)
+        {
+            var termXmlId = xmlReader.GetAttribute("id");
+            var position = xmlReader.GetAttribute("n");
+            string text = GetInnerContentAsString(xmlReader);
+
+            //TODO create term
+        }
+    }
+}
---
+++ UJCSystem/ITJakub.FileProcessing.Core/XMLProcessing/Processors/Terms/TermsProcessor.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Castle.MicroKernel;
+using ITJakub.FileProcessing.Core.XMLProcessing.XSLT;
+
+namespace ITJakub.FileProcessing.Core.XMLProcessing.Processors.Terms
+{
+    public class TermsProcessor : ProcessorBase
+    {
+        public TermsProcessor(XsltTransformationManager xsltTransformationManager, IKernel container)
+            : base(xsltTransformationManager, container)
+        {
+        }
+
+        protected override string NodeName
+        {
+            get { return "terms"; }
+        }
+
+        protected override IEnumerable<ProcessorBase> SubProcessors
+        {
+            get
+            {
+                return new List<ProcessorBase>
+                {
+                    Container.Resolve<TermProcessor>()
+                };
+            }
+        }
+    }
+}
