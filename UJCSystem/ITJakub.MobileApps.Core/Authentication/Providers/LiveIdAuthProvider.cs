ec2e0e43485583d7be8bb976deb890b8c7095bec
Martin Mudra <mudrama1@fel.cvut.cz>
2015-09-09 20:00:07 +0200

    Live ID authentication draft.

--- UJCSystem/ITJakub.MobileApps.Core/Authentication/Providers/LiveIdAuthProvider.cs
+++ UJCSystem/ITJakub.MobileApps.Core/Authentication/Providers/LiveIdAuthProvider.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using ITJakub.MobileApps.DataContracts;
+using Microsoft.Live;
 using User = ITJakub.MobileApps.DataEntities.Database.Entities.User;
 
 namespace ITJakub.MobileApps.Core.Authentication.Providers
@@ -18,7 +20,20 @@ namespace ITJakub.MobileApps.Core.Authentication.Providers
 
         public AuthenticateResultInfo Authenticate(string accessToken, string email)
         {
-            throw new NotImplementedException();
+
+            LiveAuthClient client = new LiveAuthClient("0000000048122D4E");//HACK temp id
+            //LiveConnectClient cclient = new LiveConnectClient(client.Session);
+
+            LiveConnectSession session = Task.Run(() => client.ExchangeAuthCodeAsync(accessToken)).Result;
+            var cclient = new LiveConnectClient(session);
+
+            LiveOperationResult meRs = Task.Run(() => cclient.GetAsync("me")).Result;
+
+            var emails =  meRs.Result["wl.emails"];
+            var image = meRs.Result["wl.image"];
+
+            return new AuthenticateResultInfo {Result = AuthResultType.Success, UserImageLocation =(string) image};
+
         }
 
         
