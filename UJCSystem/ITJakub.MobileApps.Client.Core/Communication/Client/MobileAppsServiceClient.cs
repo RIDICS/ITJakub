c83db8b576e4dc6f276d79a8ea20c65b26b6cde2
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2015-09-14 17:08:05 +0200

    Default URLs to censeo2.felk.cvut.cz

--- UJCSystem/ITJakub.MobileApps.Client.Core/Communication/Client/MobileAppsServiceClient.cs
+++ UJCSystem/ITJakub.MobileApps.Client.Core/Communication/Client/MobileAppsServiceClient.cs
@@ -16,8 +16,9 @@ namespace ITJakub.MobileApps.Client.Core.Communication.Client
     public class MobileAppsServiceClient : ClientBase<IMobileAppsService>
     {
         //private const string EndpointAddress = "http://localhost/ITJakub.MobileApps.Service/MobileAppsService.svc";
-        private const string EndpointAddress = "http://147.32.81.136/ITJakub.MobileApps.Service/MobileAppsService.svc";
+        //private const string EndpointAddress = "http://147.32.81.136/ITJakub.MobileApps.Service/MobileAppsService.svc";
         //private const string EndpointAddress = "http://itjakubmobileapps.cloudapp.net/MobileAppsService.svc";
+        private const string EndpointAddress = "http://censeo2.felk.cvut.cz/ITJakub.MobileApps.Service/MobileAppsService.svc";
 
         private readonly ClientMessageInspector m_clientMessageInspector;
 
