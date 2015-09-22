5e42eb7783b47df11f4a414b8f47a707c581d12e
Martin Mudra <mudrama1@fel.cvut.cz>
2015-09-22 16:40:22 +0200

    hotfix for zipfile folders

--- Resources/BackupFeedbackAndUsers.sql
+++ Resources/BackupFeedbackAndUsers.sql
@@ -52,9 +52,6 @@ CREATE TABLE [dbo].[NewsSyndicationItem](
 SET IDENTITY_INSERT ITJakubDBBackup.[dbo].[User] ON
 
 
-GO
-
-
 INSERT INTO ITJakubDBBackup.[dbo].[User]
 (
     Id,
@@ -120,65 +117,3 @@ SET IDENTITY_INSERT ITJakubDBBackup.[dbo].[NewsSyndicationItem] OFF
 
 
 
-
-
-
---COPY BACK
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[User] ON
-
-
-INSERT INTO [ITJakubDB].[dbo].[User]
-(
-    Id,
-    [FirstName],
-    [LastName],
-    [Email],
-    [AuthenticationProvider],
-    [CommunicationToken],
-    [CommunicationTokenCreateTime],
-    [PasswordHash],
-    [Salt],
-    [CreateTime],
-    [AvatarUrl],
-    [UserName]
-)
-
-SELECT * FROM [ITJakubDBBackup].[dbo].[User]
-
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[User] OFF
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[Feedbacks] ON
-
-INSERT INTO [ITJakubDB].[dbo].[Feedbacks]
-(
-    [Id],
-    [FeedbackType],
-    [Text],
-    [CreateDate],
-    [Name],
-    [Email],
-    [User],
-    [BookHeadword],
-    [Category]
-)
-SELECT * FROM [ITJakubDBBackup].[dbo].[Feedbacks] f
-
-
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[Feedbacks] OFF
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[NewsSyndicationItem] ON
-
-
-INSERT INTO [ITJakubDB].[dbo].[NewsSyndicationItem]
-(
-     [Id],
-    [Title],
-    [CreateDate],
-    [Text],
-    [Url],
-    [ItemType],
-    [User]
-)
-SELECT * FROM [ITJakubDBBackup].[dbo].[NewsSyndicationItem]
-
-
-
-SET IDENTITY_INSERT [ITJakubDB].[dbo].[NewsSyndicationItem] OFF
