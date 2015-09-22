ca17a4d8fc33c242110c03338dc454165c0b75eb
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2015-09-22 20:05:15 +0200

    Commands on group list pages.

--- UJCSystem/ITJakub.MobileApps.Client.MainApp/ViewModel/GroupList/MyGroupListViewModel.cs
+++ UJCSystem/ITJakub.MobileApps.Client.MainApp/ViewModel/GroupList/MyGroupListViewModel.cs
@@ -1,6 +1,3 @@
-using System.Collections.ObjectModel;
-using Windows.UI.Xaml.Controls;
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using ITJakub.MobileApps.Client.Core.Manager.Groups;
 using ITJakub.MobileApps.Client.Core.Service;
@@ -10,14 +7,12 @@ using ITJakub.MobileApps.Client.Shared.Communication;
 
 namespace ITJakub.MobileApps.Client.MainApp.ViewModel.GroupList
 {
-    public class MyGroupListViewModel : ViewModelBase
+    public class MyGroupListViewModel : GroupListViewModelBase
     {
         private readonly IDataService m_dataService;
         private readonly INavigationService m_navigationService;
         private readonly IErrorService m_errorService;
-        private ObservableCollection<GroupInfoViewModel> m_groupList;
-        private bool m_isGroupListEmpty;
-        private bool m_loading;
+        
 
         public MyGroupListViewModel(IDataService dataService, INavigationService navigationService, IErrorService errorService)
         {
@@ -25,18 +20,22 @@ namespace ITJakub.MobileApps.Client.MainApp.ViewModel.GroupList
             m_navigationService = navigationService;
             m_errorService = errorService;
 
-            ConnectToGroupViewModel = new ConnectToGroupViewModel(dataService, errorService);
-
             InitCommands();
+            InitViewModels();
             LoadData();
         }
 
         private void InitCommands()
         {
             GoBackCommand = new RelayCommand(m_navigationService.GoBack);
-            GroupClickCommand = new RelayCommand<ItemClickEventArgs>(OpenGroup);
+            RefreshListCommand = new RelayCommand(LoadData);
         }
-        
+
+        private void InitViewModels()
+        {
+            ConnectToGroupViewModel = new ConnectToGroupViewModel(m_dataService, m_errorService, LoadData);
+        }
+
         private void LoadData()
         {
             Loading = true;
@@ -49,54 +48,23 @@ namespace ITJakub.MobileApps.Client.MainApp.ViewModel.GroupList
                     return;
                 }
 
-                GroupList = new ObservableCollection<GroupInfoViewModel>(groupList);
+                m_completeGroupList = groupList;
+                DisplayGroupList();
             });
         }
 
         public RelayCommand GoBackCommand { get; private set; }
-
-        public RelayCommand<ItemClickEventArgs> GroupClickCommand { get; set; }
-
+        
+        public RelayCommand RefreshListCommand { get; private set; }
+        
         public ConnectToGroupViewModel ConnectToGroupViewModel { get; set; }
 
-        public ObservableCollection<GroupInfoViewModel> GroupList
-        {
-            get { return m_groupList; }
-            set
-            {
-                m_groupList = value;
-                RaisePropertyChanged();
-                IsGroupListEmpty = m_groupList.Count == 0;
-            }
-        }
 
-        public bool IsGroupListEmpty
+        protected override void OpenGroup(GroupInfoViewModel group)
         {
-            get { return m_isGroupListEmpty; }
-            set
-            {
-                m_isGroupListEmpty = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public bool Loading
-        {
-            get { return m_loading; }
-            set
-            {
-                m_loading = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        
-        private void OpenGroup(ItemClickEventArgs args)
-        {
-            var group = args.ClickedItem as GroupInfoViewModel;
             if (group == null)
                 return;
-            
+
             //var ownCurrentGroup = m_ownedGroupIds.Contains(group.GroupId);
             //m_dataService.SetCurrentGroup(group.GroupId, ownCurrentGroup ? GroupType.Owner : GroupType.Member);
             m_dataService.SetCurrentGroup(group.GroupId, GroupType.Member); // TODO determine type
@@ -104,5 +72,6 @@ namespace ITJakub.MobileApps.Client.MainApp.ViewModel.GroupList
             m_navigationService.Navigate<ApplicationHostView>();
         }
 
+        public override bool IsTeacherView { get { return false; } }
     }
 }
