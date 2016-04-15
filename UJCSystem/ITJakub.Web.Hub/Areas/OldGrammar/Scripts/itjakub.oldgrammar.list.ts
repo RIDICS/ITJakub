0514105ca66dbff03a752685b75c5f69b9364b0e
Jan Trejbal <jan.trejbal@gmail.com>
2016-04-15 12:31:13 +0200

    Convert OldGrammar list into bohemiatextbank module inicializator

--- UJCSystem/ITJakub.Web.Hub/Areas/OldGrammar/Scripts/itjakub.oldgrammar.list.ts
+++ UJCSystem/ITJakub.Web.Hub/Areas/OldGrammar/Scripts/itjakub.oldgrammar.list.ts
@@ -1,263 +1,49 @@
-﻿
-
-var search: Search;
-
-$(document).ready(() => {
-
-    const urlSearchKey = "search";
-    const urlPageKey = "page";
-    const urlSelectionKey = "selected";
-    const urlSortAscKey = "sortAsc";
-    const urlSortCriteriaKey = "sortCriteria";
-
-    var readyForInit = false;
-    var notInitialized = true;
-
-    var booksCountOnPage = 5;
-
-    var bookIdsInQuery = new Array();
-    var categoryIdsInQuery = new Array();
-
-    var selectedBookIds = new Array();
-    var selectedCategoryIds = new Array();
-
-    var bibliographyModule: BibliographyModule;
-    var bookSelector: DropDownSelect2;
-
-    var initPage: number = null;
-
-    function initializeFromUrlParams() {
-        if (readyForInit && notInitialized) {
-
-            notInitialized = false;
-
-            var page = getQueryStringParameterByName(urlPageKey);
-
-            if (page) {
-                initPage = parseInt(page);
+﻿$(document).ready(() => {
+    const modulInicializator = new ModulInicializator({
+        base: {
+            url: {
+                searchKey: "search",
+                pageKey: "page",
+                selectionKey: "selected",
+                sortAscKey: "sortAsc",
+                sortCriteriaKey: "sortCriteria"
             }
+        },
+        bibliographyModule: {
+            resultsContainer: "#listResults",
+            sortBarContainer: "#listResultsHeader",
+            forcedBookType: BookTypeEnum.Grammar,
+            customConfigurationPath: "OldGrammar/OldGrammar/GetListConfiguration"
+        },
+        search: {
+            container: document.getElementById("listSearchDiv") as HTMLDivElement,
+            enabledOptions: [
+                SearchTypeEnum.Title,
+                SearchTypeEnum.Author,
+                SearchTypeEnum.Editor,
+                SearchTypeEnum.Dating
+            ]
+        },
+        searchBox: {
+            inputFieldElement: ".searchbar-input",
+            controllerPath: "OldGrammar/OldGrammar",
 
-            var sortedAsc = getQueryStringParameterByName(urlSortAscKey);
-            var sortCriteria = getQueryStringParameterByName(urlSortCriteriaKey);
-
-            if (sortedAsc && sortCriteria) {
-                bibliographyModule.setSortedAsc(sortedAsc === "true");
-                bibliographyModule.setSortCriteria(<SortEnum>(<any>(sortCriteria)));
+            dataSet: {
+                name: "Title",
+                groupHeader: "Název"
+            },
+            searchBoxInputSelector: ".searchbar-input.tt-input",
+            searchUrl: {
+                advanced: getBaseUrl() + "OldGrammar/OldGrammar/AdvancedSearchPaged",
+                text: getBaseUrl() + "OldGrammar/OldGrammar/TextSearchPaged",
+                textCount: getBaseUrl() + "OldGrammar/OldGrammar/TextSearchCount"
             }
-
-            var selected = getQueryStringParameterByName(urlSelectionKey);
-
-            var searched = getQueryStringParameterByName(urlSearchKey);
-            search.writeTextToTextField(searched);
-
-            if (selected) {
-                bookSelector.setStateFromUrlString(selected);
-            } else {
-                search.processSearch(); //if not explicitly selected 
-            }
-
-        } else if (!notInitialized) {
-            search.processSearch();
-        } else {
-            readyForInit = true;
+        },
+        dropDownSelect: {
+            dropDownSelectContainer: "#dropdownSelectDiv",
+            dataUrl: getBaseUrl() + "OldGrammar/OldGrammar/GetGrammarsWithCategories",
+            showStar: true
         }
-
-    }
-
-    function actualizeSelectedBooksAndCategoriesInQuery() {
-        bookIdsInQuery = selectedBookIds;
-        categoryIdsInQuery = selectedCategoryIds;
-    }
-
-    function hideTypeahead() {
-        $(".twitter-typeahead").find(".tt-menu").hide();
-    };
-
-    var typeaheadSearchBox = new SearchBox(".searchbar-input", "OldGrammar/OldGrammar");
-    typeaheadSearchBox.addDataSet("Title", "Název");
-    typeaheadSearchBox.create();
-    typeaheadSearchBox.value($(".searchbar-input.tt-input").val());
-
-    $(".searchbar-input.tt-input").change(() => {        //prevent clearing input value on blur() 
-        typeaheadSearchBox.value($(".searchbar-input.tt-input").val());
     });
-    
-    var callbackDelegate = new DropDownSelectCallbackDelegate();
-    callbackDelegate.selectedChangedCallback = (state: State) => {
-        selectedBookIds = new Array();
-
-        for (var i = 0; i < state.SelectedItems.length; i++) {
-            selectedBookIds.push(state.SelectedItems[i].Id);
-        }
-
-        selectedCategoryIds = new Array();
-
-        for (var i = 0; i < state.SelectedCategories.length; i++) {
-            selectedCategoryIds.push(state.SelectedCategories[i].Id);
-        }
-
-        var parametersUrl = DropDownSelect2.getUrlStringFromState(state);
-        typeaheadSearchBox.clearAndDestroy();
-        typeaheadSearchBox.addDataSet("Title", "Název", parametersUrl);
-        typeaheadSearchBox.create();
-        typeaheadSearchBox.value($(".searchbar-input.tt-input").val());
-    };
-    callbackDelegate.dataLoadedCallback = () => {
-        var selectedIds = bookSelector.getSelectedIds();
-        selectedBookIds = selectedIds.selectedBookIds;
-        selectedCategoryIds = selectedIds.selectedCategoryIds;
-        initializeFromUrlParams();
-    };
-
-    bookSelector = new DropDownSelect2("#dropdownSelectDiv", getBaseUrl() + "OldGrammar/OldGrammar/GetGrammarsWithCategories", true, callbackDelegate);
-    bookSelector.makeDropdown();
-
-
-    function advancedSearchPaged(json: string, pageNumber: number) {
-        hideTypeahead();
-        if (typeof json === "undefined" || json === null || json === "") return;
-
-        var start = (pageNumber - 1) * bibliographyModule.getBooksCountOnPage();
-        var count = bibliographyModule.getBooksCountOnPage();
-        var sortAsc = bibliographyModule.isSortedAsc();
-        var sortingEnum = bibliographyModule.getSortCriteria();
-
-        bibliographyModule.clearBooks();
-        bibliographyModule.showLoading();
-
-        $.ajax({
-            type: "GET",
-            traditional: true,
-            url: getBaseUrl() + "OldGrammar/OldGrammar/AdvancedSearchPaged",
-            data: { json: json, start: start, count: count, sortingEnum: sortingEnum, sortAsc: sortAsc, selectedBookIds: bookIdsInQuery, selectedCategoryIds: categoryIdsInQuery },
-            dataType: 'json',
-            contentType: 'application/json',
-            success: response => {
-                bibliographyModule.showBooks(response.books);
-                updateQueryStringParameter(urlSearchKey, json);
-                updateQueryStringParameter(urlPageKey, pageNumber);
-                updateQueryStringParameter(urlSortAscKey, bibliographyModule.isSortedAsc());
-                updateQueryStringParameter(urlSortCriteriaKey, bibliographyModule.getSortCriteria());
-            }
-        });
-    }
-
-    function basicSearchPaged(text: string, pageNumber: number) {
-        hideTypeahead();
-        //if (typeof text === "undefined" || text === null || text === "") return;
-
-        var start = (pageNumber - 1) * bibliographyModule.getBooksCountOnPage();
-        var count = bibliographyModule.getBooksCountOnPage();
-        var sortAsc = bibliographyModule.isSortedAsc();
-        var sortingEnum = bibliographyModule.getSortCriteria();
-
-        bibliographyModule.clearBooks();
-        bibliographyModule.showLoading();
-
-        $.ajax({
-            type: "GET",
-            traditional: true,
-            url: getBaseUrl() + "OldGrammar/OldGrammar/TextSearchPaged",
-            data: { text: text, start: start, count: count, sortingEnum: sortingEnum, sortAsc: sortAsc, selectedBookIds: bookIdsInQuery, selectedCategoryIds: categoryIdsInQuery },
-            dataType: 'json',
-            contentType: 'application/json',
-            success: response => {
-                bibliographyModule.showBooks(response.books);
-                updateQueryStringParameter(urlSearchKey, text);
-                updateQueryStringParameter(urlPageKey, pageNumber);
-                updateQueryStringParameter(urlSortAscKey, bibliographyModule.isSortedAsc());
-                updateQueryStringParameter(urlSortCriteriaKey, bibliographyModule.getSortCriteria());
-            }
-        });
-    }
-
-    function pageClickCallbackForBiblModule(pageNumber: number) {
-
-        if (search.isLastQueryJson()) {
-            advancedSearchPaged(search.getLastQuery(), pageNumber);
-        } else {
-            basicSearchPaged(search.getLastQuery(), pageNumber);
-        }
-    }
-
-
-    function sortOrderChanged() {
-        bibliographyModule.showPage(1);
-    }
-
-    bibliographyModule = new BibliographyModule("#listResults", "#listResultsHeader", sortOrderChanged, BookTypeEnum.Grammar, "OldGrammar/OldGrammar/GetListConfiguration");
-
-    function createPagination(booksCount: number) {
-        var pages = Math.ceil(booksCount / booksCountOnPage);
-        if (initPage && initPage <= pages) {
-            bibliographyModule.createPagination(booksCountOnPage, pageClickCallbackForBiblModule, booksCount, initPage);
-        } else {
-            bibliographyModule.createPagination(booksCountOnPage, pageClickCallbackForBiblModule, booksCount);
-        }
-    }
-
-    function basicSearch(text: string) {
-        hideTypeahead();
-        actualizeSelectedBooksAndCategoriesInQuery();
-        //if (typeof text === "undefined" || text === null || text === "") return;
-
-        bibliographyModule.clearBooks();
-        bibliographyModule.showLoading();
-
-        $.ajax({
-            type: "GET",
-            traditional: true,
-            url: getBaseUrl() + "OldGrammar/OldGrammar/TextSearchCount",
-            data: { text: text, selectedBookIds: bookIdsInQuery, selectedCategoryIds: categoryIdsInQuery },
-            dataType: 'json',
-            contentType: 'application/json',
-            success: response => {
-                createPagination(response["count"]); //enable pagination
-                updateQueryStringParameter(urlSearchKey, text);
-                updateQueryStringParameter(urlSelectionKey, DropDownSelect2.getUrlStringFromState(bookSelector.getState()));
-                updateQueryStringParameter(urlSortAscKey, bibliographyModule.isSortedAsc());
-                updateQueryStringParameter(urlSortCriteriaKey, bibliographyModule.getSortCriteria());
-            }
-        });
-    }
-
-    function advancedSearch(json: string) {
-        hideTypeahead();
-        if (typeof json === "undefined" || json === null || json === "") return;
-        actualizeSelectedBooksAndCategoriesInQuery();
-
-        bibliographyModule.clearBooks();
-        bibliographyModule.showLoading();
-
-        $.ajax({
-            type: "GET",
-            traditional: true,
-            url: getBaseUrl() + "OldGrammar/OldGrammar/AdvancedSearchResultsCount",
-            data: { json: json, selectedBookIds: bookIdsInQuery, selectedCategoryIds: categoryIdsInQuery },
-            dataType: 'json',
-            contentType: 'application/json',
-            success: response => {
-                createPagination(response["count"]); //enable pagination
-                updateQueryStringParameter(urlSearchKey, json);
-                updateQueryStringParameter(urlSelectionKey, DropDownSelect2.getUrlStringFromState(bookSelector.getState()));
-                updateQueryStringParameter(urlSortAscKey, bibliographyModule.isSortedAsc());
-                updateQueryStringParameter(urlSortCriteriaKey, bibliographyModule.getSortCriteria());
-
-            }
-        });
-    }
-   
-    var enabledOptions = new Array<SearchTypeEnum>();
-    enabledOptions.push(SearchTypeEnum.Title);
-    enabledOptions.push(SearchTypeEnum.Author);
-    enabledOptions.push(SearchTypeEnum.Editor);
-    enabledOptions.push(SearchTypeEnum.Dating);
-
-    search = new Search(<any>$("#listSearchDiv")[0], advancedSearch, basicSearch);
-    search.makeSearch(enabledOptions);
-
-    initializeFromUrlParams();
+    modulInicializator.init();
 });
-
-
-
