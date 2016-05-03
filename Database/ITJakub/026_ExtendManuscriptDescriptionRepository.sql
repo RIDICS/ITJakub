9c6d7701048d08600c3d222ddf461f887c0a85a1
Jan Trejbal <jan.trejbal@gmail.com>
2016-05-03 13:39:30 +0200

    Extend manuscript description repository

---
+++ Database/ITJakub/026_ExtendManuscriptDescriptionRepository.sql
@@ -0,0 +1,16 @@
+﻿SET XACT_ABORT ON;
+USE ITJakubDB;
+
+BEGIN TRAN;
+	ALTER TABLE [dbo].[ManuscriptDescription]
+	   ALTER COLUMN Idno VARCHAR(100) NULL;
+	ALTER TABLE [dbo].[ManuscriptDescription]
+	   ALTER COLUMN Repository VARCHAR(200) NULL;
+
+	INSERT INTO [dbo].[DatabaseVersion]
+		 ( DatabaseVersion )
+	VALUES
+		 ( '026' );
+	-- DatabaseVersion - varchar
+--ROLLBACK
+COMMIT;
