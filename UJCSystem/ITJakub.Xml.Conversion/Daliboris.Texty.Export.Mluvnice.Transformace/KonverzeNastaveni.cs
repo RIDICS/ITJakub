ed33bfaf96c63a59b30143579d61c1d49298b12f
Jan Trejbal <jan.trejbal@gmail.com>
2016-05-23 13:14:05 +0200

    Draft: import Mluvnice.transformation

---
+++ UJCSystem/ITJakub.Xml.Conversion/Daliboris.Texty.Export.Mluvnice.Transformace/KonverzeNastaveni.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+	public class KonverzeNastaveni
+	{
+
+		public KonverzeNastaveni(string strDocx, string strDoc2Xml, string strXml)
+		{
+			Docx = strDocx;
+			Doc2Xml = strDoc2Xml;
+			Xml = strXml;
+		}
+
+		public string Docx { get; set; }
+		public string Doc2Xml { get; set; }
+		public string Xml { get; set; }
+		public bool BezOdsazeni { get; set; }
+
+	}
+}
