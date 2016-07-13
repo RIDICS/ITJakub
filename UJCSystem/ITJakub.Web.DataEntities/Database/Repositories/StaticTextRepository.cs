48daae3091f25e4bf3f0a548a7663c77b1e0718f
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2016-07-13 10:21:34 +0200

    New project with database only for web application

---
+++ UJCSystem/ITJakub.Web.DataEntities/Database/Repositories/StaticTextRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Castle.Facilities.NHibernateIntegration;
+using Castle.Services.Transaction;
+using ITJakub.Web.DataEntities.Database.Daos;
+using ITJakub.Web.DataEntities.Database.Entities;
+using NHibernate;
+
+namespace ITJakub.Web.DataEntities.Database.Repositories
+{
+    [Transactional]
+    public class StaticTextRepository : NHibernateTransactionalDao
+    {
+        public StaticTextRepository(ISessionManager sessManager)
+            : base(sessManager)
+        {
+        }
+
+        [Transaction(TransactionMode.Requires)]
+        public virtual IList<StaticText> GetAllTexts()
+        {
+            using (ISession session = GetSession())
+            {
+                return session.QueryOver<StaticText>().List();
+            }
+        }
+    }
+}
