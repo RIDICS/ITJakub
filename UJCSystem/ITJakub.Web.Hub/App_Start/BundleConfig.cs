725a05a65245465ba4d25b7ac0a9d38f240dfe5a
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2016-09-13 18:07:23 +0200

    Create bundles for some scripts using Gulp

--- UJCSystem/ITJakub.Web.Hub/App_Start/BundleConfig.cs
+++ UJCSystem/ITJakub.Web.Hub/App_Start/BundleConfig.cs
@@ -8,74 +8,13 @@ namespace ITJakub.Web.Hub
         public static void RegisterBundles(BundleCollection bundles)
         {
             // ----- Scripts -----
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/wwwroot/lib/jquery/dist/jquery.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/wwwroot/lib/jquery-ui/jquery-ui.js"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/wwwroot/lib/jquery-validation/dist/jquery.validate.js",
                 "~/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryappear").Include(
-                "~/wwwroot/lib/jquery-appear/src/jquery.appear.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/wwwroot/lib/modernizr/modernizr.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/eucookies").Include(
-                "~/wwwroot/lib/jquery-eu-cookie-law-popup/js/jquery-eu-cookie-law-popup.js",
-                "~/wwwroot/js/Plugins/itjakub.eucookiepopup.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/wwwroot/lib/bootstrap/dist/js/bootstrap.js",
-                "~/wwwroot/lib/respond/dest/respond.src.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
-                "~/wwwroot/lib/dropzone/downloads/dropzone.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/typeahead").Include(
-                "~/wwwroot/lib-custom/typeahead.bundle.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/keyboardLayout").Include(
-                "~/wwwroot/js/Plugins/Keyboard/itjakub.plugins.keyboard.js",
-                "~/wwwroot/js/Plugins/Keyboard/itjakub.plugins.keyboardComponent.js",
-                "~/wwwroot/js/Plugins/Keyboard/itjakub.plugins.keyboardManager.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/storage").Include(
-                "~/wwwroot/js/Plugins/Storage/itjakub.plugins.storage.manager.js",
-                "~/wwwroot/js/Plugins/Storage/itjakub.plugins.storage.cookiestorage.js",
-                "~/wwwroot/js/Plugins/Storage/itjakub.plugins.storage.localstorage.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/imagezoom").Include(
-                "~/wwwroot/lib/Wheelzoom/wheelzoom.js",
-                "~/wwwroot/lib/jquery-zoom/jquery.zoom.js"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/simplemde").Include(
                 "~/wwwroot/lib/simplemde/dist/simplemde.min.js"));
-
-            bundles.Add(new ScriptBundle("~/itjakub/javascript").Include(
-                "~/wwwroot/js/itjakub.js",
-                "~/wwwroot/js/Plugins/Progress/itjakub.plugins.progress.js",
-                "~/wwwroot/js/Plugins/itjakub.modul.inicializator.js",
-                "~/wwwroot/js/Plugins/itjakub.list.modul.inicializator.js",
-                "~/wwwroot/js/Plugins/itjakub.search.modul.inicializator.js",
-                "~/wwwroot/js/Plugins/Reader/itjakub.plugins.reader.js",
-                "~/wwwroot/js/Plugins/Bibliography/itjakub.plugins.bibliography.variableInterpreter.js",
-                "~/wwwroot/js/Plugins/Bibliography/itjakub.plugins.bibliography.factories.js",
-                "~/wwwroot/js/Plugins/Bibliography/itjakub.plugins.bibliography.configuration.js",
-                "~/wwwroot/js/Plugins/Bibliography/itjakub.plugins.bibliography.js",
-                "~/wwwroot/js/Plugins/Sort/itjakub.plugins.sort.js",
-                "~/wwwroot/js/Plugins/DropdownSelect/itjakub.plugins.dropdownselect.js",
-                "~/wwwroot/js/Plugins/DropdownSelect/itjakub.plugins.dropdownselect2.js",
-                "~/wwwroot/js/Plugins/RegExSearch/itjakub.plugins.regexsearch.js",
-                "~/wwwroot/js/Plugins/itjakub.plugins.pagination.js",
-                "~/wwwroot/js/Plugins/SearchBox/itjakub.plugins.searchbox.js",
-                "~/wwwroot/js/Plugins/SearchBox/itjakub.plugins.singlesearchbox.js"));
 
             bundles.Add(new ScriptBundle("~/itjakub/home/javascript").Include(
                 "~/wwwroot/js/itjakub.home.js"));
--- UJCSystem/ITJakub.Web.Hub/gulpfile.js
+++ UJCSystem/ITJakub.Web.Hub/gulpfile.js
@@ -17,8 +17,10 @@ var paths = {
 
 paths.less = paths.webroot + "css/**/*.less";
 paths.css = paths.webroot + "css/**/*.css";
+paths.js = paths.webroot + "js/**/*.js";
 paths.areaLess = paths.webroot + "Areas/*/css/**/*.less";
 paths.areaCss = paths.webroot + "Areas/*/css/**/*.css";
+paths.areaJs = paths.webroot + "Areas/*/js/**/*.js";
 
 
 // Clean-up
@@ -31,7 +33,15 @@ gulp.task("clean:css_areas", function (cb) {
     rimraf(paths.areaCss, cb);
 });
 
-gulp.task("clean", ["clean:css", "clean:css_areas"]);
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.js, cb);
+});
+
+gulp.task("clean:js_areas", function (cb) {
+    rimraf(paths.areaJs, cb);
+});
+
+gulp.task("clean", ["clean:css", "clean:css_areas", "clean:js", "clean:js_areas"]);
 
 
 // Less build
@@ -55,6 +65,68 @@ gulp.task("build:less_areas", function () {
 gulp.task("build:less", ["build:less_root", "build:less_areas"]);
 
 
+// Bundle JavaScript
+
+gulp.task("bundle:itjakub", function () {
+    return gulp.src([
+            paths.webroot + "js/itjakub.js",
+            paths.webroot + "js/Plugins/itjakub.eucookiepopup.js"
+        ])
+        .pipe(concat("itjakub.bundle.js"))
+        //.pipe(uglify())
+        .pipe(gulp.dest(paths.webroot + "js/bundles"));
+});
+
+gulp.task("bundle:itjakub_plugins", function () {
+    return gulp.src([
+            paths.webroot + "js/itjakub.js",
+            paths.webroot + "js/Plugins/Progress/itjakub.plugins.progress.js",
+            paths.webroot + "js/Plugins/itjakub.modul.inicializator.js",
+            paths.webroot + "js/Plugins/itjakub.list.modul.inicializator.js",
+            paths.webroot + "js/Plugins/itjakub.search.modul.inicializator.js",
+            paths.webroot + "js/Plugins/Reader/itjakub.plugins.reader.js",
+            paths.webroot + "js/Plugins/Bibliography/itjakub.plugins.bibliography.variableInterpreter.js",
+            paths.webroot + "js/Plugins/Bibliography/itjakub.plugins.bibliography.factories.js",
+            paths.webroot + "js/Plugins/Bibliography/itjakub.plugins.bibliography.configuration.js",
+            paths.webroot + "js/Plugins/Bibliography/itjakub.plugins.bibliography.js",
+            paths.webroot + "js/Plugins/Sort/itjakub.plugins.sort.js",
+            paths.webroot + "js/Plugins/DropdownSelect/itjakub.plugins.dropdownselect.js",
+            paths.webroot + "js/Plugins/DropdownSelect/itjakub.plugins.dropdownselect2.js",
+            paths.webroot + "js/Plugins/RegExSearch/itjakub.plugins.regexsearch.js",
+            paths.webroot + "js/Plugins/itjakub.plugins.pagination.js",
+            paths.webroot + "js/Plugins/SearchBox/itjakub.plugins.searchbox.js",
+            paths.webroot + "js/Plugins/SearchBox/itjakub.plugins.singlesearchbox.js"
+        ])
+        .pipe(concat("itjakub.plugins.bundle.js"))
+        //.pipe(uglify())
+        .pipe(gulp.dest(paths.webroot + "js/bundles"));
+});
+
+gulp.task("bundle:itjakub_keyboard", function () {
+    return gulp.src([
+            paths.webroot + "js/Plugins/Keyboard/itjakub.plugins.keyboard.js",
+            paths.webroot + "js/Plugins/Keyboard/itjakub.plugins.keyboardComponent.js",
+            paths.webroot + "js/Plugins/Keyboard/itjakub.plugins.keyboardManager.js"
+        ])
+        .pipe(concat("itjakub.keyboard.bundle.js"))
+        //.pipe(uglify())
+        .pipe(gulp.dest(paths.webroot + "js/bundles"));
+});
+
+gulp.task("bundle:itjakub_storage", function () {
+    return gulp.src([
+            paths.webroot + "js/Plugins/Storage/itjakub.plugins.storage.manager.js",
+            paths.webroot + "js/Plugins/Storage/itjakub.plugins.storage.cookiestorage.js",
+            paths.webroot + "js/Plugins/Storage/itjakub.plugins.storage.localstorage.js"
+        ])
+        .pipe(concat("itjakub.storage.bundle.js"))
+        //.pipe(uglify())
+        .pipe(gulp.dest(paths.webroot + "js/bundles"));
+});
+
+gulp.task("bundlejs", ["bundle:itjakub", "bundle:itjakub_plugins", "bundle:itjakub_keyboard", "bundle:itjakub_storage"]);
+
+
 // Main build
 
-gulp.task("build", ["build:less"]);
+gulp.task("build", ["build:less", "bundlejs"]);
