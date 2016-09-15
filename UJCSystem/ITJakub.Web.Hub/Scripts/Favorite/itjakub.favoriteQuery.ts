7208fdbee0719e5c20207ba237223bbe92fdbe33
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2016-09-15 10:36:40 +0200

    Select favorite query view

--- UJCSystem/ITJakub.Web.Hub/Scripts/Favorite/itjakub.favoriteQuery.ts
+++ UJCSystem/ITJakub.Web.Hub/Scripts/Favorite/itjakub.favoriteQuery.ts
@@ -17,15 +17,25 @@
 
         var url = getBaseUrl() + "Favorite/GetFavoriteQueryPartial";
         this.renderLoading();
-        this.renderContainer.load(url);
+        this.renderContainer.load(url, null, (responseTxt, statusTxt, xhr) => {
+            if (statusTxt === "success") {
+                this.bindEvents();
+                this.isCreated = true;
+            }
+        });
     }
 
     private renderLoading() {
+        var innerContainerDiv = document.createElement("div");
+        $(innerContainerDiv).addClass("favorite-query");
+
         var loadingDiv = document.createElement("div");
         $(loadingDiv).addClass("loading");
-        $(loadingDiv).css("min-height", "100px"); // TODO use css styles
 
-        this.renderContainer.append(loadingDiv);
+        $(innerContainerDiv).append(loadingDiv);
+        
+        this.renderContainer.empty();
+        this.renderContainer.append(innerContainerDiv);
     }
 
     public isHidden(): boolean {
@@ -41,5 +51,36 @@
             this.make();
         }
         this.renderContainer.show();
+    }
+
+    private bindEvents() {
+        $(".favorite-query-label", this.renderContainer).click((event) => {
+            var elementJquery = $(event.currentTarget);
+            var labelId = elementJquery.data("id");
+            var labelName = elementJquery.data("name");
+            var labelColor = elementJquery.data("color");
+
+            $(".favorite-query-label-selected")
+                .data("id", labelId)
+                .text(labelName)
+                .css("background-color", labelColor);
+
+            this.filterQueryList(labelId);
+        });
+    }
+
+    private filterQueryList(labelId?: number) {
+        if (!labelId) {
+            $(".favorite-query-item").removeClass("hidden");
+        } else {
+            $(".favorite-query-item")
+                .addClass("hidden")
+                .each((index, element) => {
+                    var elementLabelId = $(element).data("label-id");
+                    if (elementLabelId === labelId) {
+                        $(element).removeClass("hidden");
+                    }
+                });
+        }
     }
 }
