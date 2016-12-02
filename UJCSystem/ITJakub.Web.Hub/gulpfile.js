c3b6eaafe43bd4ca4480ebb698773adc48ed8e28
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2016-12-02 11:30:01 +0100

    Auto-compile less after save

--- UJCSystem/ITJakub.Web.Hub/gulpfile.js
+++ UJCSystem/ITJakub.Web.Hub/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='default' Clean='clean' />
+﻿/// <binding AfterBuild='default' Clean='clean' ProjectOpened='watch:less' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -9,6 +9,7 @@ var gulp = require("gulp"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
   less = require("gulp-less"),
+  watch = require("gulp-watch"),
   sourcemaps = require('gulp-sourcemaps'),
   uglify = require("gulp-uglify"),
   ts = require("gulp-typescript");
@@ -76,6 +77,16 @@ gulp.task("build:less_areas", function () {
 });
 
 gulp.task("build:less", ["build:less_root", "build:less_areas"]);
+
+gulp.task("watch:less_root", function() {
+    return gulp.watch(paths.less, ["build:less_root"]);
+});
+
+gulp.task("watch:less_areas", function () {
+    return gulp.watch(paths.areaLess, ["build:less_areas"]);
+});
+
+gulp.task("watch:less", ["watch:less_root", "watch:less_areas"]);
 
 
 // TypeScript build
