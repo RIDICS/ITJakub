d382b4c5b0cb4b75a4e14bc1d85ddeae8a84c4af
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2017-01-02 11:38:16 +0100

    Fix shared project for referencing

--- UJCSystem/Vokabular.MainService.Core/ContainerInstaller.cs
+++ UJCSystem/Vokabular.MainService.Core/ContainerInstaller.cs
@@ -1,6 +1,12 @@
-﻿namespace Vokabular.MainService.Core
+﻿using Vokabular.Shared.Container;
+
+namespace Vokabular.MainService.Core
 {
-    public class ContainerInstaller
+    public class ContainerInstaller : IContainerInstaller
     {
+        public void Install(IContainer container)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
