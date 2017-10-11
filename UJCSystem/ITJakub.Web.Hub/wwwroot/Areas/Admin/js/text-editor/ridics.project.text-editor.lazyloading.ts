1dc0d1841c660f69cfb410e9377613b4dfe132d6
Denis Senchenko <senchden@fel.cvut.cz>
2017-10-11 18:26:41 +0200

    Add custom configuration to lazyloading library.

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/text-editor/ridics.project.text-editor.lazyloading.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/text-editor/ridics.project.text-editor.lazyloading.ts
@@ -6,11 +6,17 @@
     }
 
     lazyLoad() {
+        this.initConfig();
         $(".pages-start").on("lazybeforeunveil",
             (event) => {
                 var target = $(event.target);
                 var page = target.data("page");
                 this.pageStructure.createPage(page);
             });
+    }
+
+    initConfig() {
+        var lazyConfig: any = (window as any).lazySizesConfig;
+        lazyConfig.loadMode = 1;//only load visible elements
     }
 }
