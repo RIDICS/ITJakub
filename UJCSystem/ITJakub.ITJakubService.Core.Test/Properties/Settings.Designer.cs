89edb306eaa932575275db7336f79640d381f4b5
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2017-10-19 13:18:23 +0200

    Load connection strings from secret file in remaining projects

--- UJCSystem/ITJakub.ITJakubService.Core.Test/Properties/Settings.Designer.cs
+++ UJCSystem/ITJakub.ITJakubService.Core.Test/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace ITJakub.ITJakubService.Core.Test.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost;Database=ITJakubTestDB;User Id=admin;Password=***REMOVED***;")]
-        public string DefaultConnectionString {
-            get {
-                return ((string)(this["DefaultConnectionString"]));
-            }
-        }
     }
 }
