702509f7afc25ae058085ebdf9936f9445bfe789
Denis Senchenko <senchden@fel.cvut.cz>
2017-10-19 18:49:15 +0200

    Dialogs for successful and unsuccessful content save.

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/text-editor/ridics.project.text-editor.gui.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/text-editor/ridics.project.text-editor.gui.ts
@@ -46,4 +46,58 @@
             }
         });
     }
+
+    successfullySavedContent() {
+        const dialogEl = $("#save-status-dialog");
+        dialogEl.dialog({
+            resizable: false,
+            height: "auto",
+            width: 400,
+            modal: true,
+            dialogClass: "save-confirmation-dialogue",
+            title: "Success!",
+            buttons: {
+                Ok: function () {
+                    $(this).dialog("close");
+                }
+            },
+            open: (event, ui) => {
+                const targetElement = $(event.target);
+                targetElement.closest(".ui-dialog")
+                    .find(".ui-dialog-titlebar-close")
+                    .removeClass("ui-dialog-titlebar-close")
+                    .addClass("save-confirmation-dialogue-close-button")
+                    .html(
+                        "<i class=\"fa fa-times\" aria-hidden=\"true\"></i>"); //hack, because bootstrap breaks close button icon
+            }
+        });
+        dialogEl.text("Your changes have been successfully saved.");
+    }
+
+    saveContentUnsuccessfull() {
+        const dialogEl = $("#save-status-dialog");
+        dialogEl.dialog({
+            resizable: false,
+            height: "auto",
+            width: 400,
+            modal: true,
+            dialogClass: "save-confirmation-dialogue",
+            title: "Fail",
+            buttons: {
+                Ok: function () {
+                    $(this).dialog("close");
+                }
+            },
+            open: (event, ui) => {
+                const targetElement = $(event.target);
+                targetElement.closest(".ui-dialog")
+                    .find(".ui-dialog-titlebar-close")
+                    .removeClass("ui-dialog-titlebar-close")
+                    .addClass("save-confirmation-dialogue-close-button")
+                    .html(
+                        "<i class=\"fa fa-times\" aria-hidden=\"true\"></i>"); //hack, because bootstrap breaks close button icon
+            }
+        });
+        dialogEl.text("There was an error while saving your changes.");
+    }
 }
