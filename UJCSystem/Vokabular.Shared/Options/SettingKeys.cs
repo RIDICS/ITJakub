083018cfc2ce665f4ffdb23a18be17f90ce39fa6
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2017-10-19 13:18:23 +0200

    Load connection strings from secret file in remaining projects

--- UJCSystem/Vokabular.Shared/Options/SettingKeys.cs
+++ UJCSystem/Vokabular.Shared/Options/SettingKeys.cs
@@ -3,6 +3,9 @@
     public static class SettingKeys
     {
         public const string DefaultConnectionString = "DefaultConnectionString";
+        public const string MainConnectionString = "MainConnectionString";
+        public const string WebConnectionString = "WebConnectionString";
+        public const string TestDbConnectionString = "TestDbConnectionString";
         public const string ExistDbUser = "ExistDbUser";
         public const string ExistDbPassword = "ExistDbPassword";
         public const string CardFilesUser = "CardFilesUser";
