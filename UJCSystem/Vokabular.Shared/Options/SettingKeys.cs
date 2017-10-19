e036aabdb6400648b2fda31b032a3d9521ea4e54
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2017-10-19 10:53:22 +0200

    Move user credentials for eXistDB and Card files to secret file

--- UJCSystem/Vokabular.Shared/Options/SettingKeys.cs
+++ UJCSystem/Vokabular.Shared/Options/SettingKeys.cs
@@ -3,5 +3,9 @@
     public static class SettingKeys
     {
         public const string DefaultConnectionString = "DefaultConnectionString";
+        public const string ExistDbUser = "ExistDbUser";
+        public const string ExistDbPassword = "ExistDbPassword";
+        public const string CardFilesUser = "CardFilesUser";
+        public const string CardFilesPassword = "CardFilesPassword";
     }
 }
