79f68fc0e8f82b7390906a32cd28ba46530aa3e5
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-06 18:43:04 +0100

    Start implementing image viewer logic

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-image-viewer/ridics.project.page-image-viewer.main.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-image-viewer/ridics.project.page-image-viewer.main.ts
@@ -1,5 +1,14 @@
-﻿class ImageViewerMain {
-    init(projectId: number) {
+﻿///<reference path="./ridics.project.page-image-viewer.navigation.ts" />
+///<reference path="../editors-common-base/ridics.project.editors.util.ts" />
 
+class ImageViewerMain {
+    init(projectId: number) {
+        const util = new EditorsUtil();
+        const navigation = new ImageViewerPageNavigation();
+        const compositionPagesAjax = util.getPagesList(projectId);
+        compositionPagesAjax.done((pages: IParentPage[]) => {
+            navigation.init(pages);
+        });
+        compositionPagesAjax.fail(() => {});//TODO
     }
 }
