4db736d76c089abdc76a6418645b0348eed4d8a1
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-08 15:54:45 +0100

    Add class for GUI  with basic implementation of dialogs

---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-image-viewer/ridics.project.page-image-viewer.gui.ts
@@ -0,0 +1,48 @@
+﻿class ImageViewerPageGui {
+
+    init() {
+        this.createModalDialogContainer();
+    }
+
+    private createModalDialogContainer() {
+        $("#project-resource-images").append(`<div class="modals"></div>`);
+    }
+
+    private createModalDialogElement():JQuery {
+        const dialogText = `<div class="modal fade info-modal-dialog" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title info-dialog-title"></h4>
+      </div>
+      <div class="modal-body">
+        <p class="info-dialog-message"></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+        const html = $.parseHTML(dialogText);
+        const htmlEl = $(html);
+        htmlEl.hide();
+        $(".modals").append(htmlEl);
+        return htmlEl;
+    }
+
+    showInfoDialog(title: string, message: string) {
+        var dialogEl = $(".info-modal-dialog");
+        if (!dialogEl.length) {
+            dialogEl = this.createModalDialogElement();
+        }
+        const titleEl = dialogEl.find(".info-dialog-title");
+        const messageEl = dialogEl.find(".info-dialog-message");
+        titleEl.text(title);
+        messageEl.text(message);
+        dialogEl.modal("show");
+    }
+
+    private dialogFillStrings() {}
+}
