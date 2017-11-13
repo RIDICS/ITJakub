4e2bbf550a9d3d77800d1d5b48671d0c7fd115cc
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-13 18:38:04 +0100

    Renamed file

---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.util.ts
@@ -0,0 +1,16 @@
+﻿class KeyTableViewManager {
+    loadEditor(editorType: KeyTableEditorType) {
+        const url = `${getBaseUrl()}Admin/Project/KeyTableType`;
+        $("#project-layout-content").load(url, { editorType: editorType });
+    }
+
+    createNewGenre(name: string): JQueryXHR {
+        const url = `${getBaseUrl()}Admin/Project/CreateLiteraryGenre`;
+        const id = 0; //genre doesn't have an id yet
+        const payload: ILiteraryGenreContract = {
+            id: id,
+            name: name
+        };
+        return $.post(url, { request: payload });//TODO use plain ajax, returns 415 otherwise
+    }
+}
--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.view-manager.ts
+++
@@ -1,6 +0,0 @@
-﻿class KeyTableViewManager {
-    loadEditor(editorType: KeyTableEditorType) {
-        const url = `${getBaseUrl()}Admin/Project/KeyTableType`;
-        $("#project-layout-content").load(url, { editorType: editorType });
-    }
-}
