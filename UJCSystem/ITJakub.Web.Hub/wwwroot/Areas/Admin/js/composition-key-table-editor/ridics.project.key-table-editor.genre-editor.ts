625f23912248c74979252a65513a508cea67e511
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-14 14:58:58 +0100

    Implement pagination, logic for genre creation and rename.

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.genre-editor.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.genre-editor.ts
@@ -1,34 +1,55 @@
 ﻿class KeyTableGenreEditor {
     private readonly commonUtil: EditorsUtil;
     private readonly util: KeyTableViewManager;
+    private readonly gui: EditorsGui;
+    private genreItemList: IGenreResponseContract[];
+    private numberOfItemsPerPage = 28;
 
     constructor() {
         this.commonUtil = new EditorsUtil();//TODO move everything related to own util
         this.util = new KeyTableViewManager();
+        this.gui = new EditorsGui();
     }
 
     init() {
         this.commonUtil.getLitararyGenreList().done((data: IGenreResponseContract[]) => {
+            this.genreItemList = data;
             console.log(data);
-            const itemsOnPage = 2;//TODO debug
+            const itemsOnPage = this.numberOfItemsPerPage;
             this.initPagination(data.length, itemsOnPage);
-            const listEl = $(".selectable-list-div");
-            this.generateListStructure(data, listEl);
-            this.makeSelectable(listEl);
-            this.genreCreation();
+            this.loadPage(1);
+            this.genreRename();
+            this.genreDelete();
         });
+        this.genreCreation();
     }
 
     private initPagination(itemsCount: number, itemsOnPage: number) {
         const pagination = new Pagination({
             container: $(".key-table-pagination"),
-            pageClickCallback: function (pageNumber) {//TODO
+            pageClickCallback: (pageNumber) => {
+                this.loadPage(pageNumber);
             }
         });
         pagination.make(itemsCount, itemsOnPage);
     }
 
+    private loadPage(pageNumber: number) {
+        const listEl = $(".selectable-list-div");
+        const splitArray = this.splitGenreArray(this.genreItemList, pageNumber);
+        this.generateListStructure(splitArray, listEl);
+    }
+
+    private splitGenreArray(genreItemList: IGenreResponseContract[], page: number): IGenreResponseContract[] {
+        const numberOfListItemsPerPage = this.numberOfItemsPerPage;
+        const startIndex = (page - 1) * numberOfListItemsPerPage;
+        const endIndex = page * numberOfListItemsPerPage;
+        const splitArray = genreItemList.slice(startIndex, endIndex);
+        return splitArray;
+    }
+
     private generateListStructure(genreItemList: IGenreResponseContract[], jEl: JQuery) {
+        jEl.empty();
         const listStart = `<ul class="page-list">`;
         const listItemEnd = `</li>`;
         const listEnd = "</ul>";
@@ -43,6 +64,7 @@
         elm += listEnd;
         const html = $.parseHTML(elm);
         jEl.append(html);
+        this.makeSelectable(jEl);
     }
 
     private makeSelectable(jEl: JQuery) {
@@ -51,7 +73,52 @@
 
     private genreCreation() {
         $(".crud-buttons-div").on("click", ".create-new-genre", () => {
-            this.util.createNewGenre("New genre");//TODO add dialog
+            this.gui.showInputDialog("Name input", "Please input new genre name:");
+            $(".info-dialog-ok-button").on("click", () => {
+                const textareaEl = $(".input-dialog-textarea");
+                const genreString = textareaEl.val();
+                const newGenreAjax = this.util.createNewGenre(genreString);
+                newGenreAjax.done(() => {
+                    textareaEl.val("");
+                    this.gui.showInfoDialog("Success", "New genre has been created");
+                    $(".info-dialog-ok-button").off();
+                });
+                newGenreAjax.fail(() => {
+                    this.gui.showInfoDialog("Error", "New genre has not been created");
+                    $(".info-dialog-ok-button").off();
+                });
+            });
+        });
+    }
+
+    private genreRename() {
+        $(".crud-buttons-div").on("click", ".rename-genre", () => {
+            this.gui.showInputDialog("Name input", "Please input genre name after rename:");
+            $(".info-dialog-ok-button").on("click", () => {
+                const textareaEl = $(".input-dialog-textarea");
+                const genreString = textareaEl.val();
+                const selectedPageEl = $(".page-list").children(".ui-selected");
+                if (selectedPageEl.length) {
+                    const id = selectedPageEl.data("genre-id") as number;
+                    console.log(id);
+                    const renameAjax = this.util.renameGenre(genreString, id);
+                    renameAjax.done(() => {
+                        textareaEl.val("");
+                        this.gui.showInfoDialog("Success", "Genre has been renamed");
+                        $(".info-dialog-ok-button").off();
+                    });
+                    renameAjax.fail(() => {
+                        this.gui.showInfoDialog("Error", "Genre has not been renamed");
+                        $(".info-dialog-ok-button").off();
+                    });
+                }
+            });
+        });
+    }
+    private genreDelete() {
+        $(".crud-buttons-div").on("click", ".delete-genre", () => {
+            this.gui.showConfirmationDialog("Confirm", "Are you sure you want to delete this genre?");
+            //TODO add logic on ok button
         });
     }
 }
