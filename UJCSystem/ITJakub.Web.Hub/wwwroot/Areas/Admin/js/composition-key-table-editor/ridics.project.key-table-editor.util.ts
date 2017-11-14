8012da2f27d1afdcced948f731b09bc28046724f
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-14 14:52:13 +0100

    Implement create new genre function. Implement stub functions for renaming and deleting genres.

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.util.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/composition-key-table-editor/ridics.project.key-table-editor.util.ts
@@ -8,9 +8,41 @@
         const url = `${getBaseUrl()}Admin/Project/CreateLiteraryGenre`;
         const id = 0; //genre doesn't have an id yet
         const payload: ILiteraryGenreContract = {
-            id: id,
-            name: name
+            name: name,
+            id: id
         };
-        return $.post(url, { request: payload });//TODO use plain ajax, returns 415 otherwise
+        return $.ajax({
+            type: "POST",
+            url: url,
+            data: JSON.stringify(payload),
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8"
+        });
+    }
+
+    renameGenre(name: string, id:number): JQueryXHR {
+        const url = `${getBaseUrl()}Admin/Project/CreateLiteraryGenre`;//TODO change address
+        const payload: ILiteraryGenreContract = {
+            name: name,
+            id: id
+        };
+        return $.ajax({
+            type: "POST",//TODO needs PUT
+            url: url,
+            data: JSON.stringify(payload),
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8"
+        });
+    }
+
+    deleteGenre(id: number): JQueryXHR {
+        const url = `${getBaseUrl()}Admin/Project/CreateLiteraryGenre`;//TODO change address
+        return $.ajax({
+            type: "POST",//TODO needs DELETE
+            url: url,
+            data: JSON.stringify(id),
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8"
+        });
     }
 }
