0fb02b131ff412f861818cb4e0f14dc18f03a42c
Denis Senchenko <senchden@fel.cvut.cz>
2017-11-14 14:51:01 +0100

    Add methods for text input dialog and confirmation dialog.

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/editors-common-base/ridics.project.editors.gui.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/editors-common-base/ridics.project.editors.gui.ts
@@ -8,4 +8,22 @@
         messageEl.text(message);
         dialogEl.modal("show");
     }
+
+    showInputDialog(title: string, message: string) {
+        const dialogEl = $(".input-modal-dialog");
+        const titleEl = dialogEl.find(".input-dialog-title");
+        const messageEl = dialogEl.find(".input-dialog-message");
+        titleEl.text(title);
+        messageEl.text(message);
+        dialogEl.modal("show");
+    }
+
+    showConfirmationDialog(title: string, message: string) {
+        const dialogEl = $(".confirmation-modal-dialog");
+        const titleEl = dialogEl.find(".confirmation-dialog-title");
+        const messageEl = dialogEl.find(".confirmation-dialog-message");
+        titleEl.text(title);
+        messageEl.text(message);
+        dialogEl.modal("show");
+    }
 }
