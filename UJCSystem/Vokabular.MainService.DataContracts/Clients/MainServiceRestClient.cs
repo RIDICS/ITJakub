9e8da8afc36480f0dc145998e5b2e852b2287104
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2017-11-23 11:45:08 +0100

    Communication client implementation for favorite items

--- UJCSystem/Vokabular.MainService.DataContracts/Clients/MainServiceRestClient.cs
+++ UJCSystem/Vokabular.MainService.DataContracts/Clients/MainServiceRestClient.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
 using Vokabular.MainService.DataContracts.Contracts;
+using Vokabular.MainService.DataContracts.Contracts.Favorite;
 using Vokabular.MainService.DataContracts.Contracts.Search;
 using Vokabular.MainService.DataContracts.Contracts.Type;
 using Vokabular.RestClient;
@@ -13,6 +14,7 @@ using Vokabular.RestClient.Extensions;
 using Vokabular.RestClient.Results;
 using Vokabular.Shared;
 using Vokabular.Shared.DataContracts.Types;
+using Vokabular.Shared.DataContracts.Types.Favorite;
 using Vokabular.Shared.Extensions;
 
 namespace Vokabular.MainService.DataContracts.Clients
@@ -1136,5 +1138,301 @@ namespace Vokabular.MainService.DataContracts.Clients
                 throw;
             }
         }
+
+
+        #region Favorite items
+
+        public List<FavoriteLabelContract> GetFavoriteLabelList(int? count)
+        {
+            try
+            {
+                var result = Get<List<FavoriteLabelContract>>(UrlQueryBuilder.Create("favorite/label").AddParameter("count", count).ToQuery());
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public long CreateFavoriteLabel(FavoriteLabelContractBase data)
+        {
+            try
+            {
+                var result = Post<long>("favorite/label", data);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public void UpdateFavoriteLabel(long favoriteLabelId, FavoriteLabelContractBase data)
+        {
+            try
+            {
+                Put<object>($"favorite/label/{favoriteLabelId}", data);
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public void DeleteFavoriteLabel(long favoriteLabelId)
+        {
+            try
+            {
+                Delete($"favorite/label/{favoriteLabelId}");
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public PagedResultList<FavoriteBaseInfoContract> GetFavoriteItems(int start, int count, long? filterByLabelId, FavoriteTypeEnumContract? filterByType, string filterByTitle, FavoriteSortEnumContract? sort)
+        {
+            try
+            {
+                var url = UrlQueryBuilder.Create("favorite")
+                    .AddParameter("start", start)
+                    .AddParameter("count", count)
+                    .AddParameter("filterByLabelId", filterByLabelId)
+                    .AddParameter("filterByType", filterByType)
+                    .AddParameter("filterByTitle", filterByTitle)
+                    .AddParameter("sort", sort)
+                    .ToQuery();
+                    
+                var result = GetPagedList<FavoriteBaseInfoContract>(url);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public PagedResultList<FavoriteQueryContract> GetFavoriteQueries(int start, int count, long? filterByLabelId, BookTypeEnumContract? bookType, QueryTypeEnumContract? queryType, string filterByTitle)
+        {
+            try
+            {
+                var url = UrlQueryBuilder.Create("favorite/query")
+                    .AddParameter("start", start)
+                    .AddParameter("count", count)
+                    .AddParameter("filterByLabelId", filterByLabelId)
+                    .AddParameter("bookType", bookType)
+                    .AddParameter("queryType", queryType)
+                    .AddParameter("filterByTitle", filterByTitle)
+                    .ToQuery();
+
+                var result = GetPagedList<FavoriteQueryContract>(url);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public List<FavoritePageContract> GetPageBookmarks(long projectId)
+        {
+            try
+            {
+                var result = Get<List<FavoritePageContract>>($"favorite/page?projectId={projectId}");
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public FavoriteFullInfoContract GetFavoriteItem(long favoriteId)
+        {
+            try
+            {
+                var result = Get<FavoriteFullInfoContract>($"favorite/{favoriteId}/detail");
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public void UpdateFavorite(long favoriteId, UpdateFavoriteContract data)
+        {
+            try
+            {
+                Put<object>($"favorite/{favoriteId}", data);
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public void DeleteFavorite(long favoriteId)
+        {
+            try
+            {
+                Delete($"favorite/{favoriteId}");
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public List<FavoriteBookGroupedContract> GetFavoriteLabeledBooks(IList<long> projectIds, BookTypeEnumContract? bookType)
+        {
+            try
+            {
+                var url = UrlQueryBuilder.Create("favorite/book/grouped")
+                    .AddParameterList("projectIds", projectIds)
+                    .AddParameter("bookType", bookType)
+                    .ToQuery();
+
+                var result = Get<List<FavoriteBookGroupedContract>>(url);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public List<FavoriteCategoryGroupedContract> GetFavoriteLabeledCategories()
+        {
+            try
+            {
+                var result = Get<List<FavoriteCategoryGroupedContract>>("favorite/category/grouped");
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public List<FavoriteLabelWithBooksAndCategories> GetFavoriteLabelsWithBooksAndCategories(BookTypeEnumContract? bookType)
+        {
+            try
+            {
+                var result = Get<List<FavoriteLabelWithBooksAndCategories>>("favorite/label/with-books-and-categories");
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public long CreateFavoriteBook(CreateFavoriteProjectContract data)
+        {
+            try
+            {
+                var result = Post<long>("favorite/book", data);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public long CreateFavoriteCategory(CreateFavoriteCategoryContract data)
+        {
+            try
+            {
+                var result = Post<long>("favorite/category", data);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        public long CreateFavoriteQuery(CreateFavoriteQueryContract data)
+        {
+            try
+            {
+                var result = Post<long>("favorite/query", data);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+        
+        public long CreateFavoritePage(CreateFavoritePageContract data)
+        {
+            try
+            {
+                var result = Post<long>("favorite/page", data);
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                if (m_logger.IsErrorEnabled())
+                    m_logger.LogError("{0} failed with {1}", GetCurrentMethod(), e);
+
+                throw;
+            }
+        }
+
+        #endregion
     }
 }
