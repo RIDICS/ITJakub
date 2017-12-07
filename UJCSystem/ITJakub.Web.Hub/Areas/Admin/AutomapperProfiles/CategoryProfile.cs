2f37296b0d0174f591640721efb017c9dee823d6
Denis Senchenko <senchden@fel.cvut.cz>
2017-12-07 11:35:25 +0100

    Add missing Category mapping profile

---
+++ UJCSystem/ITJakub.Web.Hub/Areas/Admin/AutomapperProfiles/CategoryProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Vokabular.MainService.DataContracts.Contracts;
+
+namespace ITJakub.Web.Hub.Areas.Admin.AutomapperProfiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryContract, int>().ConvertUsing(src => src.Id);
+        }
+    }
+}
