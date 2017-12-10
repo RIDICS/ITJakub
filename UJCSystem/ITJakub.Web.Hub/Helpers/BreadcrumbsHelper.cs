a81b98d8933e66a0ae365005c47664078657391e
Jiří Poláček <polacji7@fel.cvut.cz>
2017-12-10 18:12:33 +0100

    Localized Helpers/BreadcrumbsHelper

--- UJCSystem/ITJakub.Web.Hub/Helpers/BreadcrumbsHelper.cs
+++ UJCSystem/ITJakub.Web.Hub/Helpers/BreadcrumbsHelper.cs
@@ -6,12 +6,26 @@ namespace ITJakub.Web.Hub.Helpers
 {
     public static class BreadcrumbsHelper
     {
-        public static HtmlString Create(params string[] path)
+        public static HtmlString Create(params string[] path, string cultureName = null)
         {
+            IAutoLocalizationManager localizer = Localization.CoreLibrary.Localization.Translator;
+            if (string.IsNullOrEmpty(cultureName))
+            {
+                cultureName = localizer.DefaultCulture().ToString();
+            }
+
+            CultureInfo cultureInfo = new CultureInfo(cultureName);
+            string appNameLocalized = localizer.Translate(LocTranslationSource.File, "VokabulářWebový", cultureInfo, "global");
+
+
             var sb = new StringBuilder();
             sb.Append("<ul class=\"breadcrumb\">");
 
-            sb.Append("<li><a href=\"/\">Vokabulář webový</a></li> ");
+            //sb.Append("<li><a href=\"/\">Vokabulář webový</a></li> ");
+            sb.Append("<li><a href=\"/\">");
+            sb.Append(appNameLocalized);
+            sb.Append("</a></li> ");
+
             for (int i = 0; i < path.Length - 1; i++)
             {
                 sb.AppendFormat("<li><a href=\"#\">{0}</a></li> ", path[i]);
