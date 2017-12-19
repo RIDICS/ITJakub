28c0709d620de580b7cf446b697f04bff27a5c80
Denis Senchenko <senchden@fel.cvut.cz>
2017-12-19 17:07:45 +0100

    Style forms in feedback page, show error messages

---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/js/ridics.form-validation.ts
@@ -0,0 +1,21 @@
+﻿$(document).ready(() => {
+    $("input[type=submit]").click((event) => {
+        const targetEl = $(event.target);
+        const formEl = targetEl.closest("form");
+        if (formEl.length) {
+            const validator: JQueryValidation.Validator = formEl.data("validator");
+            if (validator) {
+                validator.settings.highlight = ((element) => {
+                    const jEl = $(element);
+                    const formGroup = jEl.closest(".form-group");
+                    formGroup.removeClass("has-success").addClass("has-error");
+                });
+                validator.settings.unhighlight = ((element) => {
+                    const jEl = $(element);
+                    const formGroup = jEl.closest(".form-group");
+                    formGroup.removeClass("has-error").addClass("has-success");
+                });
+            }
+        }
+    });
+});
