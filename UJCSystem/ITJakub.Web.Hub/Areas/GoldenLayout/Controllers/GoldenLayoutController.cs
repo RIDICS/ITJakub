9f8ae09b963beeef542ff0a8e184bd66dd0b9b49
Jan Vitha <vithajan@gmail.com>
2018-01-09 11:20:44 +0100

    testing deployment of golden layout, changed address of main service for testing to censeo2.felk.cvut.cz

---
+++ UJCSystem/ITJakub.Web.Hub/Areas/GoldenLayout/Controllers/GoldenLayoutController.cs
@@ -0,0 +1,354 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ITJakub.Web.Hub.Controllers;
+using ITJakub.Web.Hub.Converters;
+using ITJakub.Web.Hub.Core;
+using ITJakub.Web.Hub.Core.Communication;
+using ITJakub.Web.Hub.Core.Managers;
+using ITJakub.Web.Hub.Models;
+using ITJakub.Web.Hub.Models.Plugins.RegExSearch;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Vokabular.MainService.DataContracts.Contracts.Search;
+using Vokabular.MainService.DataContracts.Contracts.Type;
+using Vokabular.Shared.DataContracts.Search.Corpus;
+using Vokabular.Shared.DataContracts.Search.Criteria;
+using Vokabular.Shared.DataContracts.Search.CriteriaItem;
+using Vokabular.Shared.DataContracts.Types;
+using HitSettingsContract = Vokabular.Shared.DataContracts.Search.OldCriteriaItem.HitSettingsContract;
+
+namespace ITJakub.Web.Hub.Areas.Editions.Controllers
+{
+    [Area("GoldenLayout")]
+    public class GoldenLayoutController : AreaController
+    {
+        private readonly StaticTextManager m_staticTextManager;
+        private readonly FeedbacksManager m_feedbacksManager;
+
+        public GoldenLayoutController(StaticTextManager staticTextManager, FeedbacksManager feedbacksManager, CommunicationProvider communicationProvider) : base(communicationProvider)
+        {
+            m_staticTextManager = staticTextManager;
+            m_feedbacksManager = feedbacksManager;
+        }
+
+        protected override BookTypeEnumContract AreaBookType => BookTypeEnumContract.Edition;
+        
+        private FeedbackFormIdentification GetFeedbackFormIdentification()
+        {
+            return new FeedbackFormIdentification { Area = "GoldenLayout", Controller = "GoldenLayout" };
+        }
+
+        public ActionResult GetListConfiguration()
+        {
+            var fullPath = "~/Areas/Editions/content/BibliographyPlugin/list_configuration.json";
+            return File(fullPath, "application/json", fullPath);
+        }
+
+        public ActionResult GetSearchConfiguration()
+        {
+            var fullPath = "~/Areas/Editions/content/BibliographyPlugin/search_configuration.json";
+            return File(fullPath, "application/json", fullPath);
+        }
+
+
+        #region Views and Feedback
+        /*
+        // GET: Editions/Editions
+        public ActionResult Index()
+        {
+            return View("List");
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+        */
+        // TODO rename parameter page to pageId
+        public ActionResult Listing(long bookId, string searchText, string page)
+        {
+            using (var client = GetRestClient())
+            {
+                var book = client.GetBookInfo(bookId);
+                var pages = client.GetBookPageList(bookId);
+                return
+                    View(new BookListingModel
+                    {
+                        BookId = book.Id,
+                        BookXmlId = book.Id.ToString(), // TODO remove this property
+                        VersionXmlId = null, // TODO replace this property with snapshot ID
+                        BookTitle = book.Title,
+                        BookPages = pages,
+                        SearchText = searchText,
+                        InitPageXmlId = page, // TODO rename to InitPageId
+                        CanPrintEdition = User.IsInRole("CanEditionPrint"),
+                        JsonSerializerSettingsForBiblModule = GetJsonSerializerSettingsForBiblModule()
+                    });
+            }
+        }
+        /*
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            var pageStaticText = m_staticTextManager.GetRenderedHtmlText(StaticTexts.TextEditionInfo);
+            return View(pageStaticText);
+        }
+
+        public ActionResult Feedback()
+        {
+            var viewModel = m_feedbacksManager.GetBasicViewModel(GetFeedbackFormIdentification(), StaticTexts.TextHomeFeedback, GetUserName());
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Feedback(FeedbackViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                m_feedbacksManager.FillViewModel(model, StaticTexts.TextHomeFeedback, GetFeedbackFormIdentification());
+                return View(model);
+            }
+
+            m_feedbacksManager.CreateFeedback(model, FeedbackCategoryEnumContract.Editions, IsUserLoggedIn());
+            return View("Feedback/FeedbackSuccess");
+        }
+
+        public ActionResult Help()
+        {
+            var pageStaticText = m_staticTextManager.GetRenderedHtmlText(StaticTexts.TextEditionHelp);
+            return View(pageStaticText);
+        }
+
+        public ActionResult EditionPrinciples()
+        {
+            var pageStaticText = m_staticTextManager.GetRenderedHtmlText(StaticTexts.TextEditionPrinciples);
+            return View(pageStaticText);
+        }
+        */
+
+        #endregion
+
+        public ActionResult GetEditionsWithCategories()
+        {
+            var result = GetBooksAndCategories();
+            return Json(result);
+        }
+
+
+        #region Search book
+
+        public ActionResult AdvancedSearchResultsCount(string json, IList<long> selectedBookIds, IList<int> selectedCategoryIds)
+        {
+            var count = SearchByCriteriaJsonCount(json, selectedBookIds, selectedCategoryIds);
+            return Json(new {count});
+        }
+
+        public ActionResult AdvancedSearchPaged(string json, int start, int count, short sortingEnum, bool sortAsc, IList<long> selectedBookIds,
+            IList<int> selectedCategoryIds)
+        {
+            var result = SearchByCriteriaJson(json, start, count, sortingEnum, sortAsc, selectedBookIds, selectedCategoryIds);
+            return Json(new { books = result }, GetJsonSerializerSettingsForBiblModule());
+        }
+
+        public ActionResult TextSearchCount(string text, IList<long> selectedBookIds, IList<int> selectedCategoryIds)
+        {
+            var count = SearchByCriteriaTextCount(CriteriaKey.Title, text, selectedBookIds, selectedCategoryIds);
+            return Json(new {count});
+        }
+
+        public ActionResult TextSearchFulltextCount(string text, IList<long> selectedBookIds, IList<int> selectedCategoryIds)
+        {
+            var count = SearchByCriteriaTextCount(CriteriaKey.Fulltext, text, selectedBookIds, selectedCategoryIds);
+            return Json(new {count});
+        }
+
+        public ActionResult TextSearchPaged(string text, int start, int count, short sortingEnum, bool sortAsc, IList<long> selectedBookIds,
+            IList<int> selectedCategoryIds)
+        {
+            var result = SearchByCriteriaText(CriteriaKey.Title, text, start, count, sortingEnum, sortAsc, selectedBookIds, selectedCategoryIds);
+            return Json(new { books = result }, GetJsonSerializerSettingsForBiblModule());
+        }
+
+        public ActionResult TextSearchFulltextPaged(string text, int start, int count, short sortingEnum, bool sortAsc, IList<long> selectedBookIds,
+            IList<int> selectedCategoryIds)
+        {
+            var results = SearchByCriteriaText(CriteriaKey.Fulltext, text, start, count, sortingEnum, sortAsc, selectedBookIds, selectedCategoryIds);
+            return Json(new { books = results }, GetJsonSerializerSettingsForBiblModule());
+        }
+
+        #endregion
+
+        #region Search in book
+
+        public ActionResult AdvancedSearchInBookPaged(string json, int start, int count, string bookXmlId, string versionXmlId)
+        {
+            var deserialized = JsonConvert.DeserializeObject<IList<ConditionCriteriaDescriptionBase>>(json, new ConditionCriteriaDescriptionConverter());
+            var listSearchCriteriaContracts = Mapper.Map<IList<SearchCriteriaContract>>(deserialized);
+
+            listSearchCriteriaContracts.Add(new ResultCriteriaContract
+            {
+                Start = 0,
+                Count = 1,
+                HitSettingsContract = new HitSettingsContract
+                {
+                    ContextLength = 45,
+                    Count = count,
+                    Start = start
+                }
+            });
+
+            listSearchCriteriaContracts.Add(new ResultRestrictionCriteriaContract
+            {
+                ResultBooks = new List<BookVersionPairContract> {new BookVersionPairContract {Guid = bookXmlId, VersionId = versionXmlId}}
+            });
+            using (var client = GetMainServiceClient())
+            {
+                var result = client.SearchByCriteria(listSearchCriteriaContracts).FirstOrDefault();
+                if (result != null)
+                {
+                    return Json(new {results = result.Results}, GetJsonSerializerSettingsForBiblModule());
+                }
+
+                return Json(new {results = new PageResultContext[0]}, GetJsonSerializerSettingsForBiblModule());
+            }
+        }
+
+        public ActionResult AdvancedSearchInBookCount(string json, string bookXmlId, string versionXmlId)
+        {
+            var deserialized = JsonConvert.DeserializeObject<IList<ConditionCriteriaDescriptionBase>>(json, new ConditionCriteriaDescriptionConverter());
+            var listSearchCriteriaContracts = Mapper.Map<IList<SearchCriteriaContract>>(deserialized);
+
+            listSearchCriteriaContracts.Add(new ResultCriteriaContract
+            {
+                Start = 0,
+                Count = 1
+            });
+
+            listSearchCriteriaContracts.Add(new ResultRestrictionCriteriaContract
+            {
+                ResultBooks = new List<BookVersionPairContract> {new BookVersionPairContract {Guid = bookXmlId, VersionId = versionXmlId}}
+            });
+            using (var client = GetMainServiceClient())
+            {
+                var result = client.SearchByCriteria(listSearchCriteriaContracts).FirstOrDefault();
+                if (result != null)
+                {
+                    var count = result.TotalHitCount;
+                    return Json(new {count});
+                }
+
+                return Json(new {count = 0});
+            }
+        }
+
+        public ActionResult AdvancedSearchInBookPagesWithMatchHit(string json, long projectId, long? snapshotId)
+        {
+            var deserialized = JsonConvert.DeserializeObject<IList<ConditionCriteriaDescriptionBase>>(json, new ConditionCriteriaDescriptionConverter());
+            var listSearchCriteriaContracts = Mapper.Map<List<SearchCriteriaContract>>(deserialized);
+
+            using (var client = GetRestClient())
+            {
+                var request = new SearchPageRequestContract
+                {
+                    ConditionConjunction = listSearchCriteriaContracts
+                };
+                var result = client.SearchPage(projectId, request);
+
+                return Json(new {pages = result});
+            }
+        }
+
+        public ActionResult TextSearchInBookPaged(string text, int start, int count, string bookXmlId, string versionXmlId)
+        {
+            var listSearchCriteriaContracts = CreateTextCriteriaList(CriteriaKey.Fulltext, text);
+
+            listSearchCriteriaContracts.Add(new ResultCriteriaContract
+            {
+                Start = 0,
+                Count = 1,
+                HitSettingsContract = new HitSettingsContract
+                {
+                    ContextLength = 45,
+                    Count = count,
+                    Start = start
+                }
+            });
+            listSearchCriteriaContracts.Add(new ResultRestrictionCriteriaContract
+            {
+                ResultBooks =
+                    new List<BookVersionPairContract>
+                    {
+                        new BookVersionPairContract {Guid = bookXmlId, VersionId = versionXmlId}
+                    }
+            });
+
+            using (var client = GetMainServiceClient())
+            {
+                var result = client.SearchByCriteria(listSearchCriteriaContracts).FirstOrDefault();
+                if (result != null)
+                {
+                    return Json(new {results = result.Results}, GetJsonSerializerSettingsForBiblModule());
+                }
+
+                return Json(new {results = new PageResultContext[0]}, GetJsonSerializerSettingsForBiblModule());
+            }
+        }
+
+        public ActionResult TextSearchInBookCount(string text, string bookXmlId, string versionXmlId)
+        {
+            var listSearchCriteriaContracts = CreateTextCriteriaList(CriteriaKey.Fulltext, text);
+
+            listSearchCriteriaContracts.Add(new ResultCriteriaContract
+            {
+                Start = 0,
+                Count = 1
+            });
+            listSearchCriteriaContracts.Add(new ResultRestrictionCriteriaContract
+            {
+                ResultBooks =
+                    new List<BookVersionPairContract>
+                    {
+                        new BookVersionPairContract {Guid = bookXmlId, VersionId = versionXmlId}
+                    }
+            });
+            
+            using (var client = GetMainServiceClient())
+            {
+                var result = client.SearchByCriteria(listSearchCriteriaContracts).FirstOrDefault();
+                if (result != null)
+                {
+                    var count = result.TotalHitCount;
+                    return Json(new {count});
+                }
+
+                return Json(new {count = 0});
+            }
+        }
+
+        public ActionResult TextSearchInBookPagesWithMatchHit(string text, long projectId, long? snapshotId)
+        {
+            var listSearchCriteriaContracts = CreateTextCriteriaList(CriteriaKey.Fulltext, text);
+            
+            using (var client = GetRestClient())
+            {
+                var request = new SearchPageRequestContract
+                {
+                    ConditionConjunction = listSearchCriteriaContracts
+                };
+                var result = client.SearchPage(projectId, request);
+
+                return Json(new {pages = result});
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/GoldenLayout/js/itjakub.goldenlayout.ts
@@ -0,0 +1,165 @@
+﻿///<reference path="../../../lib/golden-layout/index.d.ts"/>
+//import GoldenLayout = require("golden-layout");
+//declare var GoldenLayout;
+
+class GoldenLayoutReader extends ReaderModule {
+    readerLayout: any;
+    public initReader(bookXmlId: string, versionXmlId: string, bookTitle: string, pageList: IPage[]) {
+        this.bookId = bookXmlId;
+        this.versionId = versionXmlId;
+        this.actualPageIndex = 0;
+        this.sliderOnPage = 0;
+        this.pages = new Array<BookPage>();
+        this.pagesById = {};
+        this.bookmarks = new Array<IBookmarkPosition>(pageList.length);
+        this.leftSidePanels = new Array<SidePanel>();
+        this.rightSidePanels = new Array<SidePanel>();
+
+        for (var i = 0; i < pageList.length; i++) { //load pageList
+            var page = pageList[i];
+            var bookPageItem = new BookPage(page.id, page.name, page.position);
+
+            this.pages.push(bookPageItem);
+            this.pagesById[bookPageItem.pageId] = bookPageItem;
+        }
+
+        $(this.readerContainer).empty();
+        var readerDiv: HTMLDivElement = document.createElement("div");
+        $(readerDiv).addClass("reader");
+
+        var readerHeadDiv: HTMLDivElement = document.createElement("div");
+        $(readerHeadDiv).addClass("reader-head content-container");
+
+        var fullscreenButton = document.createElement("button");
+        $(fullscreenButton).addClass("fullscreen-button");
+
+        var fullscreenSpan = document.createElement("span");
+        $(fullscreenSpan).addClass("glyphicon glyphicon-fullscreen");
+        $(fullscreenButton).append(fullscreenSpan);
+        $(fullscreenButton).click((event) => {
+            $(this.readerContainer).find(".reader").addClass("fullscreen");
+        });
+        readerHeadDiv.appendChild(fullscreenButton);
+
+        var fullscreenCloseButton = document.createElement("button");
+        $(fullscreenCloseButton).addClass("fullscreen-close-button");
+
+        var closeSpan = document.createElement("span");
+        $(closeSpan).addClass("glyphicon glyphicon-remove");
+        $(fullscreenCloseButton).append(closeSpan);
+        $(fullscreenCloseButton).click((event) => {
+            $(this.readerContainer).find(".reader").removeClass("fullscreen");
+        });
+        readerHeadDiv.appendChild(fullscreenCloseButton);
+
+
+        //var title = this.makeTitle(bookTitle);
+        //readerHeadDiv.appendChild(title);
+
+
+        //var controls = this.makeControls();
+        //readerHeadDiv.appendChild(controls);
+        //readerDiv.appendChild(readerHeadDiv);
+        
+        var goldenDiv = document.createElement("div");
+        goldenDiv.id = "GoldenReaderDiv";
+        readerDiv.appendChild(goldenDiv);
+        this.makeGoldenReader();
+    }
+
+    private makeGoldenReader() {
+        var config = this.createConfig();
+        //var bookText = this.getBookText();
+        this.readerLayout = new GoldenLayout(config, $('#GoldenReaderDiv'));
+        this.readerLayout.registerComponent('readerTab', function (container, state) {
+            if (state.label === 'text') {
+                //$(container.getElement()).html(this.getBookText());
+            }
+        });
+        this.readerLayout.init();
+    }
+
+    private getBookText(): HTMLDivElement {
+        var returnDiv: HTMLDivElement = document.createElement("div");
+        var textPanel: TextPanel = null;
+        if (this.showPanelList.indexOf(ReaderPanelEnum.TextPanel) >= 0) {
+            textPanel = this.appendTextPanel(returnDiv);
+        }
+        return returnDiv;
+    }
+
+    private createConfig() {
+        var layoutConfig = {
+            dimensions: {
+                headerHeight: 36
+            },
+            content: [{
+                type: 'row',
+                isClosable: false,
+                content: [{
+                    type: 'stack',
+                    width: 18,
+                    content: [{
+                        type: 'component',
+                        id: 'bookmarks',
+                        componentState: { label: 'bookmarks' },
+                        componentName: 'readerTab',
+                        title: 'Záložky'
+                    }, {
+                        type: 'component',
+                        id: 'view',
+                        componentState: { label: 'view' },
+                        componentName: 'readerTab',
+                        title: 'Zobrazení'
+                    }, {
+                        type: 'component',
+                        id: 'search',
+                        componentState: { label: 'search' },
+                        componentName: 'readerTab',
+                        title: 'Vyhledávání'
+                    }]
+                }, {
+                    type: 'component',
+                    id: 'text',
+                    componentState: { label: 'text' },
+                    componentName: 'readerTab',
+                    isClosable: false,
+                    title: 'Text'
+                }, {
+                    type: 'component',
+                    id: 'img',
+                    componentState: { label: 'img' },
+                    componentName: 'readerTab',
+                    title: 'Náhled'
+                }]
+            }]
+        };
+        return layoutConfig;
+    }
+}
+
+
+function initGoldenReader(bookXmlId: string,
+    versionXmlId: string,
+    bookTitle: string,
+    pageList: any,
+    searchedText?: string,
+    initPageXmlId?: string) {
+
+
+    function readerPageChangedCallback(pageId: number) {
+        updateQueryStringParameter("page", pageId);
+    }
+
+    var readerPanels = [
+        ReaderPanelEnum.TextPanel, ReaderPanelEnum.ImagePanel, ReaderPanelEnum.ContentPanel,
+        ReaderPanelEnum.SearchPanel, ReaderPanelEnum.SettingsPanel
+    ];
+    var panelButtons = [PanelButtonEnum.Close, PanelButtonEnum.Pin, PanelButtonEnum.ToNewWindow];
+    var readerPlugin = new GoldenLayoutReader(<any>$("#ReaderDiv")[0],
+        readerPageChangedCallback,
+        readerPanels,
+        panelButtons,
+        panelButtons);
+    readerPlugin.initReader(bookXmlId, versionXmlId, bookTitle, pageList);
+}
