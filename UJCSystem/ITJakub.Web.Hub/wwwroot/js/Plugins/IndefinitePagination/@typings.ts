75e62dc5e757f777eb0e18f55e86681627e3f99d
Denis Senchenko <senchden@fel.cvut.cz>
2018-01-16 18:20:46 +0100

    Start of indefinite pagination plugin implementation.

---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/js/Plugins/IndefinitePagination/@typings.ts
@@ -0,0 +1,13 @@
+﻿declare namespace IndefinitePagination {
+    interface Options {
+        container: HTMLDivElement | JQuery;
+        loadAllPagesButton?: boolean;
+        loadAllPagesCallback: Function;
+        showSlider?: boolean;
+        showInput?: boolean;
+        previousPageCallback?: Function;
+        nextPageCallback: Function;
+        initialCallback?: Function;
+        executeInitialCallbackOnInit?: boolean;
+    }
+}
---
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/js/Plugins/IndefinitePagination/ridics.indefinite-pagination.ts
@@ -0,0 +1,57 @@
+﻿class IndefinitePagination {
+    private readonly options: IndefinitePagination.Options;
+    private readonly paginationContainer: JQuery;
+
+    constructor(options: IndefinitePagination.Options) {
+        this.options = options;
+        this.paginationContainer = $(options.container);
+    }
+
+    make() {//TODO logic
+        const container = this.paginationContainer;
+        container.addClass("indefinite-pagination");
+        container.empty();
+        const innerContainer = $("<div></div>");
+        if (this.options.showSlider) {
+            innerContainer.append(this.makeSlider());
+        }
+
+        innerContainer.append(this.createElements());
+
+        if (this.options.showInput) {
+            innerContainer.append(this.createPageInput());
+        }
+
+        this.paginationContainer.append(innerContainer);
+    }
+
+    previousPageIsAvailable() {
+        //TODO logic
+    }
+
+    nextPageIsAvailable() {
+        //TODO logic
+    }
+
+    goToPage(pageNumber: number) {
+    //TODO implement logic. Check number of pages
+    }
+
+    private makeSlider():JQuery {
+        var slider = $("<div></div>");
+        //TODO implement logic. Check number of pages
+        return slider;
+    }
+
+    private createElements() {
+        var controlElements = $("<div></div>");
+        //TODO logic
+        return controlElements;
+    }
+
+    private createPageInput() {
+        var inputElement = $("<div></div>");
+        //TODO logic
+        return inputElement;
+    }
+}
