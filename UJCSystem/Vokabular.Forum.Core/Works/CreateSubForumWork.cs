fd3dfc78484efc6063106b92cc0f13108bc50221
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2018-09-21 14:49:45 +0200

    Refactoring

--- UJCSystem/Vokabular.Forum.Core/Works/CreateSubForumWork.cs
+++ UJCSystem/Vokabular.Forum.Core/Works/CreateSubForumWork.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using Vokabular.ForumSite.Core.Helpers;
+﻿using Vokabular.ForumSite.Core.Helpers;
 using Vokabular.ForumSite.DataEntities.Database.Entities;
 using Vokabular.ForumSite.DataEntities.Database.Enums;
 using Vokabular.ForumSite.DataEntities.Database.Repositories;
@@ -12,31 +10,22 @@ namespace Vokabular.ForumSite.Core.Works
 {
     class CreateSubForumWork : UnitOfWorkBase<int>
     {
-        private const string FirstTopicName = "Základní informace";
         private readonly ForumRepository m_forumRepository;
         private readonly CategoryRepository m_categoryRepository;
-        private readonly TopicRepository m_topicRepository;
-        private readonly MessageRepository m_messageRepository;
-        private readonly UserRepository m_userRepository;
         private readonly ForumAccessRepository m_forumAccessRepository;
         private readonly AccessMaskRepository m_accessMaskRepository;
         private readonly GroupRepository m_groupRepository;
         private readonly CategoryContract m_category;
 
-        public CreateSubForumWork(ForumRepository forumRepository, CategoryRepository categoryRepository, TopicRepository topicRepository,
-            MessageRepository messageRepository, UserRepository userRepository, ForumAccessRepository forumAccessRepository,
-            AccessMaskRepository accessMaskRepository, GroupRepository groupRepository, CategoryContract category,
-            UserDetailContract user) :
-            base(forumRepository)
+        public CreateSubForumWork(ForumRepository forumRepository, CategoryRepository categoryRepository,
+            ForumAccessRepository forumAccessRepository,
+            AccessMaskRepository accessMaskRepository, GroupRepository groupRepository, CategoryContract category) : base(forumRepository)
         {
             m_forumRepository = forumRepository;
             m_categoryRepository = categoryRepository;
             m_forumAccessRepository = forumAccessRepository;
             m_accessMaskRepository = accessMaskRepository;
             m_groupRepository = groupRepository;
-            m_userRepository = userRepository;
-            m_topicRepository = topicRepository;
-            m_messageRepository = messageRepository;
             m_category = category;
         }
 
@@ -46,15 +35,15 @@ namespace Vokabular.ForumSite.Core.Works
 
             foreach (UrlBookTypeEnum bookType in BookTypeHelper.GetBookTypeEnumsWithCategories())
             {
-                Category category = m_categoryRepository.GetCategoryByExternalId((short)bookType);
+                Category category = m_categoryRepository.GetCategoryByExternalId((short) bookType);
 
                 Forum parentForum = null;
                 if (m_category.ParentCategoryId != null)
                 {
-                    parentForum = m_forumRepository.GetForumByExternalIdAndCategory((int)m_category.ParentCategoryId, category);
+                    parentForum = m_forumRepository.GetForumByExternalIdAndCategory((int) m_category.ParentCategoryId, category);
                 }
 
-                Forum forum = new Forum(m_category.Description, category, (short)ForumTypeEnum.SubCategory)
+                Forum forum = new Forum(m_category.Description, category, (short) ForumTypeEnum.SubCategory)
                 {
                     ExternalId = m_category.Id,
                     ParentForum = parentForum
