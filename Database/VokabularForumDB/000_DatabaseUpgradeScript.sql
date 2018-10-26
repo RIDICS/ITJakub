5643c546df06c4618856e76ea338ff0cf71176be
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2018-10-26 13:07:04 +0200

    Fix forum database upgrade script

--- Database/VokabularForumDB/000_DatabaseUpgradeScript.sql
+++ Database/VokabularForumDB/000_DatabaseUpgradeScript.sql
@@ -30,7 +30,7 @@ BEGIN TRAN
 -- Add column for mapping Forum (Forum) - Project (Vokabular)
 	ALTER TABLE [dbo].[yaf_Forum] ADD ExternalProjectID BIGINT NULL
 
-	ALTER TABLE [dbo].[yaf_UserProfile] ADD VokabularID NVARCHAR(255) NULL
+	ALTER TABLE [dbo].[yaf_prov_Profile] ADD VokabularID NVARCHAR(255) NULL
 
 -- Insert version number to DatabaseVersion table
 
