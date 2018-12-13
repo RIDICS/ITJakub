a0ed118359c363141c37fe1b0cc75a8a8b993ad2
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2018-12-13 16:07:54 +0100

    FeedbackManager use contex User

--- UJCSystem/ITJakub.Web.Hub/Areas/CardFiles/Controllers/CardFilesController.cs
+++ UJCSystem/ITJakub.Web.Hub/Areas/CardFiles/Controllers/CardFilesController.cs
@@ -87,7 +87,7 @@ namespace ITJakub.Web.Hub.Areas.CardFiles.Controllers
 
         public ActionResult Feedback()
         {
-            var viewModel = m_feedbacksManager.GetBasicViewModel(GetFeedbackFormIdentification(), StaticTexts.TextHomeFeedback, IsUserLoggedIn(), "home");
+            var viewModel = m_feedbacksManager.GetBasicViewModel(GetFeedbackFormIdentification(), StaticTexts.TextHomeFeedback, IsUserLoggedIn(), "home", User);
             return View(viewModel);
         }
 
--- UJCSystem/ITJakub.Web.Hub/Areas/Editions/Controllers/EditionsController.cs
+++ UJCSystem/ITJakub.Web.Hub/Areas/Editions/Controllers/EditionsController.cs
@@ -98,7 +98,7 @@ namespace ITJakub.Web.Hub.Areas.Editions.Controllers
 
         public ActionResult Feedback()
         {
-            var viewModel = m_feedbacksManager.GetBasicViewModel(GetFeedbackFormIdentification(), StaticTexts.TextHomeFeedback, IsUserLoggedIn(), "home");
+            var viewModel = m_feedbacksManager.GetBasicViewModel(GetFeedbackFormIdentification(), StaticTexts.TextHomeFeedback, IsUserLoggedIn(), "home", User);
             return View(viewModel);
         }
 
