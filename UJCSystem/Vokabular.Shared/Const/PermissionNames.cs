1d3d216edd8181fcdfec685977887a46fffe180d
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2018-12-14 13:35:43 +0100

    Added constants for permissions

---
+++ UJCSystem/Vokabular.Shared/Const/PermissionNames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vokabular.Shared.Const
+{
+    public class PermissionNames
+    {
+        public const string ManagePermissions = "manage-permissions";
+        public const string UploadBook ="upload-book";
+        public const string AddNews = "add-news";
+        public const string ManageFeedbacks= "manage-feedbacks";
+        public const string EditLemmatization = "edit-lemmatization";
+        public const string ReadLemmatization = "read-lemmatization";
+        public const string DerivateLemmatization = "derivate-lemmatization";
+        public const string EditionPrintText = "edition-print-text";
+        public const string EditStaticText = "edit-static-text";
+        public const string CardFileNlaExcerpce = "card-file-nla-excerpce";
+        public const string CardFileGebauerExcerpce = "card-file-gebauer-excerpce";
+        public const string CardFileGebauerPrameny = "card-file-gebauer-prameny";
+        public const string CardFileStaroceskaExcerpce = "card-file-staroceska-excerpce";
+        public const string CardFileZubatyExcerpce = "card-file-zubaty-excerpce";
+        public const string CardFileArchivLidovehoJazyka = "card-file-archiv-lidoveho-jazyka";
+        public const string CardFileExcerpceTextuZ16Stoleti = "card-file-excerpce-textu-z-16-stoleti";
+        public const string CardFileLatisnkoCeskakaroteka = "card-file-latinsko-ceska-kartoteka";
+        public const string CardFileRukopisy = "card-file-rukopisy";
+        public const string CardFileJustitia = "card-file-justitia";
+        public const string CardFileTylExcerpce = "card-file-tyl-excerpce";
+        public const string CardFileNlaPrameny = "card-file-nla-prameny";
+        public const string CardFileSvoboda = "card-file-svoboda";
+        public const string CardFileExcerpcePomistnichJmen = "card-file-excerpce-pomistnich-jmen";
+        public const string CardFileTerezianskyKlaster = "card-file-tereziansky-katastr";
+        public const string CardFileArchivyMuzea = "card-file-archivy-muzea";
+        public const string CardFileStabilniKatastr = "card-file-stabilni-katastr";
+        public const string CardFileSajtl = "card-file-sajtl";
+        public const string CardFileDodatkyPsjc = "card-file-dodatky-psjc";
+        public const string CardFileGreplArchiv = "card-file-grepl-archiv";
+    }
+}
