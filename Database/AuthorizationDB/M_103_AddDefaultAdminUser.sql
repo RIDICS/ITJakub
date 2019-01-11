ccc3285bf765ee92553f1486e7f268618e7704ad
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-01-11 14:03:50 +0100

    Chaned admin password in authentication service

--- Database/AuthorizationDB/M_103_AddDefaultAdminUser.sql
+++ Database/AuthorizationDB/M_103_AddDefaultAdminUser.sql
@@ -11,14 +11,14 @@ VALUES
    ('Admin' -- Username
 	,'Admin' -- FirstName
            ,'Admin' -- LastName
-           ,'AQAAAAEAACcQAAAAEMmJB8Wd5WUJlww2+qO1ClJ7+nrFKxt5gm73qaiDqUmBSyjTXv2Vjw87lsG14XldAw==' -- PasswordHash (password is 'bob')
-           ,'7cda8f60-01de-447f-91e9-a497aa6b2146' -- SecurityStamp
+           ,'AQAAAAEAACcQAAAAEHKbp0Fsyhjj66Fh8/JxQK8n4gmvomXJ/e7Owg5Aah5S22XU1vCNzitvk9WY9LBS1Q==' -- PasswordHash (password is 'Administrator915.')
+           ,'7cda8f60-01de-447f-91e9-a497aa6b2146' -- SecurityStamp (random)
            ,false --TwoFactorEnabled
            ,NULL -- LockoutEndDateUtc
 		   ,false --LockoutEnabled
            ,0 -- AccessFailedCount
            ,'2019-01-01 00:00:00.000' -- LastChange
-		   ,'f19d060a-42e6-49b4-8655-993378fed3c1'); -- MasterUserId
+		   ,'f19d060a-42e6-49b4-8655-993378fed3c1'); -- MasterUserId (random)
 
 		   
 SELECT "Id"	INTO roleId FROM public."Role" WHERE "Name" = 'Admin';
