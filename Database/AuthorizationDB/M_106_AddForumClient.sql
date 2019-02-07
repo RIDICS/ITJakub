751409fcab92966a13cebce883b741aa2c13dcd1
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-02-07 16:10:07 +0100

    AuthenticationDB - added scripts for creating clients

---
+++ Database/AuthorizationDB/M_106_AddForumClient.sql
@@ -0,0 +1,45 @@
+﻿START TRANSACTION;
+
+DO $$
+DECLARE clientId integer;
+BEGIN
+
+INSERT INTO public."Client"(
+	"Name", "Description", "DisplayUrl", "LogoUrl", "RequireConsent")
+	VALUES ('VokabularForum', 'Vokabular Webovy - Forum', 'http://vokabular.ujc.cas.cz', 'http://vokabular.ujc.cas.cz', false);
+
+		   
+SELECT "Id"	INTO clientId FROM public."Client" WHERE "Name" = 'VokabularForum';
+
+INSERT INTO public."Uri"(
+	"Uri", "ClientId")
+	VALUES ('https://localhost:50165/auth.aspx?auth=vokabular', clientId);
+
+INSERT INTO public."Uri_UriType"("UriId", "UriTypeId")
+	VALUES ((SELECT "Id" FROM public."Uri" WHERE "Uri" = 'https://localhost:50165/auth.aspx?auth=vokabular'), (SELECT "Id" FROM public."UriType" WHERE "Value" = 'Redirect'));
+
+INSERT INTO public."Secret"(
+	"Value", "Description", "Expiration", "ResourceId", "ClientId", "Discriminator")
+	VALUES ('secret', 'secret', null, null, clientId, 'ClientSecret');
+
+INSERT INTO public."Client_ApiResource"(
+	"ClientId", "ApiResourceId")
+	VALUES (clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'authorization-provider'));
+
+INSERT INTO public."Client_GrantType"(
+	"ClientId", "GrantTypeId")
+	VALUES (clientId, (SELECT "Id" FROM public."GrantType" WHERE "Value" = 'authorization_code'));
+
+INSERT INTO public."Client_IdentityResource"(
+	"ClientId", "IdentityResourceId")
+	VALUES (clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'offline_access')),
+	(clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'openid')),
+	(clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'profile'));
+
+END $$;
+
+INSERT INTO public."VersionInfo"("Version", "AppliedOn", "Description") 
+VALUES 
+(106, now(), 'M_106_AddForumClient');
+
+COMMIT;
---
+++ Database/AuthorizationDB/M_107_AddBatchImportClient.sql
@@ -0,0 +1,40 @@
+﻿START TRANSACTION;
+
+DO $$
+DECLARE clientId integer;
+BEGIN
+
+INSERT INTO public."Client"(
+	"Name", "Description", "DisplayUrl", "LogoUrl", "RequireConsent")
+	VALUES ('VokabularBatchImport', 'Vokabular Webovy - aplikace pro hromadny import del', 'http://vokabular.ujc.cas.cz', 'http://vokabular.ujc.cas.cz', false);
+
+		   
+SELECT "Id"	INTO clientId FROM public."Client" WHERE "Name" = 'VokabularBatchImport';
+
+INSERT INTO public."Uri"(
+	"Uri", "ClientId")
+	VALUES ('https://127.0.0.1:7890/', clientId);
+
+INSERT INTO public."Uri_UriType"("UriId", "UriTypeId")
+	VALUES ((SELECT "Id" FROM public."Uri" WHERE "Uri" = 'https://127.0.0.1:7890/'), (SELECT "Id" FROM public."UriType" WHERE "Value" = 'Redirect'));
+	
+INSERT INTO public."Client_ApiResource"(
+	"ClientId", "ApiResourceId")
+	VALUES (clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'authorization-provider'));
+
+INSERT INTO public."Client_GrantType"(
+	"ClientId", "GrantTypeId")
+	VALUES (clientId, (SELECT "Id" FROM public."GrantType" WHERE "Value" = 'hybrid'));
+
+INSERT INTO public."Client_IdentityResource"(
+	"ClientId", "IdentityResourceId")
+	VALUES (clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'openid')),
+	(clientId, (SELECT "Id" FROM public."Resource" WHERE "Name" = 'profile'));
+
+END $$;
+
+INSERT INTO public."VersionInfo"("Version", "AppliedOn", "Description") 
+VALUES 
+(107, now(), 'M_107_AddBatchImportClient');
+
+COMMIT;
