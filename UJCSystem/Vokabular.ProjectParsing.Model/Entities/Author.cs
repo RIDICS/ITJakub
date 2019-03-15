533abdb1110f6911934965a835e531e7e4ee49a2
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-03-15 11:34:25 +0100

    ProjectParsing Author - added method Equals, GetHashCode

--- UJCSystem/Vokabular.ProjectParsing.Model/Entities/Author.cs
+++ UJCSystem/Vokabular.ProjectParsing.Model/Entities/Author.cs
@@ -2,13 +2,26 @@
 {
     public class Author
     {
-        public string FirstName;
-        public string LastName;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public Author(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Author author && author.FirstName.Equals(FirstName) && author.FirstName.Equals(FirstName);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((FirstName != null ? FirstName.GetHashCode() : 0) * 397) ^ (LastName != null ? LastName.GetHashCode() : 0);
+            }
         }
     }
 }
