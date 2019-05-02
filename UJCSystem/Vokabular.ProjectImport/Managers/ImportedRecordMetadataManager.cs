bb547fd087bce52dcc535e76c5b6f7a62b41bd56
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-05-02 09:14:07 +0200

    ImportService - fixed creating ImportedRecordMetadata

--- UJCSystem/Vokabular.ProjectImport/Managers/ImportedRecordMetadataManager.cs
+++ UJCSystem/Vokabular.ProjectImport/Managers/ImportedRecordMetadataManager.cs
@@ -7,15 +7,17 @@ namespace Vokabular.ProjectImport.Managers
     public class ImportedRecordMetadataManager
     {
         private readonly ImportedRecordMetadataRepository m_importedRecordMetadataRepository;
+        private readonly ProjectRepository m_projectRepository;
 
-        public ImportedRecordMetadataManager(ImportedRecordMetadataRepository importedRecordMetadataRepository)
+        public ImportedRecordMetadataManager(ImportedRecordMetadataRepository importedRecordMetadataRepository, ProjectRepository projectRepository)
         {
             m_importedRecordMetadataRepository = importedRecordMetadataRepository;
+            m_projectRepository = projectRepository;
         }
 
         public int CreateImportedRecordMetadata(ImportedRecord importedRecord, int importHistoryId)
         {
-            var result = new CreateImportedRecordMetadataWork(m_importedRecordMetadataRepository, importedRecord, importHistoryId).Execute();
+            var result = new CreateImportedRecordMetadataWork(m_importedRecordMetadataRepository, m_projectRepository, importedRecord, importHistoryId).Execute();
             return result;
         }
     }
--- UJCSystem/Vokabular.ProjectImport/Works/CreateImportedRecordMetadataWork.cs
+++ UJCSystem/Vokabular.ProjectImport/Works/CreateImportedRecordMetadataWork.cs
@@ -2,40 +2,45 @@
 using Vokabular.DataEntities.Database.Repositories;
 using Vokabular.DataEntities.Database.UnitOfWork;
 using Vokabular.ProjectParsing.Model.Entities;
-using Project = Vokabular.DataEntities.Database.Entities.Project;
 
 namespace Vokabular.ProjectImport.Works
 {
     public class CreateImportedRecordMetadataWork : UnitOfWorkBase<int>
     {
         private readonly ImportedRecordMetadataRepository m_importedRecordMetadataRepository;
+        private readonly ProjectRepository m_projectRepository;
         private readonly ImportedRecord m_data;
         private readonly int m_importHistoryId;
 
-        public CreateImportedRecordMetadataWork(ImportedRecordMetadataRepository importedRecordMetadataRepository, ImportedRecord data, int importHistoryId) : base(importedRecordMetadataRepository)
+        public CreateImportedRecordMetadataWork(ImportedRecordMetadataRepository importedRecordMetadataRepository, ProjectRepository projectRepository, ImportedRecord data, int importHistoryId) : base(importedRecordMetadataRepository)
         {
             m_importedRecordMetadataRepository = importedRecordMetadataRepository;
+            m_projectRepository = projectRepository;
             m_data = data;
             m_importHistoryId = importHistoryId;
         }
 
         protected override int ExecuteWorkImplementation()
         {
-            var importHistory = m_importedRecordMetadataRepository.Load<ImportHistory>(m_importHistoryId);
-            Project project = null;          
+            var importHistory = m_importedRecordMetadataRepository.Load<ImportHistory>(m_importHistoryId);        
             ImportedProjectMetadata importedProjectMetadata = null;
+            Snapshot latestSnapshot = null;
 
-            if (!(m_data.IsNew && m_data.IsFailed) && m_data.ImportedProjectMetadataId.HasValue)
+            if (m_data.ImportedProjectMetadataId.HasValue)
             {
-                importedProjectMetadata = m_importedRecordMetadataRepository.Load<ImportedProjectMetadata>(m_data.ImportedProjectMetadataId.Value);
-                project = m_importedRecordMetadataRepository.Load<Project>(m_data.ProjectId);
+                importedProjectMetadata = m_importedRecordMetadataRepository.Load<ImportedProjectMetadata>(m_data.ImportedProjectMetadataId.Value);   
+            }
+
+            if (!m_data.IsFailed)
+            {
+                latestSnapshot = m_projectRepository.GetLatestSnapshot(m_data.ProjectId);
             }
 
             var importMetadata = new ImportedRecordMetadata
             {
                LastUpdate = importHistory,
                LastUpdateMessage = m_data.FaultedMessage,
-               Snapshot = project?.LatestPublishedSnapshot,
+               Snapshot = latestSnapshot,
                ImportedProjectMetadata = importedProjectMetadata
             };
 
