1ded34c539bfe0db3aa478d1d7d2e07a85d20a62
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-07-01 10:15:23 +0200

    Fix ForumAccess entity according to specified mapping

--- UJCSystem/Vokabular.Forum.DataEntities/Database/Entities/Forumaccess.cs
+++ UJCSystem/Vokabular.Forum.DataEntities/Database/Entities/Forumaccess.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 namespace Vokabular.ForumSite.DataEntities.Database.Entities {
     
     public class ForumAccess {
-        public int GroupID { get; set; }
-        public int ForumID { get; set; }
         public Group Group { get; set; }
         public Forum Forum { get; set; }
         public AccessMask AccessMask { get; set; }
@@ -16,16 +14,16 @@ namespace Vokabular.ForumSite.DataEntities.Database.Entities {
 			if (obj == null) return false;
 			var t = obj as ForumAccess;
 			if (t == null) return false;
-			if (GroupID == t.GroupID
-			 && ForumID == t.ForumID)
+			if (Group.GroupID == t.Group.GroupID
+			 && Forum.ForumID == t.Forum.ForumID)
 				return true;
 
 			return false;
         }
         public override int GetHashCode() {
 			int hash = GetType().GetHashCode();
-			hash = (hash * 397) ^ GroupID.GetHashCode();
-			hash = (hash * 397) ^ ForumID.GetHashCode();
+			hash = (hash * 397) ^ Group.GroupID.GetHashCode();
+			hash = (hash * 397) ^ Forum.ForumID.GetHashCode();
 
 			return hash;
         }
