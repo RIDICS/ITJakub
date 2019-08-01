1b1085aadebc50a1801b6c1f7fc796afb9c93977
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-08-01 09:46:21 +0200

    Use LocalizationService in StaticTextManager

--- UJCSystem/ITJakub.Web.Hub/Core/Managers/StaticTextManager.cs
+++ UJCSystem/ITJakub.Web.Hub/Core/Managers/StaticTextManager.cs
@@ -11,28 +11,31 @@ namespace ITJakub.Web.Hub.Core.Managers
     public class StaticTextManager
     {
         private readonly IDynamicTextService m_dynamicTextService;
+        private readonly ILocalizationService m_localizationService;
         private readonly IMarkdownToHtmlConverter m_markdownToHtmlConverter;
 
-        public StaticTextManager(IDynamicTextService dynamicTextService, IMarkdownToHtmlConverter markdownToHtmlConverter)
+        public StaticTextManager(IDynamicTextService dynamicTextService, ILocalizationService localizationService, IMarkdownToHtmlConverter markdownToHtmlConverter)
         {
             m_dynamicTextService = dynamicTextService;
+            m_localizationService = localizationService;
             m_markdownToHtmlConverter = markdownToHtmlConverter;
         }
         
-        public StaticTextViewModel GetText(string name, string scope)
+        public EditStaticTextViewModel GetText(string name, string scope)
         {
             var staticText = m_dynamicTextService.GetDynamicText(name, scope);           
-
+           
             if (staticText == null)
             {
-                return new StaticTextViewModel
+                return new EditStaticTextViewModel
                 {
                     Name = name,
+                    Scope = scope,
                     IsRecordExists = false
                 };
             }
 
-            var staticTextViewModel = new StaticTextViewModel
+            var staticTextViewModel = new EditStaticTextViewModel
             {
                 Format = (StaticTextFormatType) staticText.Format,
                 Name = staticText.Name,
@@ -57,41 +60,21 @@ namespace ITJakub.Web.Hub.Core.Managers
             return staticTextViewModel;
         }
 
-        //TODO #Localization
         public StaticTextViewModel GetRenderedHtmlText(string name, string scope)
         {
             var staticText = m_dynamicTextService.GetDynamicText(name, scope);
-            if (staticText == null)
-            {
-                return new StaticTextViewModel
-                {
-                    Name = name,
-                    Format = StaticTextFormatType.Markdown,
-                    IsRecordExists = false
-                };
-            }
+            var isRecordExist = staticText != null;
+            var text = isRecordExist ? staticText.Text : m_localizationService.Translate(name, scope);
 
+            var htmlText = m_markdownToHtmlConverter.ConvertToHtml(text);
+            
             var viewModel = new StaticTextViewModel
             {
-                Format = (StaticTextFormatType)staticText.Format,
-                IsRecordExists = true,
-                LastModificationAuthor = staticText.ModificationUser,
-                LastModificationTime = staticText.ModificationTime,
-                Name = staticText.Name,
-                Text = staticText.Text,
-                Scope = staticText.DictionaryScope
+                IsRecordExists = isRecordExist,
+                Name = name,
+                Text = htmlText,
+                Scope = scope
             };
-
-            switch (viewModel.Format)
-            {
-                case StaticTextFormatType.Markdown:
-                    viewModel.Text = m_markdownToHtmlConverter.ConvertToHtml(staticText.Text);
-                    viewModel.Format = StaticTextFormatType.Html;
-                    break;
-                case StaticTextFormatType.PlainText:
-                case StaticTextFormatType.Html:
-                    break;
-            }
 
             return viewModel;
         }
