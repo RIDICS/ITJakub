fa5de98f96ce7aed41a12826bc54fefbfd350068
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-02 14:52:20 +0200

    [Draft] ReaderController for redirecting

---
+++ UJCSystem/ITJakub.Web.Hub/Areas/BookReader/Controllers/BookReaderController.cs
@@ -0,0 +1,25 @@
+﻿using ITJakub.Web.Hub.Controllers;
+using ITJakub.Web.Hub.Core.Communication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITJakub.Web.Hub.Areas.BookReader.Controllers
+{
+    [Area("BookReader")]
+    public class BookReaderController : BaseController
+    {
+        public BookReaderController(CommunicationProvider communicationProvider) : base(communicationProvider)
+        {
+        }
+
+        public ActionResult Listing(long bookId, string searchText, string pageId)
+        {
+            return RedirectToAction("Listing", "Editions",new
+            {
+                Area = "Editions",
+                bookId,
+                searchText,
+                page = pageId,
+            });
+        }
+    }
+}
