1a2147f8e6e329d3baddaa373ef99ce2204ff3e3
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-08-19 12:50:28 +0200

    Predeploy script - check if database is running

--- PreDeploy.ps1
+++ PreDeploy.ps1
@@ -1,7 +1,9 @@
 #!/usr/bin/env pwsh
 
 param (
-	[bool]$recreateDatabases = $true
+    [bool]$recreateDatabases = $true,
+    [string]$elasticSearchUrl = "localhost:9200",
+    [string]$existDbUrl = "localhost:8080/exist"
 )
 
 $CurrentPath = (Get-Location -PSProvider FileSystem).ProviderPath
@@ -10,6 +12,47 @@ Write-Host
 Write-Host "Using root directory: ${CurrentPath}"
 Write-Host
 Write-Host
+
+function TestConnection {
+    Param (
+        [String]$serviceName,
+        [String]$url)
+
+    $request = [Net.HttpWebRequest]::Create($url)  
+
+    try 
+    {     
+        $response = [Net.HttpWebResponse]$request.GetResponse() 
+        Write-Host "Database ${serviceName} is running"
+        $request.Abort() 
+    }     
+    Catch  
+    { 
+        Write-Error "Database ${serviceName} on url ${url} does not run" 
+        exit 1
+    }
+}
+
+$httpPrefix = "http://"
+
+function AddPrefix {
+    param (
+        [string] $url,
+        [string] $prefix = $httpPrefix       
+    )
+
+    if(-Not($url.Contains($prefix)))
+    {
+       return $prefix + $url 
+    }
+}
+
+$elasticSearchUrl = AddPrefix $elasticSearchUrl
+$existDbTempUrl = AddPrefix $existDbUrl
+
+Write-Host "Testing connections"
+TestConnection "ElasticSearch" -url $elasticSearchUrl
+TestConnection "eXist-db" -url  $existDbTempUrl
 
 
 $DatabaseFolderPath = Join-Path $CurrentPath "Database"
@@ -21,10 +64,10 @@ Write-Host "Running script  ${ElasticSearchScript}"
 
 if($recreateDatabases)
 {
-    & $ElasticScripthPath -recreateMode
+    & $ElasticScripthPath -url $elasticSearchUrl -recreateMode
 }
 else {
-	& $ElasticScripthPath
+	& $ElasticScripthPath -url $elasticSearchUrl
 }
 
 $ExistDbScript = ""
@@ -38,6 +81,9 @@ else {
 
 $ExistDbScriptPath = Join-Path $DatabaseFolderPath $ExistDbScript
 Write-Host "Running script  ${ExistDbScript}"
-& $ExistDbScriptPath
+$existPrefix = "xmldb:exist://" 
+$existDbTempUrl = AddPrefix $existDbUrl -prefix $existPrefix
+$existDbTempUrl = $existDbTempUrl + "/xmlrpc"
+& $ExistDbScriptPath $existDbTempUrl
 
 Set-Location $CurrentPath
