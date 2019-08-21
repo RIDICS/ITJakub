998574f38933d187bfcecd4f33cdc85c885de48d
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-08-21 14:08:53 +0200

    Refactor M_002_DefaultValues, add comments

--- UJCSystem/Vokabular.Database.Migrations/Migrations/VokabularDB/M_002_DefaultValues.cs
+++ UJCSystem/Vokabular.Database.Migrations/Migrations/VokabularDB/M_002_DefaultValues.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using FluentMigrator;
-using FluentMigrator.SqlServer;
 using Ridics.DatabaseMigrator.QueryBuilder;
 using Ridics.DatabaseMigrator.Shared.TagsAttributes;
 
@@ -11,120 +10,138 @@ namespace Vokabular.Database.Migrations.Migrations.VokabularDB
     [Migration(002)]
     public class M_002_DefaultValues : ForwardOnlyMigration
     {
+        private const string PageToHtml = "pageToHtml.xsl";
+        private const string DictionaryToHtml = "dictionaryToHtml.xsl";
+        private const string PageToRtf = "pageToRtf.xsl";
+        private const string DescriptionPaged = "def for paged";
+        private const string DescriptionDicts = "def for dicts";
+
+        // Output formats
+        private const int Unknown = 0;
+        private const int Html = 1;
+        private const int Rtf = 2;
+        private const int Xml = 3;
+
+        // Resource levels
+        private const int Version = 0;
+        private const int Book = 1;
+        private const int Shared = 2;
+
+        // Book types
+        private const int Edition = 0;                   
+        private const int Dictionary = 1;                
+        private const int Grammar = 2;                   
+        private const int ProfessionalLiterature = 3;    
+        private const int TextBank = 4;                  
+        private const int BibliographicalItem = 5;       
+        private const int CardFile = 6;
+        private const int AudioBook = 7;           
+        
+        private const int TrueValue = 1;                 
+        private const int FalseValue = 0;                 
+
+
         public override void Up()
         {
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 0, Type = 0});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 1, Type = 1});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 2, Type = 2});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 3, Type = 3});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 4, Type = 4});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 5, Type = 5});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 6, Type = 6});
-            Insert.IntoTable("BookType").WithIdentityInsert().Row(new { Id = 7, Type = 7});
+            Insert.IntoTable("BookType")
+                .Row(new {Type = Edition}) 
+                .Row(new {Type = Dictionary}) 
+                .Row(new {Type = Grammar}) 
+                .Row(new {Type = ProfessionalLiterature}) 
+                .Row(new {Type = TextBank})
+                .Row(new {Type = BibliographicalItem})
+                .Row(new {Type = CardFile})
+                .Row(new {Type = AudioBook});
 
             Execute.WithConnection((connection, transaction) =>
             {
                 var editionTypeId = Query.Conn(connection, transaction).Select<int>("Id").From("BookType")
-                    .Where("Type", "0").Run().Single();
+                    .Where("Type", Edition.ToString()).Run().Single();
 
                 var dictionaryTypeId = Query.Conn(connection, transaction).Select<int>("Id").From("BookType")
-                    .Where("Type", "1").Run().Single();
+                    .Where("Type", Dictionary.ToString()).Run().Single();
 
                 var grammarTypeId = Query.Conn(connection, transaction).Select<int>("Id").From("BookType")
-                    .Where("Type", "2").Run().Single();
+                    .Where("Type", Grammar.ToString()).Run().Single();
 
                 var professionalLiteratureTypeId = Query.Conn(connection, transaction).Select<int>("Id").From("BookType")
-                    .Where("Type", "3").Run().Single();
+                    .Where("Type", ProfessionalLiterature.ToString()).Run().Single();
 
                 var textBankTypeId = Query.Conn(connection, transaction).Select<int>("Id").From("BookType")
-                    .Where("Type", "4").Run().Single();
-
-               Query.Conn(connection, transaction).Insert("Transformation")
-                    .Row(new
-                    {
-                        Name = "pageToHtml.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 1,
-                        BookType = editionTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
+                    .Where("Type", TextBank.ToString()).Run().Single();
 
                 Query.Conn(connection, transaction).Insert("Transformation")
                     .Row(new
                     {
-                        Name = "dictionaryToHtml.xsl",
-                        Description = "def for dicts",
-                        OutputFormat = 1,
+                        Name = PageToHtml,
+                        Description = DescriptionPaged,
+                        OutputFormat = Html,
+                        BookType = editionTypeId,
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
+                    .Row(new
+                    {
+                        Name = DictionaryToHtml,
+                        Description = DescriptionDicts,
+                        OutputFormat = Html,
                         BookType = dictionaryTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToHtml.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 1,
+                        Name = PageToHtml,
+                        Description = DescriptionPaged,
+                        OutputFormat = Html,
                         BookType = grammarTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToHtml.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 1,
+                        Name = PageToHtml,
+                        Description = DescriptionPaged,
+                        OutputFormat = Html,
                         BookType = professionalLiteratureTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToHtml.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 1,
+                        Name = PageToHtml,
+                        Description = DescriptionPaged,
+                        OutputFormat = Html,
                         BookType = textBankTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToRtf.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 2,
+                        Name = PageToRtf,
+                        Description = DescriptionPaged,
+                        OutputFormat = Rtf,
                         BookType = editionTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToRtf.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 2,
+                        Name = PageToRtf,
+                        Description = DescriptionPaged,
+                        OutputFormat = Rtf,
                         BookType = grammarTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
-                    }).Run();
-
-                Query.Conn(connection, transaction).Insert("Transformation")
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
+                    })
                     .Row(new
                     {
-                        Name = "pageToRtf.xsl",
-                        Description = "def for paged",
-                        OutputFormat = 2,
+                        Name = PageToRtf,
+                        Description = DescriptionPaged,
+                        OutputFormat = Rtf,
                         BookType = professionalLiteratureTypeId,
-                        IsDefaultForBookType = 1,
-                        ResourceLevel = 2
+                        IsDefaultForBookType = TrueValue,
+                        ResourceLevel = Shared
                     }).Run();
             });
         }
