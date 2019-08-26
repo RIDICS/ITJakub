d6355afec149e02a31f328bcd5a887a70b8b2e52
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-26 18:09:02 +0200

    Register MainServiceClient in BatchImport app using ServiceCollection, change lifecycle

--- UJCSystem/ITJakub.BatchImport.Client/ComponentInstaller.cs
+++ UJCSystem/ITJakub.BatchImport.Client/ComponentInstaller.cs
@@ -5,27 +5,30 @@ using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using ITJakub.BatchImport.Client.BusinessLogic;
 using ITJakub.BatchImport.Client.BusinessLogic.Communication;
+using ITJakub.BatchImport.Client.ServiceClient;
+using Microsoft.Extensions.DependencyInjection;
 using Vokabular.MainService.DataContracts;
-using Vokabular.MainService.DataContracts.Clients;
+using Vokabular.Shared.WcfService;
 
 namespace ITJakub.BatchImport.Client
 {
     public class ComponentInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
-        {            
-            container.Register(Component.For<FileUploadManager>());
-            container.Register(Component.For<CommunicationProvider>());
-
+        {
             var mainServiceConfiguration = new MainServiceClientConfiguration
             {
                 Url = new Uri(ConfigurationManager.AppSettings["MainService"])
             };
 
-            container.Register(Component.For<MainServiceClientConfiguration>().Instance(mainServiceConfiguration));
-            container.Register(Component.For<IMainServiceAuthTokenProvider, AuthenticationManager>());
-            container.Register(Component.For<MainServiceRestClient>().LifeStyle.Singleton);
-            container.Register(Component.For<MainServiceSessionClient>());
+            container.Register(Component.For<AuthenticationManager>());
+            container.Register(Component.For<FileUploadManager>());
+            container.Register(Component.For<CommunicationProvider>());
+
+            var services = new ServiceCollection();
+            services.RegisterMainServiceClientComponents<MainServiceAuthTokenProvider, MainServiceClientLocalization>(mainServiceConfiguration);
+
+            container.AddServicesCollection(services);
         }
     }
 }
