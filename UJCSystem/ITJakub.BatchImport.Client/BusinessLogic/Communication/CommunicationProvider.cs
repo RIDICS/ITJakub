19dd15a0e7f34498c26fdcf6b6e351548c46808b
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-26 14:52:58 +0200

    Remove dependencies to obsolete projects, remove old clients from BatchImport app

--- UJCSystem/ITJakub.BatchImport.Client/BusinessLogic/Communication/CommunicationProvider.cs
+++ UJCSystem/ITJakub.BatchImport.Client/BusinessLogic/Communication/CommunicationProvider.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using ITJakub.ITJakubService.DataContracts;
-using ITJakub.ITJakubService.DataContracts.Clients;
-using Vokabular.MainService.DataContracts.Clients;
+﻿using Vokabular.MainService.DataContracts.Clients;
 
 namespace ITJakub.BatchImport.Client.BusinessLogic.Communication
 {
     public class CommunicationProvider
     {
         private readonly MainServiceSessionClient m_mainServiceSessionClient;
-        private const string EncryptedEndpointName = "ItJakubServiceEncrypted";
-        private const string MainServiceEndpointName = "ItJakubService";
-        private const string MainServiceEndpointNameAuthenticated = "ItJakubService.Authenticated";
-        private const string StreamedServiceEndpointName = "ItJakubServiceStreamed";
-        //private const string StreamedServiceEndpointNameAuthenticated = "ItJakubServiceStreamed.Authenticated";
-        private const string StreamedServiceEndpointNameAuthenticated = "ItJakubServiceStreamed";
 
         public CommunicationProvider(MainServiceSessionClient mainServiceSessionClient)
         {
@@ -24,62 +15,5 @@ namespace ITJakub.BatchImport.Client.BusinessLogic.Communication
         {
             return m_mainServiceSessionClient;
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="username">Username</param>
-        /// <param name="authenticationToken">Password or communication token</param>
-        /// <returns></returns>
-        public IItJakubService GetAuthenticatedClient(string username, string authenticationToken)
-        {
-            var client = new ItJakubServiceClient(MainServiceEndpointNameAuthenticated);
-            if (client.ClientCredentials == null)
-            {
-                throw new ArgumentException("Cannot set credentials for client");
-            }
-
-            client.ClientCredentials.UserName.UserName = username;
-            client.ClientCredentials.UserName.Password = authenticationToken;
-            return client;
-        }
-
-        public IItJakubService GetUnsecuredClient()
-        {
-            var client = new ItJakubServiceClient(MainServiceEndpointName);
-            return client;
-        }
-
-        public ItJakubServiceEncryptedClient GetEncryptedClient()
-        {
-            var client = new ItJakubServiceEncryptedClient(EncryptedEndpointName);
-            return client;
-        }
-
-        public ItJakubServiceStreamedClient GetStreamingClient()
-        {
-            var client = new ItJakubServiceStreamedClient(StreamedServiceEndpointName);
-            return client;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="username">Username</param>
-        /// <param name="authenticationToken">Password or communication token</param>>
-        /// <returns></returns>
-        public ItJakubServiceStreamedClient GetStreamingClientAuthenticated(string username, string authenticationToken)
-        {
-            var client = new ItJakubServiceStreamedClient(StreamedServiceEndpointNameAuthenticated);
-            if (client.ClientCredentials == null)
-            {
-                throw new ArgumentException("Cannot set credentials for client");
-            }
-            client.ClientCredentials.UserName.UserName = username;
-            client.ClientCredentials.UserName.Password = authenticationToken;
-            return client;
-        }
-
-
     }
 }
