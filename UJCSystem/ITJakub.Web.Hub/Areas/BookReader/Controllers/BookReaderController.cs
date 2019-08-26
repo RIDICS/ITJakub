948763ccda246d22ebde6e3f1c2edb9f0e668191
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-26 13:30:09 +0200

    Redirect to correct book reader by default book type

--- UJCSystem/ITJakub.Web.Hub/Areas/BookReader/Controllers/BookReaderController.cs
+++ UJCSystem/ITJakub.Web.Hub/Areas/BookReader/Controllers/BookReaderController.cs
@@ -1,6 +1,8 @@
-﻿using ITJakub.Web.Hub.Controllers;
+﻿using System;
+using ITJakub.Web.Hub.Controllers;
 using ITJakub.Web.Hub.Core.Communication;
 using Microsoft.AspNetCore.Mvc;
+using Vokabular.Shared.DataContracts.Types;
 
 namespace ITJakub.Web.Hub.Areas.BookReader.Controllers
 {
@@ -25,14 +27,50 @@ namespace ITJakub.Web.Hub.Areas.BookReader.Controllers
             {
                 return NotFound();
             }
-            
-            return RedirectToAction("Listing", "Editions",new
+
+            switch (snapshotInfo.DefaultBookType)
             {
-                Area = "Editions",
-                bookId,
-                searchText,
-                page = pageId,
-            });
+                case BookTypeEnumContract.Edition:
+                case BookTypeEnumContract.ProfessionalLiterature:
+                case BookTypeEnumContract.TextBank:
+                    return RedirectToAction("Listing", "Editions", new
+                    {
+                        Area = "Editions",
+                        bookId,
+                        searchText,
+                        page = pageId,
+                    });
+                case BookTypeEnumContract.Dictionary:
+                    return RedirectToAction("Listing", "Dictionaries", new
+                    {
+                        Area = "Dictionaries",
+                        books = $"[{bookId}]"
+                    });
+                case BookTypeEnumContract.Grammar:
+                    return RedirectToAction("Listing", "OldGrammar", new
+                    {
+                        Area = "OldGrammar",
+                        bookId,
+                        searchText,
+                        page = pageId,
+                    });
+                case BookTypeEnumContract.BibliographicalItem:
+                    // No direct access to book (content doesn't exist), but at least display the bibliography list
+                    return RedirectToAction("Search", "Bibliographies", new
+                    {
+                        Area = "Bibliographies",
+                    });
+                case BookTypeEnumContract.AudioBook:
+                    // No direct access to book, but at least display the audio books list
+                    return RedirectToAction("List", "AudioBooks", new
+                    {
+                        Area = "AudioBooks",
+                    });
+                case BookTypeEnumContract.CardFile:
+                    return BadRequest();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
