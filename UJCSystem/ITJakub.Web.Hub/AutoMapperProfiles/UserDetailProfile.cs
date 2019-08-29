5a31b92d9be9042ac31635e065aaa3654de268a3
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-29 15:11:07 +0200

    Add missing AutoMapper profile for displaying user detail

--- UJCSystem/ITJakub.Web.Hub/AutoMapperProfiles/UserDetailProfile.cs
+++ UJCSystem/ITJakub.Web.Hub/AutoMapperProfiles/UserDetailProfile.cs
@@ -8,14 +8,6 @@ namespace ITJakub.Web.Hub.AutoMapperProfiles
     {
         public UserDetailProfile()
         {
-            CreateMap<UserDetailContract, UserDetailViewModel>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.Roles, opt => opt.Ignore());
-
             CreateMap<UserContract, UserDetailViewModel>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
@@ -23,6 +15,18 @@ namespace ITJakub.Web.Hub.AutoMapperProfiles
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
                 .ForMember(dest => dest.Email, opt => opt.Ignore())
                 .ForMember(dest => dest.Roles, opt => opt.Ignore());
+
+            CreateMap<UserDetailContract, UserDetailViewModel>()
+                .IncludeBase<UserContract, UserDetailViewModel>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Roles, opt => opt.Ignore());
+
+
+            CreateMap<UserDetailContract, UpdateUserViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName));
 
             CreateMap<UserDetailContract, UpdateContactViewModel>()
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
