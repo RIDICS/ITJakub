b5abb73d4d12a787ffb2beb45ad088f7e6b4f84c
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-08-30 14:11:41 +0200

    Add portal type header to Main service Swagger

---
+++ UJCSystem/Vokabular.MainService.DataContracts/MainServiceHeaders.cs
@@ -0,0 +1,7 @@
+﻿namespace Vokabular.MainService.DataContracts
+{
+    public static class MainServiceHeaders
+    {
+        public const string PortalTypeHeader = "X-Portal-Type";
+    }
+}
---
+++ UJCSystem/Vokabular.Shared.AspNetCore/WebApiUtils/Documentation/AddGlobalHeaderFilter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Vokabular.Shared.AspNetCore.WebApiUtils.Documentation
+{
+    public class AddGlobalHeaderFilter : IOperationFilter
+    {
+        private readonly GlobalHeaderValues m_headerValues;
+
+        public AddGlobalHeaderFilter(GlobalHeaderValues headerValues)
+        {
+            m_headerValues = headerValues;
+        }
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<IParameter>();
+
+            operation.Parameters.Add(new NonBodyParameter
+            {
+                Name = m_headerValues.Name,
+                In = "header",
+                Type = "string",
+                Enum = m_headerValues.Values,
+                Default = m_headerValues.DefaultValue,
+                Required = false
+            });
+        }
+    }
+
+    public class GlobalHeaderValues
+    {
+        public string Name { get; set; }
+        public List<object> Values { get; set; }
+        public string DefaultValue { get; set; }
+    }
+}
