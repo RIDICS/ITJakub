0a7343431694f190744a34f45c8902513dc8c3a3
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-09-12 09:18:24 +0200

    Fix ReSharper type error in TypeScript

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/ridics.project.work.tabs.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/ridics.project.work.tabs.ts
@@ -1109,7 +1109,7 @@ class ProjectWorkForumTab extends ProjectModuleTabBase {
 }
 
 class MetadataUiHelper {
-    static addPerson($container: JQuery<HTMLDivElement>,
+    static addPerson($container: JQuery<HTMLElement>,
         name: string,
         surname: string,
         idValue: string | number,
