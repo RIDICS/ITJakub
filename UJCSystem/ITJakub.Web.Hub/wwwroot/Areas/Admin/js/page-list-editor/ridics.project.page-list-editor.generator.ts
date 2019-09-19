0497fd532433a803c81e619bb7b2c97bb9babad0
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-09-19 13:48:28 +0200

    Page list editor - improve page generator

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-list-editor/ridics.project.page-list-editor.generator.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-list-editor/ridics.project.page-list-editor.generator.ts
@@ -6,125 +6,276 @@
 };
 
 class PageListGenerator {
-    generatePageList(from: number, to: number, style: number, doublePage: boolean): string[
-        ] {
+    private readonly regex1rTemplate = "^(([1-9]){1}|([0-9]){2,})(r|v)$";
+    private readonly regexI1rTemplate = `^(I')(([1-9]){1}|([0-9]){2,})(r|v)$`;
+    private readonly regexRomanTemplate = "^[MDCLXVI)(]+$";
+    private readonly regexArabicTemplate = "^([1-9]{1}|[0-9]{2,})$";
+
+    generatePageList(from: string, to: string, style: number, doublePage: boolean): string[] {
         var stringList: string[] = [];
         var beginCount = 1;
         var leftPage = "";
-        if (doublePage) {
-            to = to * 2;
-        }
+       
+        const fromNumber = this.convertToNumber(from, style);
+        const toNumber = this.convertToNumber(to, style);
+        
         switch (style) {
-        case PageListFormat["1r"]:
-        {
-            for (let i = from; i <= to; i++) {
-                const numbericalPart = Math.ceil(i / 2);
-                const literalPart = i % 2 === 0 ? "v" : "r"; //TODO check whether v/r depict even/odd
-                const pageName = `${numbericalPart}${literalPart}`;
-                if (doublePage) {
-                    if (beginCount % 2 !== 0) {
-                        leftPage = pageName;
+            case PageListFormat["1r"]:
+                {
+                    for (let i = fromNumber; i <= toNumber; i++) {
+                        const numbericalPart = Math.ceil(i / 2);
+                        const literalPart = i % 2 === 0 ? "v" : "r"; //TODO check whether v/r depict even/odd
+                        const pageName = `${numbericalPart}${literalPart}`;
+                        if (doublePage) {
+                            if (beginCount % 2 !== 0) {
+                                leftPage = pageName;
+
+                                if (i === toNumber) {
+                                    stringList.push(pageName);
+                                }
+                            }
+                            if (beginCount % 2 === 0) {
+                                stringList.push(`${leftPage}–${pageName}`);
+                            }
+
+                            beginCount++;
+                        } else {
+                            stringList.push(pageName);
+                        }
                     }
-                    if (beginCount % 2 === 0) {
-                        stringList.push(`${leftPage}–${pageName}`);
-                    }
-                    beginCount++;
-                } else {
-                    stringList.push(pageName);
+                    break;
                 }
-            }
-            break;
-        }
-        case PageListFormat["I'1r"]:
-        {
-            for (let i = from; i <= to; i++) {
-                const prefix = "I"; //TODO check how prefix is defined
-                const numbericalPart = Math.ceil(i / 2);
-                const literalPart = i % 2 === 0 ? "v" : "r"; //TODO check whether v/r depict even/odd
-                const pageName = `${prefix}'${numbericalPart}${literalPart}`;
-                if (doublePage) {
-                    if (beginCount % 2 !== 0) {
-                        leftPage = pageName;
+            case PageListFormat["I'1r"]:
+                {
+                    for (let i = fromNumber; i <= toNumber; i++) {
+                        const prefix = "I"; //TODO check how prefix is defined
+                        const numbericalPart = Math.ceil(i / 2);
+                        const literalPart = i % 2 === 0 ? "v" : "r"; //TODO check whether v/r depict even/odd
+                        const pageName = `${prefix}'${numbericalPart}${literalPart}`;
+                        if (doublePage) {
+                            if (beginCount % 2 !== 0) {
+                                leftPage = pageName;
+
+                                if (i === toNumber) {
+                                    stringList.push(pageName);
+                                }
+                            }
+                            if (beginCount % 2 === 0) {
+                                stringList.push(`${leftPage}–${pageName}`);
+                            }
+                            beginCount++;
+                        } else {
+                            stringList.push(pageName);
+                        }
                     }
-                    if (beginCount % 2 === 0) {
-                        stringList.push(`${leftPage}–${pageName}`);
-                    }
-                    beginCount++;
-                } else {
-                    stringList.push(pageName);
+                    break;
                 }
-            }
-            break;
-        }
-        case PageListFormat.Roman:
-        {
-            for (let i = from; i <= to; i++) {
-                const pageName = this.romanise(i);
-                if (doublePage) {
-                    if (beginCount % 2 !== 0) {
-                        leftPage = pageName;
+            case PageListFormat.Roman:
+                {
+                    for (let i = fromNumber; i <= toNumber; i++) {
+                        const pageName = this.romanise(i);
+                        if (doublePage) {
+                            if (beginCount % 2 !== 0) {
+                                leftPage = pageName;
+
+                                if (i === toNumber) {
+                                    stringList.push(pageName);
+                                }
+                            }
+                            if (beginCount % 2 === 0) {
+                                stringList.push(`${leftPage}–${pageName}`);
+                            }
+                            beginCount++;
+                        } else {
+                            stringList.push(pageName);
+                        }
                     }
-                    if (beginCount % 2 === 0) {
-                        stringList.push(`${leftPage}–${pageName}`);
-                    }
-                    beginCount++;
-                } else {
-                    stringList.push(pageName);
+                    break;
                 }
-            }
-            break;
-        }
-        case PageListFormat.Arabic:
-        {
-            for (let i = from; i <= to; i++) {
-                const pageName = i.toString();
-                if (doublePage) {
-                    if (beginCount % 2 !== 0) {
-                        leftPage = pageName;
+            case PageListFormat.Arabic:
+                {
+                    for (let i = fromNumber; i <= toNumber; i++) {
+                        const pageName = i.toString();
+                        if (doublePage) {
+                            if (beginCount % 2 !== 0) {
+                                leftPage = pageName;
+
+                                if (i === toNumber) {
+                                    stringList.push(pageName);
+                                }
+                            }
+                            if (beginCount % 2 === 0) {
+                                stringList.push(`${leftPage}–${pageName}`);
+                            }
+                            beginCount++;
+                        } else {
+                            stringList.push(pageName);
+                        }
                     }
-                    if (beginCount % 2 === 0) {
-                        stringList.push(`${leftPage}–${pageName}`);
-                    }
-                    beginCount++;
-                } else {
-                    stringList.push(pageName);
+                    break;
                 }
-            }
-            break;
-        }
-        default:
-        {
-            alert("Invalid format");
-            break;
-        }
+            default:
+                {
+                    alert("Invalid format");
+                    break;
+                }
         }
         return stringList;
     }
 
+    checkInputValue(value: string, style: number) {
+        if (value === "")
+            return false;
+
+        let regex: RegExp;
+        switch (style) {
+            case PageListFormat["1r"]:
+                regex = new RegExp(this.regex1rTemplate);
+                break;
+            case PageListFormat["I'1r"]:
+                regex = new RegExp(this.regexI1rTemplate);
+                break;
+            case PageListFormat.Roman:
+                regex = new RegExp(this.regexRomanTemplate);
+                break;
+            case PageListFormat.Arabic:
+                regex = new RegExp(this.regexArabicTemplate);
+                break;
+            default:
+                return false;
+        }
+        return regex.test(value);
+    }
+
+    checkValidPagesLength(from: string, to: string, style: number) {
+        return this.convertToNumber(from, style) <= this.convertToNumber(to, style);
+    }
+
+    checkValidDoublePageRange(from: string, to: string, style: number) {
+        if (!this.checkInputValue(from, style) || !this.checkInputValue(to, style)) {
+            return false;
+        }
+
+        switch (style) {
+            case PageListFormat["1r"]:
+            {
+                const first = from[from.length - 1];
+                const last = to[to.length - 1];
+                return first !== last;
+            }
+            case PageListFormat["I'1r"]:
+            {
+                const first = from[from.length - 1];
+                const last = to[to.length - 1];
+                return first !== last;
+            }
+            case PageListFormat.Roman:
+            case PageListFormat.Arabic:
+            {
+                const fromNumber = this.convertToNumber(from, style);
+                const toNumber = this.convertToNumber(to, style);
+                return (toNumber - fromNumber) % 2 === 1;
+            }
+        }
+
+        return false;
+    }
+
+    private convertToNumber(value: string, style: number): number {
+        if (this.checkInputValue(value, style)) {
+            switch (style) {
+            case PageListFormat["1r"]:
+            {
+                let numericValue = Number(value.substr(0, value.length - 2));
+                if (value[value.length - 1] === "r") {
+                    numericValue = - 1;
+                }
+                return numericValue;
+            }
+            case PageListFormat["I'1r"]:
+            {
+                let numericValue = Number(value.substr(2, value.length - 2));
+                if (value[value.length - 1] === "r") {
+                    numericValue = - 1;
+                }
+                return numericValue;
+            }
+            case PageListFormat.Roman:
+                return this.fromRoman(value);
+            case PageListFormat.Arabic:
+                return Number(value);
+            default:
+                return NaN;
+            }
+        }
+        return NaN;
+    }
+
     private romanise(arabic: number): string {
-        const lookup = {
-            M: 1000,
-            CM: 900,
-            D: 500,
-            CD: 400,
-            C: 100,
-            XC: 90,
-            L: 50,
-            XL: 40,
-            X: 10,
-            IX: 9,
-            V: 5,
-            IV: 4,
-            I: 1
-        };
         let roman = "";
         let i: string;
-        for (i in lookup) {
-            while (arabic >= lookup[i]) {
+        for (i in this.romanLookup) {
+            while (arabic >= this.romanLookup[i]) {
                 roman += i;
-                arabic -= lookup[i];
+                arabic -= this.romanLookup[i];
             }
         }
         return roman;
     }
+
+    private fromBigRoman(rn) {
+        let sum = 0;
+        let splitRoman;
+        const rx = /(\(*)([MDCLXVI]+)/g;
+
+        while ((splitRoman = rx.exec(rn)) != null) {
+            const exponent = splitRoman[1].length;
+            let roman = this.fromRoman(splitRoman[2]);
+            if (isNaN(roman)) return NaN;
+            if (exponent) roman *= Math.pow(1000, exponent);
+            sum += roman;
+        }
+        return sum;
+    }
+
+    private fromRoman(roman): number {
+        roman = roman.toUpperCase();
+        const reg = new RegExp(this.regexRomanTemplate);
+        if (reg.test(roman)) {
+            if (roman.indexOf("(") === 0)
+                return this.fromBigRoman(roman);
+
+            let sum = 0;
+            let i = 0;
+            let next: number;
+            let val: number;
+
+            while (i < roman.length) {
+                val = this.romanLookup[roman.charAt(i++)];
+                next = this.romanLookup[roman.charAt(i)] || 0;
+                if (next - val > 0) val *= -1;
+                sum += val;
+            }
+
+            if (this.romanise(sum) === roman)
+                return sum;
+        }
+        return NaN;
+    };
+
+    private romanLookup = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
 }
--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-list-editor/ridics.project.page-list-editor.main.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/page-list-editor/ridics.project.page-list-editor.main.ts
@@ -12,8 +12,57 @@
         this.errorHandler = new ErrorHandler();
         this.util = new EditorsApiClient();
         this.listGenerator = new PageListGenerator();
+
+        jQuery.extend(jQuery.validator.messages,
+            {
+                required: localization.translate("EnterValidNumbers", "RidicsProject").value
+            });
+
+        $.validator.addMethod(
+            "regex",
+            (value, element, regex) => {
+                if (value === "")
+                    return false;
+                return this.listGenerator.checkInputValue(value, this.getSelectedFormat());
+            },
+            localization.translate("EnterValidNumbers", "RidicsProject").value
+        );
+
+        $("#rangeForm").validate({
+            rules: {
+                "project-pages-generate-from": {
+                    required: true,
+                    regex: true,
+                },
+                "project-pages-generate-to": {
+                    required: true,
+                    regex: true,
+                }
+            },
+            highlight: (element) => {
+                $(element).parents(".form-group").removeClass("has-success").addClass("has-error");
+            },
+            unhighlight: (element) => {
+                $(element).parents(".form-group").removeClass("has-error").addClass("has-success");
+            }
+        });
+
+        $("#project-pages-format").change(() => {
+            $("#rangeForm").validate().element(":input[name=\"project-pages-generate-from\"]");
+            $("#rangeForm").validate().element(":input[name=\"project-pages-generate-to\"]");
+            this.setAlerts();
+        });
+      
+        const doublePageRadiobuttonEl = $(".doublepage-radiobutton");
+        doublePageRadiobuttonEl.change(() => {
+            this.setAlerts();
+        });
+
+        $("#project-pages-generate-to, #project-pages-generate-from").on("input", () => {
+            this.setAlerts();
+        });
     }
-    
+
     init(projectId: number) {
         this.editDialog = new BootstrapDialogWrapper({
             element: $("#project-pages-dialog"),
@@ -28,24 +77,26 @@
             this.moveList(false, $(".pages"));
         });
 
-        $(".save-pages-button").on("click", () => {
-            const pages = $(".page-row").toArray();
-            const pageListArray: IUpdatePage[] = [];
-            for (let i = 0; i < pages.length; i++) {
-                pageListArray.push({
-                    id: $(pages[i]).data("page-id"),
-                    position: i + 1,
-                    name: $(pages[i]).find(".name").text().trim()
+        $(".save-pages-button").on("click",
+            () => {
+                const pages = $(".page-row").toArray();
+                const pageListArray: IUpdatePage[] = [];
+                for (let i = 0; i < pages.length; i++) {
+                    pageListArray.push({
+                        id: $(pages[i]).data("page-id"),
+                        position: i + 1,
+                        name: $(pages[i]).find(".name").text().trim()
+                    });
+                }
+                this.util.savePageList(projectId, pageListArray).done(() => {
+                    $("#unsavedChanges").addClass("hide");
+                }).fail((error) => {
+                    this.gui.showInfoDialog(localization.translate("Error").value,
+                        this.errorHandler.getErrorMessage(error));
                 });
-            }
-            this.util.savePageList(projectId, pageListArray).done(() => {
-                $("#unsavedChanges").addClass("hide");
-            }).fail((error) => {
-                this.gui.showInfoDialog(localization.translate("Error").value, this.errorHandler.getErrorMessage(error));
             });
-        });
 
-       this.initPageRowClicks();
+        this.initPageRowClicks();
 
         $("#project-pages-edit-button").click(() => {
             this.editDialog.show();
@@ -60,16 +111,14 @@
                     this.checkmarkFS();
                 }
             }
-           
+
             this.trackSpecialPagesCheckboxesState();
 
-            
+
             $(".page-list-editor-content").on("click",
                 ".generate-page-list",
                 () => {
-                    const doublePageRadiobuttonEl = $(".doublepage-radiobutton");
-                    const doublePageGeneration = doublePageRadiobuttonEl.prop("checked") as boolean;
-                    this.startGeneration(doublePageGeneration);
+                    this.startGeneration();
                 });
 
             $(".page-list-editor-content").on("click",
@@ -81,6 +130,55 @@
                 }
             );
         });
+    }
+
+    private getSelectedFormat(): number {
+        const formatString = $("#project-pages-format").find(":selected").data("format-value") as string;
+        return PageListFormat[formatString] as number;
+    }
+
+    private isSetDoublePageGeneration(): boolean {
+        const doublePageRadiobuttonEl = $(".doublepage-radiobutton");
+        return doublePageRadiobuttonEl.prop("checked") as boolean;
+    }
+
+    private setAlerts(): void {
+        const from = String($("#project-pages-generate-from").val());
+        const to = String($("#project-pages-generate-to").val());
+
+        const doubleGenerationWarning = $("#doubleGenerationWarning");
+        const swapNumbersError = $("#swapNumbersError");
+
+        if (!this.listGenerator.checkInputValue(from, this.getSelectedFormat()) ||
+            !this.listGenerator.checkInputValue(to, this.getSelectedFormat())) {
+            doubleGenerationWarning.addClass("hide");
+            swapNumbersError.addClass("hide");
+        }
+        else {
+            if (this.listGenerator.checkValidPagesLength(from, to, this.getSelectedFormat())) {
+                swapNumbersError.addClass("hide");
+                this.setDoubleGenerationWarning();
+            } else {
+                swapNumbersError.removeClass("hide");
+                doubleGenerationWarning.addClass("hide");
+            }
+        }
+    }
+
+    private setDoubleGenerationWarning(): void {
+        const doubleGenerationWarning = $("#doubleGenerationWarning");
+        if (this.isSetDoublePageGeneration()) {
+            const to = String($("#project-pages-generate-from").val());
+            const from = String($("#project-pages-generate-to").val());
+
+            if (this.listGenerator.checkValidDoublePageRange(to, from, this.getSelectedFormat())) {
+                doubleGenerationWarning.addClass("hide");
+            } else {
+                doubleGenerationWarning.removeClass("hide");
+            }
+        } else {
+            doubleGenerationWarning.addClass("hide");
+        }
     }
 
     private initPageRowClicks() {
@@ -99,17 +197,19 @@
         });
 
         $(".page-row .edit-page").off();
-        $(".page-row .edit-page").on("click", (event) => {
-            event.stopPropagation();
-            this.editPage($(event.currentTarget));
-        });
+        $(".page-row .edit-page").on("click",
+            (event) => {
+                event.stopPropagation();
+                this.editPage($(event.currentTarget));
+            });
 
         $(".page-row").off();
-        $(".page-row").on("click", (event) => {
-            const checkbox = $(event.currentTarget).find(".selection-checkbox");
-            checkbox.prop("checked", !checkbox.is(":checked"));
-            this.selectPage($(event.currentTarget));
-        });
+        $(".page-row").on("click",
+            (event) => {
+                const checkbox = $(event.currentTarget).find(".selection-checkbox");
+                checkbox.prop("checked", !checkbox.is(":checked"));
+                this.selectPage($(event.currentTarget));
+            });
     }
 
     private editPage(element: JQuery) {
@@ -158,7 +258,7 @@
         const subcontent = content.find(".sub-content");
         subcontent.addClass("loader");
         pageDetail.removeClass("hide");
-        
+
         this.util.getPageDetail(pageId).done((response) => {
             subcontent.removeClass("loader");
             subcontent.html(response);
@@ -268,33 +368,34 @@
             });
     }
 
-    private startGeneration(doublePage:boolean) {
-        const fromFieldValue = $("#project-pages-generate-from").val() as string;
-        const toFieldValue = $("#project-pages-generate-to").val() as string;
-        if (/\d+/.test(fromFieldValue) && /\d+/.test(toFieldValue)) {
-            const from = parseInt(fromFieldValue);
-            const to = parseInt(toFieldValue);
-            const formatString = $("#project-pages-format").find(":selected").data("format-value") as string;
-            const format = PageListFormat[formatString] as number;
-            if (!isNaN(from) && !isNaN(to)) {
-                if (to > from) {
-                    const pageList = this.listGenerator.generatePageList(from, to, format, doublePage);
-                    this.populateList(pageList);
-                    this.enableCheckboxes();
-                    this.trackSpecialPagesCheckboxesState();
-                } else {
-                    this.gui.showInfoDialog(localization.translate("Warning").value, localization.translate("SwapNumbers", "RidicsProject").value);
-                }
-            }
+    private startGeneration() {
+        const fromFieldValue = ($("#project-pages-generate-from").val() as string).replace(" ", "");;
+        const toFieldValue = ($("#project-pages-generate-to").val() as string).replace(" ", "");;
+
+        if (!this.listGenerator.checkInputValue(fromFieldValue, this.getSelectedFormat()) ||
+            !this.listGenerator.checkInputValue(toFieldValue, this.getSelectedFormat())) {
+            this.gui.showInfoDialog(localization.translate("Warning").value,
+                localization.translate("EnterValidNumbers", "RidicsProject").value);
+        } else if (this.listGenerator.checkValidPagesLength(String($("#project-pages-generate-from").val()),
+            String($("#project-pages-generate-to").val()),
+            this.getSelectedFormat()))
+        {
+            const pageList = this.listGenerator.generatePageList(fromFieldValue, toFieldValue, this.getSelectedFormat(), this.isSetDoublePageGeneration());
+            this.populateList(pageList);
+            this.enableCheckboxes();
+            this.trackSpecialPagesCheckboxesState();
         } else {
-            this.gui.showInfoDialog(localization.translate("Warning").value, localization.translate("EnterValidNumbers", "RidicsProject").value);
+            this.gui.showInfoDialog(localization.translate("Warning").value,
+                localization.translate("SwapNumbers", "RidicsProject").value);
         }
     }
 
     private populateList(pageList: string[]) {
         const listContainerEl = $(".page-listing tbody");
+        console.log(listContainerEl.children().length);
         if (listContainerEl.children().length) {
-            this.gui.showInfoDialog(localization.translate("Info").value, localization.translate("AddingGeneratedNames", "RidicsProject").value);
+            this.gui.showInfoDialog(localization.translate("Info").value,
+                localization.translate("AddingGeneratedNames", "RidicsProject").value);
         }
 
         for (let page of pageList) {
