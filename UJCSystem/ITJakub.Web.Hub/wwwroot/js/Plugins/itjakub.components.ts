a857f13496d6a966e1af7ad1c77478b3f0e61bca
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-09-26 15:18:21 +0200

    Update Dropzone, keep old styles

--- UJCSystem/ITJakub.Web.Hub/wwwroot/js/Plugins/itjakub.components.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/js/Plugins/itjakub.components.ts
@@ -8,6 +8,17 @@
             autoProcessQueue: true,
             parallelUploads: 5,
             acceptedFiles: this.acceptedFiles,
+            previewTemplate: `<div class="dz-preview dz-file-preview">
+  <div class="dz-details">
+    <div class="dz-filename"><span data-dz-name></span></div>
+    <div class="dz-size" data-dz-size></div>
+    <img data-dz-thumbnail />
+  </div>
+  <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+  <div class="dz-success-mark"><span>✔</span></div>
+  <div class="dz-error-mark"><span>✘</span></div>
+  <div class="dz-error-message"><span data-dz-errormessage></span></div>
+</div>`,
 
             dictInvalidFileType: localization.translate("FormatNotSupported", "PluginsJs").value + this.acceptedFiles,
             dictDefaultMessage: localization.translate("DropFilesForUpload", "PluginsJs").value,
