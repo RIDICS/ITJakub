6f17a84184a20c910c16c0b01f4600a012f47cba
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-10-01 08:57:42 +0200

    Add sequence column to ProjectResponsiblePerson

---
+++ UJCSystem/Vokabular.Database.Migrations/Migrations/VokabularDB/M_011_ResponsiblePersonOrder.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using Ridics.DatabaseMigrator.Shared.TagsAttributes;
+
+namespace Vokabular.Database.Migrations.Migrations.VokabularDB
+{
+    [DatabaseTags(DatabaseTagTypes.VokabularDB)]
+    [MigrationTypeTags(CoreMigrationTypeTagTypes.Structure, CoreMigrationTypeTagTypes.All)]
+    [Migration(011)]
+    public class M_011_ResponsiblePersonOrder : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Project_ResponsiblePerson").AddColumn("Sequence").AsInt32().NotNullable().SetExistingRowsTo(1);
+        }
+    }
+}
