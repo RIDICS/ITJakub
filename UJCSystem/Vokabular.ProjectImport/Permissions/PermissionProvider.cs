74a828eb82ec544415f615023868ff9f8ec5fee2
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-10-08 10:01:20 +0200

    Refactor loading permissions for bibliography import library

--- UJCSystem/Vokabular.ProjectImport/Permissions/PermissionProvider.cs
+++ UJCSystem/Vokabular.ProjectImport/Permissions/PermissionProvider.cs
@@ -1,25 +1,35 @@
-﻿using System.Linq;
-using Ridics.Authentication.DataContracts;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Ridics.Authentication.HttpClient.Client.Auth;
+using Vokabular.DataEntities.Database.Repositories;
+using Vokabular.Shared.DataEntities.UnitOfWork;
 
 namespace Vokabular.ProjectImport.Permissions
 {
     public class PermissionProvider : IPermissionsProvider
     {
         private readonly PermissionApiClient m_permissionApiClient;
+        private readonly PermissionRepository m_permissionRepository;
 
-        public PermissionProvider(PermissionApiClient permissionApiClient)
+        public PermissionProvider(PermissionApiClient permissionApiClient, PermissionRepository permissionRepository)
         {
             m_permissionApiClient = permissionApiClient;
+            m_permissionRepository = permissionRepository;
         }
 
-        public PermissionContract GetPermissionByName(string name)
+        public IList<int> GetRoleIdsByPermissionName(string name)
         {
             var allPermissions = m_permissionApiClient.GetAllPermissionsAsync(name).GetAwaiter().GetResult();
-            var permission =
-                allPermissions.SingleOrDefault(x => x.Name == name);
+            var permission = allPermissions.SingleOrDefault(x => x.Name == name);
+            if (permission == null)
+            {
+                return null;
+            }
 
-            return permission;
+            var roleExternalIds = m_permissionApiClient.GetRoleIdsByPermission(permission.Id).GetAwaiter().GetResult();
+            var roleIds = m_permissionRepository.InvokeUnitOfWork(x => x.GetGroupIdsByExternalIds(roleExternalIds));
+
+            return roleIds;
         }
     }
 }
