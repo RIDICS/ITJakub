ae472eea1aa8c4accece3a06547de88046e14da1
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-10-10 11:02:48 +0200

    Markdown header analyzer and test

---
+++ UJCSystem/Vokabular.MainService.Test/MarkdownHeaderAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vokabular.TextConverter.Markdown.Extensions;
+
+namespace Vokabular.MainService.Test
+{
+    [TestClass]
+    public class MarkdownHeaderAnalyzerTest
+    {
+        [TestMethod]
+        public void TestHeaderAnalyzer()
+        {
+            var testText = @"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+### Etiam quis quam.
+#Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.
+  ## # Maecenas sollicitudin.
+Mauris ## suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus.
+	#	Aliquam erat volutpat.
+Nulla non lectus sed nisl molestie malesuada.";
+
+            var analyzer = new MarkdownHeaderAnalyzer();
+            var result = analyzer.FindAllHeaders(testText);
+
+            Assert.AreEqual(3, result.Count);
+
+            Assert.AreEqual(3, result[0].Level);
+            Assert.AreEqual("Etiam quis quam.", result[0].Header);
+
+            Assert.AreEqual(2, result[1].Level);
+            Assert.AreEqual("# Maecenas sollicitudin.", result[1].Header);
+
+            Assert.AreEqual(1, result[2].Level);
+            Assert.AreEqual("Aliquam erat volutpat.", result[2].Header);
+        }
+    }
+}
---
+++ UJCSystem/Vokabular.TextConverter/Markdown/Extensions/MarkdownHeaderAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Vokabular.TextConverter.Markdown.Extensions
+{
+    public class MarkdownHeaderAnalyzer
+    {
+        public IList<MarkdownHeaderData> FindAllHeaders(string text)
+        {
+            var resultList = new List<MarkdownHeaderData>();
+
+            using (var stringReader = new StringReader(text))
+            {
+                string line;
+                while ((line = stringReader.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    if (!line.StartsWith("#"))
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] == '#')
+                        {
+                            continue;
+                        }
+                        
+                        if (char.IsWhiteSpace(line[i]))
+                        {
+                            var headerData = new MarkdownHeaderData
+                            {
+                                Level = i,
+                                Header = line.Substring(i).Trim(),
+                            };
+                            resultList.Add(headerData);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return resultList;
+        }
+    }
+
+    public class MarkdownHeaderData
+    {
+        public int Level { get; set; }
+        public string Header { get; set; }
+    }
+}
