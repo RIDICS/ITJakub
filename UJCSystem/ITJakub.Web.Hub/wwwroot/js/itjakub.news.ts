14df76ea00f332da24368930e56824611abed88c
Jakub Rada <jacub.rada@gmail.com>
2019-11-07 16:50:41 +0100

    removing forgotten sample elements for testing colors

--- UJCSystem/ITJakub.Web.Hub/wwwroot/js/itjakub.news.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/js/itjakub.news.ts
@@ -91,15 +91,6 @@
                 itemDiv.appendChild(itemMessage);
 
                 $(this.newsContainer).append(itemDiv);
-                $(this.newsContainer).append(lv.create(null, "lv-bars md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-circles md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-dots md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-squares md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-dashed md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-spinner md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-line md lv-mid lvt-5").getElement());
-                $(this.newsContainer).append(lv.create(null, "lv-bordered_line md lv-mid lvt-5").getElement());
-                
             }
         }
     }
