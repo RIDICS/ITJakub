21df81a9e976ad6d83cbf71fb616b8a98cd97ed3
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-11-19 13:46:44 +0100

    Rename AuthorTitle column in Feedback to AuthorEmail

---
+++ UJCSystem/Vokabular.Database.Migrations/Migrations/VokabularDB/M_016_FixFeedbackAuthorEmail.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using Ridics.DatabaseMigrator.Shared.TagsAttributes;
+
+namespace Vokabular.Database.Migrations.Migrations.VokabularDB
+{
+    [DatabaseTags(DatabaseTagTypes.VokabularDB)]
+    [MigrationTypeTags(CoreMigrationTypeTagTypes.Structure, CoreMigrationTypeTagTypes.All)]
+    [Migration(016)]
+    public class M_016_FixFeedbackAuthorEmail : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Rename.Column("AuthorTitle").OnTable("Feedback").To("AuthorEmail");
+        }
+    }
+}
