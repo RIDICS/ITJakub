3be80f2cb27ec277f90176870fb15cbabf12c538
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-12-02 16:08:23 +0100

    Categorization editor - fix project selecting

--- UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/ridics.project.work.tabs.ts
+++ UJCSystem/ITJakub.Web.Hub/wwwroot/Areas/Admin/js/ridics.project.work.tabs.ts
@@ -1076,7 +1076,7 @@ class ProjectWorkCategorizationTab extends ProjectMetadataTabBase {
 
     private onProjectListLoaded() {
         $("#add-project-to-group-dialog .project-row").on("click", (event) => {
-            $(event.currentTarget as Node as Element).addClass("selected").siblings().removeClass("selected");
+            $(event.currentTarget as Node as Element).addClass("active").siblings().removeClass("active");
         });
     }
 
@@ -1090,7 +1090,7 @@ class ProjectWorkCategorizationTab extends ProjectMetadataTabBase {
     }
 
     private assignProjectToGroup() {
-        const selectedProjectRow = $("#add-project-to-group-dialog .project-row.selected");
+        const selectedProjectRow = $("#add-project-to-group-dialog .project-row.active");
         if (selectedProjectRow.length < 1) {
             this.addProjectToGroupDialog.showError(localization.translate("ProjectIsNotSelected", "PermissionJs").value);
             return;
