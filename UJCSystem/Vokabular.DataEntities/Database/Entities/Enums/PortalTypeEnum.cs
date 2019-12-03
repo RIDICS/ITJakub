c8bf62f49ffddf830a56e30499c1ab3ad896d580
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-12-03 13:03:16 +0100

    Add property PortalType to Feedback and News

---
+++ UJCSystem/Vokabular.DataEntities/Database/Entities/Enums/PortalTypeEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Vokabular.DataEntities.Database.Entities.Enums
+{
+    public enum PortalTypeEnum : byte
+    {       
+        Research = 0,
+        Community = 1,
+    }
+}
---
+++ UJCSystem/Vokabular.Database.Migrations/Migrations/VokabularDB/M_018_AddPortalTypeToNewsAndFeedBack.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using Ridics.DatabaseMigrator.Shared.TagsAttributes;
+
+namespace Vokabular.Database.Migrations.Migrations.VokabularDB
+{
+    [DatabaseTags(DatabaseTagTypes.VokabularDB)]
+    [MigrationTypeTags(CoreMigrationTypeTagTypes.Structure, CoreMigrationTypeTagTypes.All)]
+    [Migration(018)]
+    public class M_018_AddPortalTypeToNewsAndFeedBack : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Feedback")
+                .AddColumn("ProjectType").AsInt16().NotNullable().SetExistingRowsTo(0);
+            
+            Alter.Table("NewsSyndicationItem")
+                .AddColumn("ProjectType").AsInt16().NotNullable().SetExistingRowsTo(0);
+        }
+    }
+}
