af2ef2a7c34da723d7f10088f58a361dbb7e4488
Tomáš Hrabáček <hrabato2@fel.cvut.cz>
2019-12-03 12:23:29 +0100

    Separate community and research portal static texts

---
+++ UJCSystem/ITJakub.Web.Hub/Core/DictionaryScopeReslover.cs
@@ -0,0 +1,23 @@
+﻿using ITJakub.Web.Hub.Options;
+using Microsoft.Extensions.Options;
+
+namespace ITJakub.Web.Hub.Core
+{
+    public class DictionaryScopeResolver
+    {
+        private const string CommunityTextsPrefix = "community";
+        private readonly PortalOption m_portalOption;
+
+        public DictionaryScopeResolver(IOptions<PortalOption> portalOption)
+        {
+            m_portalOption = portalOption.Value;
+        }
+
+        public string GetDictionaryScope(string dictionaryScope)
+        {
+            return m_portalOption.PortalType == PortalType.CommunityPortal && !dictionaryScope.Contains(CommunityTextsPrefix) 
+                ? $"{CommunityTextsPrefix}-{dictionaryScope}" 
+                : dictionaryScope;
+        }
+    }
+}
