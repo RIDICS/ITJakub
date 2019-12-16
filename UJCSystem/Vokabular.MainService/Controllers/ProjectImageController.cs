a875e42a5e29af093802c0cac71f0f576078d92c
Vladimír Pokorný <vlada.pokorny@seznam.cz>
2019-12-16 14:53:55 +0100

    Authorize access in ProjectImageController

--- UJCSystem/Vokabular.MainService/Controllers/ProjectImageController.cs
+++ UJCSystem/Vokabular.MainService/Controllers/ProjectImageController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Vokabular.DataEntities.Database.Entities.Enums;
 using Vokabular.MainService.Core.Managers;
 using Vokabular.MainService.DataContracts.Contracts;
 
@@ -10,15 +11,19 @@ namespace Vokabular.MainService.Controllers
     public class ProjectImageController : BaseController
     {
         private readonly ProjectContentManager m_projectContentManager;
+        private readonly AuthorizationManager m_authorizationManager;
 
-        public ProjectImageController(ProjectContentManager projectContentManager)
+        public ProjectImageController(ProjectContentManager projectContentManager, AuthorizationManager authorizationManager)
         {
             m_projectContentManager = projectContentManager;
+            m_authorizationManager = authorizationManager;
         }
 
         [HttpGet("{projectId}/image")]
         public List<ImageWithPageContract> GetImageList(long projectId)
         {
+            m_authorizationManager.AuthorizeBook(projectId, PermissionFlag.ReadProject);
+
             var result = m_projectContentManager.GetImageResourceList(projectId);
             return result;
         }
@@ -26,6 +31,8 @@ namespace Vokabular.MainService.Controllers
         [HttpGet("page/{pageId}/image")]
         public ImageContract GetImageResourceByPageId(long pageId)
         {
+            m_authorizationManager.AuthorizeResource(pageId, PermissionFlag.ReadProject);
+
             var result = m_projectContentManager.GetImageResourceByPageId(pageId);
             return result;
         }
@@ -33,6 +40,8 @@ namespace Vokabular.MainService.Controllers
         [HttpGet("image/{imageId}/content")]
         public IActionResult GetImage(long imageId)
         {
+            m_authorizationManager.AuthorizeResource(imageId, PermissionFlag.ReadProject);
+
             var result = m_projectContentManager.GetImageResource(imageId);
             if (result == null)
                 return NotFound();
@@ -43,6 +52,8 @@ namespace Vokabular.MainService.Controllers
         [HttpGet("image/version/{imageVersionId}/content")]
         public IActionResult GetImageVersion(long imageVersionId)
         {
+            m_authorizationManager.AuthorizeResourceVersion(imageVersionId, PermissionFlag.ReadProject);
+
             var result = m_projectContentManager.GetImageResourceVersion(imageVersionId);
             if (result == null)
                 return NotFound();
@@ -54,9 +65,20 @@ namespace Vokabular.MainService.Controllers
         [ProducesResponseType(typeof(NewResourceResultContract), StatusCodes.Status200OK)]
         public IActionResult CreateNewImageResourceVersion([FromForm] CreateImageContract data, IFormFile file)
         {
+            // Authorization is below
+
             if ((data.ImageId == null || data.OriginalVersionId == null) && data.ResourcePageId == null)
             {
                 return BadRequest("Image must be specified by ImageId + OriginalVersionId or by ResourcePageId");
+            }
+
+            if (data.ResourcePageId != null)
+            {
+                m_authorizationManager.AuthorizeResource(data.ResourcePageId.Value, PermissionFlag.EditProject);
+            }
+            else
+            {
+                m_authorizationManager.AuthorizeResource(data.ImageId.Value, PermissionFlag.EditProject);
             }
 
             var result = m_projectContentManager.CreateNewImageVersion(data, file.OpenReadStream());
